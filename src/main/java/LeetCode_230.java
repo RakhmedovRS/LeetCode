a5920bfd7a6cc7f28505304101d458d6c77c84b9
rassoll <rassoll32@gmail.com>
2020-02-08 14:34:43 +0300

    LeetCode 230

---
+++ src/main/java/LeetCode_230.java
@@ -0,0 +1,54 @@
+import java.util.LinkedList;
+
+/**
+ * @author RakhmedovRS
+ * @created 08-Feb-20
+ */
+public class LeetCode_230
+{
+	public static class TreeNode
+	{
+		int val;
+		TreeNode left;
+		TreeNode right;
+
+		TreeNode(int x){ val = x; }
+	}
+
+	public void kthSmallest(TreeNode root, LinkedList<Integer> values, int k)
+	{
+		if (root == null)
+		{
+			return;
+		}
+
+		if (values.size() == k)
+		{
+			return;
+		}
+
+		if (root.left == null && root.right == null)
+		{
+			values.add(root.val);
+			return;
+		}
+
+		kthSmallest(root.left, values, k);
+		if (values.size() == k)
+		{
+			return;
+		}
+		else
+		{
+			values.add(root.val);
+		}
+		kthSmallest(root.right, values, k);
+	}
+
+	public int kthSmallest(TreeNode root, int k)
+	{
+		LinkedList<Integer> values = new LinkedList<>();
+		kthSmallest(root, values, k);
+		return values.getLast();
+	}
+}
