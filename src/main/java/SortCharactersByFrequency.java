03216e9a454427ac67d50c6d9e4700b62b552ea2
rassoll <rassoll32@gmail.com>
2020-03-04 21:21:53 +0300

    Sort Characters By Frequency

---
+++ src/main/java/SortCharactersByFrequency.java
@@ -0,0 +1,47 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Sort Characters By Frequency
+ * LeetCode 451
+ *
+ * @author RakhmedovRS
+ * @created 04-Mar-20
+ */
+public class SortCharactersByFrequency
+{
+	public String frequencySort(String s)
+	{
+		Map<Character, Integer> cache = new HashMap<>();
+		for (Character ch : s.toCharArray())
+		{
+			cache.put(ch, cache.getOrDefault(ch, 0) + 1);
+		}
+
+		List<Map.Entry<Integer, Character>> secondCache = new ArrayList<>();
+		for (Map.Entry<Character, Integer> entry : cache.entrySet())
+		{
+			secondCache.add(new java.util.AbstractMap.SimpleEntry<>(entry.getValue(), entry.getKey()));
+		}
+
+		secondCache.sort((val1, val2) -> val2.getKey().compareTo(val1.getKey()));
+
+		StringBuilder sb = new StringBuilder();
+		for (Map.Entry<Integer, Character> entry : secondCache)
+		{
+			for (int i = 0; i < entry.getKey(); i++)
+			{
+				sb.append(entry.getValue());
+			}
+		}
+
+		return sb.toString();
+	}
+
+	public static void main(String[] args)
+	{
+		System.out.println(new SortCharactersByFrequency().frequencySort("tree"));
+	}
+}
