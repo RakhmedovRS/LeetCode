1f6c4336c12e67eeef5d34d67a6eedd215fc3b66
rassoll <rassoll32@gmail.com>
2020-03-08 11:54:57 +0300

    Print Words Vertically

---
+++ src/main/java/PrintWordsVertically.java
@@ -0,0 +1,60 @@
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Print Words Vertically
+ * LeetCode 1324
+ *
+ * @author RakhmedovRS
+ * @created 08-Mar-20
+ */
+public class PrintWordsVertically
+{
+	public List<String> printVertically(String s)
+	{
+		if (s == null || "".equals(s))
+		{
+			return new ArrayList<>();
+		}
+
+		String[] words = s.split(" ");
+		int maxLength = 0;
+		for (String word : words)
+		{
+			maxLength = Math.max(maxLength, word.length());
+		}
+
+		List<String> result = new ArrayList<>(maxLength);
+
+		StringBuilder temp;
+		for (int i = 0; i < maxLength; i++)
+		{
+			temp = new StringBuilder();
+			for (String word : words)
+			{
+				if (i <= word.length() - 1)
+				{
+					temp.append(word.charAt(i));
+				}
+				else
+				{
+					temp.append(" ");
+				}
+			}
+
+			while (temp.length() != 0 && temp.charAt(temp.length() - 1) == ' ')
+			{
+				temp.deleteCharAt(temp.length() - 1);
+			}
+
+			result.add(temp.toString());
+		}
+
+		return result;
+	}
+
+	public static void main(String[] args)
+	{
+		System.out.println(new PrintWordsVertically().printVertically("HOW ARE YOU"));
+	}
+}
