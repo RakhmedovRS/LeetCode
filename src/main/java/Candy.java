8683307056060b7d2e3dfc30e3952cd09e00fe65
rassoll <rassoll32@gmail.com>
2020-03-08 11:39:54 +0300

    Candy

---
+++ src/main/java/Candy.java
@@ -0,0 +1,58 @@
+import java.util.Arrays;
+
+/**
+ * Candy
+ * LeetCode 135
+ *
+ * @author RakhmedovRS
+ * @created 08-Mar-20
+ */
+public class Candy
+{
+	public int candy(int[] ratings)
+	{
+		int[] memo = new int[ratings.length];
+		Arrays.fill(memo, 1);
+
+		for (int i = 1; i < ratings.length - 2; i++)
+		{
+			int l = 0;
+			int r = 0;
+
+			if (ratings[i] > ratings[i - 1])
+			{
+				l = memo[i - 1];
+			}
+
+			if (ratings[i] > ratings[i + 1])
+			{
+				r = memo[i + 1];
+			}
+
+			memo[i] = Math.max(l, r) + 1;
+		}
+
+		for (int i = 1; i < ratings.length; i++)
+		{
+			if (ratings[i] > ratings[i - 1] && memo[i] <= memo[i - 1])
+			{
+				memo[i] = memo[i - 1] + 1;
+			}
+		}
+
+		for (int i = ratings.length - 2; i >= 0; i--)
+		{
+			if (ratings[i] > ratings[i + 1] && memo[i] <= memo[i + 1])
+			{
+				memo[i] = memo[i + 1] + 1;
+			}
+		}
+
+		return Arrays.stream(memo).sum();
+	}
+
+	public static void main(String[] args)
+	{
+		System.out.println(new Candy().candy(new int[]{1, 2, 2}));
+	}
+}
