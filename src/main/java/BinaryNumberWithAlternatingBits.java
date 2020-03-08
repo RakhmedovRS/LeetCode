bcfa4f463c4f5f7f651216cd29e7af3c5aae4547
rassoll <rassoll32@gmail.com>
2020-03-08 16:57:56 +0300

    Binary Number with Alternating Bits

---
+++ src/main/java/BinaryNumberWithAlternatingBits.java
@@ -0,0 +1,54 @@
+/**
+ * Binary Number with Alternating Bits
+ * LeetCode 693
+ *
+ * @author RakhmedovRS
+ * @created 08-Mar-20
+ */
+public class BinaryNumberWithAlternatingBits
+{
+	public boolean hasAlternatingBits(int n)
+	{
+		StringBuilder cache = new StringBuilder(32);
+		while (n > 0)
+		{
+			if ((n & 1) != 0)
+			{
+				cache.append('1');
+			}
+			else
+			{
+				cache.append('0');
+			}
+			n = n >> 1;
+		}
+
+		boolean meetFirstBite = false;
+		while (!meetFirstBite)
+		{
+			if (cache.charAt(cache.length() - 1) == '1')
+			{
+				meetFirstBite = true;
+			}
+			else
+			{
+				cache.deleteCharAt(cache.length() - 1);
+			}
+		}
+
+		for (int i = 1; i <= cache.length() - 1; i++)
+		{
+			if (cache.charAt(i) == cache.charAt(i - 1))
+			{
+				return false;
+			}
+		}
+
+		return true;
+	}
+
+	public static void main(String[] args)
+	{
+		System.out.println(new BinaryNumberWithAlternatingBits().hasAlternatingBits(4));
+	}
+}
