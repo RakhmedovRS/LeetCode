294fc7d8f977197c5eb40c9c5906d4d896b84610
rassoll <rassoll32@gmail.com>
2020-03-11 22:14:14 +0300

    Remove Duplicates from Sorted Array

---
+++ src/main/java/RemoveDuplicatesFromSortedArray.java
@@ -0,0 +1,26 @@
+/**
+ * @author RakhmedovRS
+ * @created 11-Mar-20
+ */
+@LeetCode(id = 26, name = "Remove Duplicates from Sorted Array", url = "https://leetcode.com/problems/remove-duplicates-from-sorted-array/")
+public class RemoveDuplicatesFromSortedArray
+{
+	public int removeDuplicates(int[] nums)
+	{
+		if (nums == null || nums.length == 0)
+		{
+			return 0;
+		}
+
+		int endSubArray = 0;
+		for (int i = 1; i < nums.length; i++)
+		{
+			if (nums[endSubArray] != nums[i])
+			{
+				nums[++endSubArray] = nums[i];
+			}
+		}
+
+		return endSubArray + 1;
+	}
+}
