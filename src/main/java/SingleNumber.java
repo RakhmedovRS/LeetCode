72f8af1102144fb4cd2a445da0a8e7e35717ec4d
rassoll <rassoll32@gmail.com>
2020-03-11 22:01:16 +0300

    Single Number

---
+++ src/main/java/SingleNumber.java
@@ -0,0 +1,17 @@
+/**
+ * @author RakhmedovRS
+ * @created 11-Mar-20
+ */
+@LeetCode(id = 136, name = "Single Number", url = "https://leetcode.com/problems/single-number/")
+public class SingleNumber
+{
+	public int singleNumber(int[] nums)
+	{
+		int a = 0;
+		for (int num : nums)
+		{
+			a ^= num;
+		}
+		return a;
+	}
+}
