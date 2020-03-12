c8801b378ee489fba358669488cbf7946a381787
rassoll <rassoll32@gmail.com>
2020-03-12 21:14:52 +0300

    Maximum Product Subarray

--- src/main/java/MaximumProductSubarray.java
+++ src/main/java/MaximumProductSubarray.java
@@ -1,10 +1,8 @@
 /**
- * Maximum Product Subarray
- * LeetCode 152
- *
  * @author RakhmedovRS
  * @created 07-Mar-20
  */
+@LeetCode(id = 152, name = "Maximum Product Subarray", url = "https://leetcode.com/problems/maximum-product-subarray/")
 public class MaximumProductSubarray
 {
 	public int maxProduct(int[] nums)
@@ -30,6 +28,34 @@ public class MaximumProductSubarray
 		}
 
 		return finalMax;
+	}
+
+	public int maxProduct1(int[] nums)
+	{
+		if (nums == null || nums.length == 0)
+		{
+			return 0;
+		}
+
+		int[] negative = new int[nums.length];
+		int[] positive = new int[nums.length];
+		negative[0] = nums[0];
+		positive[0] = nums[0];
+
+		int maxProduct = nums[0];
+		for (int i = 1; i < nums.length; i++)
+		{
+			if (nums[i] == 0)
+			{
+				negative[0] = 0;
+				positive[0] = 0;
+			}
+			positive[i] = Math.max(nums[i], Math.max(nums[i] * positive[i - 1], nums[i] * negative[i - 1]));
+			negative[i] = Math.min(nums[i], Math.min(nums[i] * positive[i - 1], nums[i] * negative[i - 1]));
+			maxProduct = Math.max(maxProduct, positive[i]);
+		}
+
+		return maxProduct;
 	}
 
 	public static void main(String[] args)
