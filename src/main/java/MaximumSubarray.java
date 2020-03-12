6325d6706531e54a64f781d882f8dbec20ef6605
rassoll <rassoll32@gmail.com>
2020-03-12 21:10:21 +0300

    Maximum Subarray

--- src/main/java/MaximumSubarray.java
+++ src/main/java/MaximumSubarray.java
@@ -1,13 +1,29 @@
 /**
- * Maximum Subarray
- * LeetCode 53
- *
  * @author RakhmedovRS
  * @created 07-Mar-20
  */
+@LeetCode(id = 53, name = "Maximum Subarray", url = "https://leetcode.com/problems/maximum-subarray/")
 public class MaximumSubarray
 {
 	public int maxSubArray(int[] nums)
+	{
+		if (nums == null || nums.length == 0)
+		{
+			return 0;
+		}
+
+		int currentMax = nums[0];
+		int totalMax = nums[0];
+		for (int i = 1; i < nums.length; i++)
+		{
+			currentMax = Math.max(currentMax + nums[i], nums[i]);
+			totalMax = Math.max(totalMax, currentMax);
+		}
+
+		return totalMax;
+	}
+
+	public int maxSubArray1(int[] nums)
 	{
 		if (nums == null || nums.length == 0)
 		{
