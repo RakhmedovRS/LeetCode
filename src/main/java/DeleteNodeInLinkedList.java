c2ea4cbfc880e7c1fc68bc5e2e0a4aca3502757c
rassoll <rassoll32@gmail.com>
2020-03-13 21:04:04 +0300

    Delete Node in a Linked List

---
+++ src/main/java/DeleteNodeInLinkedList.java
@@ -0,0 +1,33 @@
+/**
+ * @author RakhmedovRS
+ * @created 13-Mar-20
+ */
+@LeetCode(id = 237, name = "Delete Node in a Linked List", url = "https://leetcode.com/problems/delete-node-in-a-linked-list/")
+public class DeleteNodeInLinkedList
+{
+	public class ListNode
+	{
+		int val;
+		ListNode next;
+
+		ListNode(int x){ val = x; }
+	}
+
+	public void deleteNode(ListNode node)
+	{
+		if (node.next == null)
+		{
+			return;
+		}
+
+		ListNode prev = node;
+		while (node.next != null)
+		{
+			node.val = node.next.val;
+
+			prev = node;
+			node = node.next;
+		}
+		prev.next = null;
+	}
+}
