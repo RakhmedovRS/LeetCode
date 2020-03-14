121755a93b1a20ba5f8a0070c6ef3e6ce32d55c2
rassoll <rassoll32@gmail.com>
2020-03-14 22:16:29 +0300

    First Unique Character in a String

---
+++ src/main/java/FirstUniqueCharacterInString.java
@@ -0,0 +1,30 @@
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author RakhmedovRS
+ * @created 14-Mar-20
+ */
+@LeetCode(id = 387, name = "First Unique Character in a String", url = "https://leetcode.com/problems/first-unique-character-in-a-string/")
+public class FirstUniqueCharacterInString
+{
+	public int firstUniqChar(String s)
+	{
+		char[] values = s.toCharArray();
+		Map<Character, Integer> cache = new HashMap<>();
+		for (char ch : values)
+		{
+			cache.put(ch, cache.getOrDefault(ch, 0) + 1);
+		}
+
+		for (int i = 0; i < values.length; i++)
+		{
+			if (cache.get(values[i]) == 1)
+			{
+				return i;
+			}
+		}
+
+		return -1;
+	}
+}
