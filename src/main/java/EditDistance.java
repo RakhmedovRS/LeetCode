45a6730df68928163fb836018828cd6ee18aca85
rassoll <rassoll32@gmail.com>
2020-03-14 15:36:24 +0300

    Edit Distance

--- src/main/java/EditDistance.java
+++ src/main/java/EditDistance.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 /**
  * @author RakhmedovRS
  * @created 14-Mar-20
@@ -41,10 +43,57 @@ public class EditDistance
 		return memo[rowsCount - 1][columnsCount - 1];
 	}
 
+	public int minDistance1(String word1, String word2)
+	{
+		int[][] memo = new int[word1.length()][word2.length()];
+		for (int[] row : memo)
+		{
+			Arrays.fill(row, -1);
+		}
+
+		return levenshteinDistance(word1, word2, word1.length() - 1, word2.length() - 1, memo);
+	}
+
+	private int levenshteinDistance(String firstWord, String secondWord, int firstIndex, int secondIndex, int[][] memo)
+	{
+		if (firstIndex < 0)
+		{
+			return secondIndex + 1;
+		}
+		else if (secondIndex < 0)
+		{
+			return firstIndex + 1;
+		}
+
+		if (memo[firstIndex][secondIndex] != -1)
+		{
+			return memo[firstIndex][secondIndex];
+		}
+
+		if (firstWord.charAt(firstIndex) == secondWord.charAt(secondIndex))
+		{
+			memo[firstIndex][secondIndex] = levenshteinDistance(firstWord, secondWord, firstIndex - 1, secondIndex - 1, memo);
+		}
+		else
+		{
+			int delete = levenshteinDistance(firstWord, secondWord, firstIndex - 1, secondIndex, memo);
+			int insert = levenshteinDistance(firstWord, secondWord, firstIndex, secondIndex - 1, memo);
+			int replace = levenshteinDistance(firstWord, secondWord, firstIndex - 1, secondIndex - 1, memo);
+
+			memo[firstIndex][secondIndex] = Math.min(delete, Math.min(insert, replace)) + 1;
+		}
+
+		return memo[firstIndex][secondIndex];
+	}
+
 	public static void main(String[] args)
 	{
 		System.out.println(new EditDistance().minDistance("", "a"));
 		System.out.println(new EditDistance().minDistance("horse", "ros"));
 		System.out.println(new EditDistance().minDistance("abba", "abba"));
+
+		System.out.println(new EditDistance().minDistance1("", "a"));
+		System.out.println(new EditDistance().minDistance1("horse", "ros"));
+		System.out.println(new EditDistance().minDistance1("abba", "abba"));
 	}
 }
