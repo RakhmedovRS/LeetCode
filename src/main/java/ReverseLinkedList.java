7e27c88e99a62871c01947871ccf9c4f5d8c103f
rassoll <rassoll32@gmail.com>
2020-03-14 21:16:30 +0300

    Reverse Linked List

---
+++ src/main/java/ReverseLinkedList.java
@@ -0,0 +1,48 @@
+/**
+ * @author RakhmedovRS
+ * @created 14-Mar-20
+ */
+@LeetCode(id = 206, name = "Reverse Linked List", url ="https://leetcode.com/problems/reverse-linked-list/")
+public class ReverseLinkedList
+{
+	public static class ListNode
+	{
+		int val;
+		ListNode next;
+
+		ListNode(int x){ val = x; }
+	}
+
+	public ListNode reverseList(ListNode head)
+	{
+		if (head == null || head.next == null)
+		{
+			return head;
+		}
+
+		ListNode prev = null;
+		ListNode next = head.next;
+		while (next != null)
+		{
+			head.next = prev;
+			prev = head;
+			head = next;
+			next = next.next;
+		}
+
+		head.next = prev;
+
+		return head;
+	}
+
+	public static void main(String[] args)
+	{
+		ListNode head = new ListNode(1);
+		head.next = new ListNode(2);
+		head.next.next = new ListNode(3);
+		head.next.next.next = new ListNode(4);
+		head.next.next.next.next = new ListNode(5);
+
+		new ReverseLinkedList().reverseList(head);
+	}
+}
