7a3dd275aed3867f83c485eb115733eadca78e29
rassoll <rassoll32@gmail.com>
2020-03-16 19:20:38 +0300

    Gas Station

---
+++ src/main/java/GasStation.java
@@ -0,0 +1,40 @@
+import common.LeetCode;
+
+/**
+ * @author RakhmedovRS
+ * @created 16-Mar-20
+ */
+@LeetCode(id = 134, name = "Gas Station", url = "https://leetcode.com/problems/gas-station/")
+public class GasStation
+{
+	public int canCompleteCircuit(int[] gas, int[] cost)
+	{
+		int tank = 0;
+		for (int i = 0; i < gas.length; i++)
+		{
+			tank += gas[i] - cost[i];
+		}
+		if (tank < 0)
+		{
+			return -1;
+		}
+
+		int start = 0;
+		int accumulate = 0;
+		for (int i = 0; i < gas.length; i++)
+		{
+			int curGain = gas[i] - cost[i];
+			if (accumulate + curGain < 0)
+			{
+				start = i + 1;
+				accumulate = 0;
+			}
+			else
+			{
+				accumulate += curGain;
+			}
+		}
+
+		return start;
+	}
+}
