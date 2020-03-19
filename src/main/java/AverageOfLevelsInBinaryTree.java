c4806cfb2f5ac79e201fddfa9f42703c7ff4de37
rassoll <rassoll32@gmail.com>
2020-03-19 21:54:24 +0300

    Average of Levels in Binary Tree

---
+++ src/main/java/AverageOfLevelsInBinaryTree.java
@@ -0,0 +1,61 @@
+import common.LeetCode;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+/**
+ * @author RakhmedovRS
+ * @created 19-Mar-20
+ */
+@LeetCode(id = 637, name = "Average of Levels in Binary Tree", url = "https://leetcode.com/problems/average-of-levels-in-binary-tree/")
+public class AverageOfLevelsInBinaryTree
+{
+	public class TreeNode
+	{
+		int val;
+		TreeNode left;
+		TreeNode right;
+
+		TreeNode(int x){ val = x; }
+	}
+
+	public List<Double> averageOfLevels(TreeNode root)
+	{
+		if (root == null)
+		{
+			return new ArrayList<>();
+		}
+
+		Queue<TreeNode> queue = new LinkedList<>();
+		queue.add(root);
+		List<Double> values = new LinkedList<>();
+		int size;
+		TreeNode temp;
+		double sum;
+		while (!queue.isEmpty())
+		{
+			size = queue.size();
+			sum = 0D;
+			for (int i = 0; i < size; i++)
+			{
+				temp = queue.poll();
+				sum += temp.val;
+
+				if (temp.left != null)
+				{
+					queue.offer(temp.left);
+				}
+
+				if (temp.right != null)
+				{
+					queue.offer(temp.right);
+				}
+			}
+			values.add(sum / size);
+		}
+
+		return values;
+	}
+}
