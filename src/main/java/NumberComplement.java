6c65e00769fa510beb0b95f2e6852d476324c75e
rassoll <rassoll32@gmail.com>
2020-03-22 13:21:50 +0300

    Number Complement

---
+++ src/main/java/NumberComplement.java
@@ -0,0 +1,27 @@
+import common.LeetCode;
+
+/**
+ * @author RakhmedovRS
+ * @created 22-Mar-20
+ */
+@LeetCode(id = 476, name = "Number Complement", url = "https://leetcode.com/problems/number-complement/")
+public class NumberComplement
+{
+	public int findComplement(int num)
+	{
+		int i = 1;
+		int mask = 1;
+		while (mask <= num && i++ < 32)
+		{
+			num = num ^ mask;
+			mask <<= 1;
+		}
+		return num;
+	}
+
+	public static void main(String[] args)
+	{
+		System.out.println(new NumberComplement().findComplement(5));
+		System.out.println(new NumberComplement().findComplement(1));
+	}
+}
