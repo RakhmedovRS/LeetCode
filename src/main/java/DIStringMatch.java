810f251fe8155bad438bfa4cdc8de32fbcd5c757
rassoll <rassoll32@gmail.com>
2020-03-30 21:33:21 +0300

    DI String Match

---
+++ src/main/java/DIStringMatch.java
@@ -0,0 +1,36 @@
+import common.LeetCode;
+
+/**
+ * @author RakhmedovRS
+ * @created 30-Mar-20
+ */
+@LeetCode(id = 942, name = "DI String Match", url = "https://leetcode.com/problems/di-string-match/")
+public class DIStringMatch
+{
+	public int[] diStringMatch(String S)
+	{
+		if (S == null || S.length() == 0)
+		{
+			return new int[0];
+		}
+
+		int min = 0;
+		int max = S.length();
+
+		int[] answer = new int[S.length() + 1];
+		for (int i = 0; i < S.length(); i++)
+		{
+			if (S.charAt(i) == 'D')
+			{
+				answer[i] = max--;
+			}
+			else
+			{
+				answer[i] = min++;
+			}
+		}
+		answer[S.length()] = min;
+
+		return answer;
+	}
+}
