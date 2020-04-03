a754509c075e280fbba843f3d2423a419af10903
rassoll <rassoll32@gmail.com>
2020-04-03 12:03:39 +0300

    Maximum Subarray

--- src/main/java/MaximumSubarray.java
+++ src/main/java/MaximumSubarray.java
@@ -14,15 +14,49 @@ public class MaximumSubarray
 			return 0;
 		}
 
-		int currentMax = nums[0];
-		int totalMax = nums[0];
-		for (int i = 1; i < nums.length; i++)
+		return maxSubArray(nums, 0, nums.length - 1);
+	}
+
+	public int maxSubArray(int[] nums, int left, int right)
+	{
+		if (left == right)
 		{
-			currentMax = Math.max(currentMax + nums[i], nums[i]);
-			totalMax = Math.max(totalMax, currentMax);
+			return nums[left];
 		}
 
-		return totalMax;
+		if (left > right)
+		{
+			return Integer.MIN_VALUE;
+		}
+
+		int center = left + (right - left) / 2;
+		int leftMax = maxSubArray(nums, left, center);
+		int rightMax = maxSubArray(nums, center + 1, right);
+		int centerMax = maxCrossSubArray(nums, left, center, right);
+
+		return Math.max(centerMax, Math.max(leftMax, rightMax));
+	}
+
+	private int maxCrossSubArray(int[] nums, int left, int center, int right)
+	{
+		int leftMax = Integer.MIN_VALUE;
+		int rightMax = Integer.MIN_VALUE;
+
+		int sum = 0;
+		for (int i = center; i >= left; i--)
+		{
+			sum += nums[i];
+			leftMax = Math.max(leftMax, sum);
+		}
+
+		sum = 0;
+		for (int i = center + 1; i <= right; i++)
+		{
+			sum += nums[i];
+			rightMax = Math.max(rightMax, sum);
+		}
+
+		return leftMax + rightMax;
 	}
 
 	public int maxSubArray1(int[] nums)
@@ -41,5 +75,10 @@ public class MaximumSubarray
 		}
 
 		return totalMax;
+	}
+
+	public static void main(String[] args)
+	{
+		System.out.println(new MaximumSubarray().maxSubArray(new int[]{-2, 1, -3, 4, -1, 2, 1, -5, 4}));
 	}
 }
