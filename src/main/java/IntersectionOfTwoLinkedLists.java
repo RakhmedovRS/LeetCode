019c35cd5467f5dec895a3ba0804372db722adec
rassoll <rassoll32@gmail.com>
2020-04-07 18:12:30 +0300

    Intersection of Two Linked Lists

---
+++ src/main/java/IntersectionOfTwoLinkedLists.java
@@ -0,0 +1,54 @@
+import common.LeetCode;
+
+/**
+ * @author RakhmedovRS
+ * @created 07-Apr-20
+ */
+@LeetCode(id = 160, name = "Intersection of Two Linked Lists", url = "https://leetcode.com/problems/intersection-of-two-linked-lists/")
+public class IntersectionOfTwoLinkedLists
+{
+	public class ListNode
+	{
+		int val;
+		ListNode next;
+
+		ListNode(int x)
+		{
+			val = x;
+			next = null;
+		}
+	}
+
+	public ListNode getIntersectionNode(ListNode headA, ListNode headB)
+	{
+		if (headA == null || headB == null)
+		{
+			return null;
+		}
+
+		ListNode first = headA;
+		ListNode second = headB;
+		boolean headASwiched = false;
+		boolean headBSwiched = false;
+		while (headA != null && headB != null)
+		{
+			if (headA == headB)
+			{
+				return headA;
+			}
+			headA = headA.next;
+			headB = headB.next;
+			if (headA == null && !headASwiched)
+			{
+				headA = second;
+				headASwiched = true;
+			}
+			if (headB == null && !headBSwiched)
+			{
+				headB = first;
+				headBSwiched = true;
+			}
+		}
+		return null;
+	}
+}
