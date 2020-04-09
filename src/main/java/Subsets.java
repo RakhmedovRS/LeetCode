8242568f8d22fb04fd16d937a8bded3b008d6361
rassoll <rassoll32@gmail.com>
2020-04-09 20:14:12 +0300

    Subsets

---
+++ src/main/java/Subsets.java
@@ -0,0 +1,35 @@
+import common.LeetCode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author RakhmedovRS
+ * @created 09-Apr-20
+ */
+@LeetCode(id = 78, name = "Subsets", url = "https://leetcode.com/problems/subsets/")
+public class Subsets
+{
+	public List<List<Integer>> subsets(int[] nums)
+	{
+		List<List<Integer>> result = new ArrayList<>();
+		if (nums == null || nums.length == 0)
+		{
+			return result;
+		}
+		result.add(new ArrayList<>());
+
+		for (int i = nums.length - 1; i >= 0; i--)
+		{
+			int size = result.size();
+			for (int pos = 0; pos < size; pos++)
+			{
+				List<Integer> newSet = new ArrayList<>(result.get(pos));
+				newSet.add(nums[i]);
+				result.add(newSet);
+			}
+		}
+
+		return result;
+	}
+}
