ed442df3b0512a060e8cc04109f155c1810ad1df
rassoll <rassoll32@gmail.com>
2020-04-10 21:50:24 +0300

    Swap Nodes in Pairs

---
+++ src/main/java/SwapNodesInPairs.java
@@ -0,0 +1,43 @@
+import common.LeetCode;
+
+/**
+ * @author RakhmedovRS
+ * @created 10-Apr-20
+ */
+@LeetCode(id = 24, name = "Swap Nodes in Pairs", url = "https://leetcode.com/problems/swap-nodes-in-pairs/")
+public class SwapNodesInPairs
+{
+	public static class ListNode
+	{
+		int val;
+		ListNode next;
+
+		ListNode(int x){ val = x; }
+	}
+
+	public ListNode swapPairs(ListNode head)
+	{
+		ListNode dummy = new ListNode(0);
+		dummy.next = head;
+		ListNode current = dummy;
+		while (current.next != null && current.next.next != null)
+		{
+			ListNode first = current.next;
+			ListNode second = current.next.next;
+			first.next = second.next;
+			current.next = second;
+			current.next.next = first;
+			current = current.next.next;
+		}
+		return dummy.next;
+	}
+
+	public static void main(String[] args)
+	{
+		ListNode root = new ListNode(1);
+		root.next = new ListNode(2);
+		root.next.next = new ListNode(3);
+		root.next.next.next = new ListNode(4);
+		new SwapNodesInPairs().swapPairs(root);
+	}
+}
