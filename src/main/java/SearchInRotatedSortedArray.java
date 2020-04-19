67c6e572e57b55c7fdebab47d0e8dc9155242e63
rassoll <rassoll32@gmail.com>
2020-04-19 13:46:11 +0300

    Search in Rotated Sorted Array is updated

--- src/main/java/SearchInRotatedSortedArray.java
+++ src/main/java/SearchInRotatedSortedArray.java
@@ -4,7 +4,7 @@ import common.LeetCode;
  * @author RakhmedovRS
  * @created 10-Apr-20
  */
-@LeetCode(id= 33, name = "Search in Rotated Sorted Array", url = "https://leetcode.com/problems/search-in-rotated-sorted-array/")
+@LeetCode(id = 33, name = "Search in Rotated Sorted Array", url = "https://leetcode.com/problems/search-in-rotated-sorted-array/")
 public class SearchInRotatedSortedArray
 {
 	public int search(int[] nums, int target)
@@ -16,9 +16,10 @@ public class SearchInRotatedSortedArray
 
 		int left = 0;
 		int right = nums.length - 1;
+		int middle;
 		while (left < right)
 		{
-			int middle = left + (right - left) / 2;
+			middle = left + (right - left) / 2;
 			if (nums[middle] > nums[right])
 			{
 				left = middle + 1;
@@ -29,36 +30,41 @@ public class SearchInRotatedSortedArray
 			}
 		}
 
-		int start = left;
-		left = 0;
-		right = nums.length - 1;
-
-		if (target >= nums[start] && target <= nums[right])
+		if (target >= nums[left] && target <= nums[nums.length - 1])
 		{
-			left = start;
+			right = nums.length - 1;
 		}
 		else
 		{
-			right = start;
+			right = left - 1;
+			left = 0;
 		}
 
 		while (left <= right)
 		{
-			int middle = left + (right - left) / 2;
+			middle = left + (right - left) / 2;
 			if (nums[middle] == target)
 			{
 				return middle;
 			}
-			else if(nums[middle] < target)
+			else if (nums[middle] > target)
 			{
-				left = middle + 1;
+				right = middle - 1;
 			}
 			else
 			{
-				right = middle - 1;
+				left = middle + 1;
 			}
 		}
 
 		return -1;
+	}
+
+	public static void main(String[] args)
+	{
+		System.out.println(new SearchInRotatedSortedArray().search(new int[]{5, 1, 3}, 1));
+		System.out.println(new SearchInRotatedSortedArray().search(new int[]{0, 1, 2, 4, 5, 6, 7}, 0));
+		System.out.println(new SearchInRotatedSortedArray().search(new int[]{4, 5, 6, 7, 0, 1, 2}, 0));
+		System.out.println(new SearchInRotatedSortedArray().search(new int[]{4, 5, 6, 7, 0, 1, 2}, 3));
 	}
 }
