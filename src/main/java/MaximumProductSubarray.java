81b526be6a329522cfddc16ecb69919998b093b6
rassoll <rassoll32@gmail.com>
2020-04-28 22:33:47 +0300

    Maximum Product Subarray is updated

--- src/main/java/MaximumProductSubarray.java
+++ src/main/java/MaximumProductSubarray.java
@@ -9,27 +9,30 @@ public class MaximumProductSubarray
 {
 	public int maxProduct(int[] nums)
 	{
+		int maxProduct = 0;
 		if (nums == null || nums.length == 0)
 		{
-			return 0;
+			return maxProduct;
 		}
 
-		int currentMin = nums[0];
-		int currentMax = nums[0];
-		int finalMax = nums[0];
+		int[] positive = new int[nums.length];
+		int[] negative = new int[nums.length];
+		positive[0] = nums[0];
+		negative[0] = nums[0];
+		maxProduct = nums[0];
+
 		for (int i = 1; i < nums.length; i++)
 		{
-			int temp = currentMax;
-			currentMax = Math.max(nums[i], Math.max(currentMax * nums[i], currentMin * nums[i]));
-			currentMin = Math.min(nums[i], Math.min(temp * nums[i], currentMin * nums[i]));
+			int pos = nums[i] * positive[i - 1];
+			int neg = nums[i] * negative[i - 1];
 
-			if (currentMax > finalMax)
-			{
-				finalMax = currentMax;
-			}
+			positive[i] = Math.max(nums[i], Math.max(pos, neg));
+			negative[i] = Math.min(nums[i], Math.min(pos, neg));
+
+			maxProduct = Math.max(maxProduct, positive[i]);
 		}
 
-		return finalMax;
+		return maxProduct;
 	}
 
 	public int maxProduct1(int[] nums)
