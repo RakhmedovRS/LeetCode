72f01a96b65049302f1e4d6f09046375036de6b3
rassoll <rassoll32@gmail.com>
2020-05-01 14:16:55 +0300

    Lowest Common Ancestor of a Binary Tree

---
+++ src/main/java/LowestCommonAncestorOfBinaryTree.java
@@ -0,0 +1,46 @@
+import common.LeetCode;
+
+/**
+ * @author RakhmedovRS
+ * @created 01-May-20
+ */
+@LeetCode(id = 236, name = "Lowest Common Ancestor of a Binary Tree", url = "https://leetcode.com/problems/lowest-common-ancestor-of-a-binary-tree/")
+public class LowestCommonAncestorOfBinaryTree
+{
+	public class TreeNode
+	{
+		int val;
+		TreeNode left;
+		TreeNode right;
+
+		TreeNode(int x){ val = x; }
+	}
+
+	public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q)
+	{
+		if (root == null)
+		{
+			return null;
+		}
+
+		if (root.val == p.val || root.val == q.val)
+		{
+			return root;
+		}
+
+		TreeNode left = lowestCommonAncestor(root.left, p, q);
+		TreeNode right = lowestCommonAncestor(root.right, p, q);
+		if (right != null && left != null)
+		{
+			return root;
+		}
+		else if (left != null)
+		{
+			return left;
+		}
+		else
+		{
+			return right;
+		}
+	}
+}
