eb7cfc5901dbc9ae3802a0e1f13e6b9628d73a77
rassoll <rassoll32@gmail.com>
2020-05-04 14:51:09 +0300

    K-diff Pairs in an Array is updated

--- src/main/java/KdiffPairsInArray.java
+++ src/main/java/KdiffPairsInArray.java
@@ -1,6 +1,5 @@
 import common.LeetCode;
 
-import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -13,28 +12,50 @@ public class KdiffPairsInArray
 {
 	public int findPairs(int[] nums, int k)
 	{
-		if (nums == null || nums.length <= 1)
+		if (nums == null || nums.length <= 1 || k < 0)
 		{
 			return 0;
 		}
 
-		Arrays.sort(nums);
-		Map<Integer, Integer> memo = new HashMap<>();
-		for (int i = 0; i < nums.length - 1; i++)
+		int pairs = 0;
+		Map<Long, Integer> memo = new HashMap<>();
+		for (int i = 0; i < nums.length; i++)
 		{
-			for (int j = i + 1; j < nums.length; j++)
+			memo.put((long) nums[i], memo.getOrDefault((long) nums[i], 0) + 1);
+		}
+
+		if (k == 0)
+		{
+			for (Integer value : memo.values())
 			{
-				if (Math.abs(nums[i] - nums[j]) == k)
+				if (value >= 2)
 				{
-					Integer J = memo.get(nums[i]);
-					if (J == null || J != nums[j])
-					{
-						memo.put(nums[i], nums[j]);
-					}
+					pairs++;
+				}
+			}
+		}
+		else
+		{
+			for (int i = nums.length - 1; i >= 0; i--)
+			{
+				Long target = ((long) nums[i]) + k;
+				if (memo.containsKey(target))
+				{
+					pairs++;
+					memo.remove(target);
 				}
 			}
 		}
 
-		return memo.values().size();
+		return pairs;
+	}
+
+	public static void main(String[] args)
+	{
+		System.out.println(new KdiffPairsInArray().findPairs(new int[]{1, 2, 3, 4, 5}, -1));
+		System.out.println(new KdiffPairsInArray().findPairs(new int[]{1, 2, 3, 4, 5}, 4));
+		System.out.println(new KdiffPairsInArray().findPairs(new int[]{1, 3, 1, 5, 4}, 0));
+		System.out.println(new KdiffPairsInArray().findPairs(new int[]{3, 1, 4, 1, 5}, 2));
+		System.out.println(new KdiffPairsInArray().findPairs(new int[]{1, 2, 3, 4, 5}, 1));
 	}
 }
