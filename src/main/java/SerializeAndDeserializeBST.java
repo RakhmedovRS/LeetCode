ece0152312154aacc8978abf249d434bf457ace0
rassoll <rassoll32@gmail.com>
2020-05-06 20:37:45 +0300

    Serialize and Deserialize BST

---
+++ src/main/java/SerializeAndDeserializeBST.java
@@ -0,0 +1,65 @@
+import common.LeetCode;
+
+/**
+ * @author RakhmedovRS
+ * @created 06-May-20
+ */
+@LeetCode(id = 449, name = "Serialize and Deserialize BST", url = "https://leetcode.com/problems/serialize-and-deserialize-bst/")
+public class SerializeAndDeserializeBST
+{
+	public class TreeNode
+	{
+		int val;
+		TreeNode left;
+		TreeNode right;
+
+		TreeNode(int x){ val = x; }
+	}
+
+	public String serialize(TreeNode root)
+	{
+		if (root == null)
+		{
+			return "null";
+		}
+
+		StringBuilder stringBuilder = new StringBuilder();
+		serialize(root, stringBuilder);
+		return stringBuilder.toString();
+	}
+
+	public void serialize(TreeNode root, StringBuilder stringBuilder)
+	{
+		if (root == null)
+		{
+			stringBuilder.append("null");
+			return;
+		}
+
+		stringBuilder.append(root.val);
+		stringBuilder.append(",");
+		serialize(root.left, stringBuilder);
+		stringBuilder.append(",");
+		serialize(root.right, stringBuilder);
+	}
+
+	public TreeNode deserialize(String data)
+	{
+		String[] nodes = data.split(",");
+		return buildTree(nodes, new int[]{0});
+	}
+
+	private TreeNode buildTree(String[] nodes, int[] index)
+	{
+		if ("null".equals(nodes[index[0]]))
+		{
+			index[0]++;
+			return null;
+		}
+
+		TreeNode treeNode = new TreeNode(Integer.parseInt(nodes[index[0]++]));
+		treeNode.left = buildTree(nodes, index);
+		treeNode.right = buildTree(nodes, index);
+		return treeNode;
+	}
+}
