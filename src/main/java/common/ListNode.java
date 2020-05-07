676b80d0e8d5323a2bd0ec988fb910b18d3533a1
rassoll <rassoll32@gmail.com>
2020-05-07 21:13:15 +0300

    Remove Linked List Elements

---
+++ src/main/java/common/ListNode.java
@@ -0,0 +1,21 @@
+package common;
+
+/**
+ * @author RakhmedovRS
+ * @created 07-May-20
+ */
+public class ListNode
+{
+	public int val;
+	public ListNode next;
+
+	public ListNode(){}
+
+	public ListNode(int val){ this.val = val; }
+
+	public ListNode(int val, ListNode next)
+	{
+		this.val = val;
+		this.next = next;
+	}
+}
