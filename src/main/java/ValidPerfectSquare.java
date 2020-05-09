b9ef20efe5e99f54221b1992ed0bea1352666557
rassoll <rassoll32@gmail.com>
2020-05-09 14:44:05 +0300

    Valid Perfect Square

---
+++ src/main/java/ValidPerfectSquare.java
@@ -0,0 +1,45 @@
+import common.LeetCode;
+
+/**
+ * @author RakhmedovRS
+ * @created 09-May-20
+ */
+@LeetCode(id = 367, name = "Valid Perfect Square", url = "https://leetcode.com/problems/valid-perfect-square/")
+public class ValidPerfectSquare
+{
+	public boolean isPerfectSquare(int num)
+	{
+		if (num == 1)
+		{
+			return true;
+		}
+		else if (num == 2 || num == 3)
+		{
+			return false;
+		}
+
+		long left = 1;
+		long right = num / 2;
+		long middle;
+		long square;
+		while (left <= right)
+		{
+			middle = left + (right - left) / 2;
+			square = middle * middle;
+			if (square == num)
+			{
+				return true;
+			}
+			else if (square > num)
+			{
+				right = middle - 1;
+			}
+			else
+			{
+				left = middle + 1;
+			}
+		}
+
+		return false;
+	}
+}
