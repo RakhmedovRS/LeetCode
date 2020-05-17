4125df912a4675b3b898318424c76455b03af59c
rassoll <rassoll32@gmail.com>
2020-05-17 15:03:13 +0300

    Shifting Letters

---
+++ src/main/java/ShiftingLetters.java
@@ -0,0 +1,32 @@
+import common.LeetCode;
+
+/**
+ * @author RakhmedovRS
+ * @created 17-May-20
+ */
+@LeetCode(id = 848, name = "Shifting Letters", url = "https://leetcode.com/problems/shifting-letters/")
+public class ShiftingLetters
+{
+	public String shiftingLetters(String S, int[] shifts)
+	{
+		char[] values = S.toCharArray();
+
+		for (int i = shifts.length - 2; i >= 0; i--)
+		{
+			shifts[i] += shifts[i + 1];
+			shifts[i] %= 26;
+		}
+
+		for (int i = 0; i < shifts.length; i++)
+		{
+			values[i] = (char) (((values[i] - 'a') + shifts[i]) % 26 + 'a');
+		}
+
+		return String.valueOf(values);
+	}
+
+	public static void main(String[] args)
+	{
+		System.out.println(new ShiftingLetters().shiftingLetters("abc", new int[]{3, 5, 9}));
+	}
+}
