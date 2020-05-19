a0cd7569dc98fbb7b6d881209f9db0ba3e5155a2
rassoll <rassoll32@gmail.com>
2020-05-19 20:00:31 +0300

    Online Stock Span

---
+++ src/main/java/OnlineStockSpan.java
@@ -0,0 +1,37 @@
+import common.LeetCode;
+
+import java.util.Stack;
+
+/**
+ * @author RakhmedovRS
+ * @created 19-May-20
+ */
+@LeetCode(id = 901, name = "Online Stock Span", url = "https://leetcode.com/problems/online-stock-span/")
+public class OnlineStockSpan
+{
+	class StockSpanner
+	{
+
+		Stack<Integer> prices, weights;
+
+		public StockSpanner()
+		{
+			prices = new Stack();
+			weights = new Stack();
+		}
+
+		public int next(int price)
+		{
+			int w = 1;
+			while (!prices.isEmpty() && prices.peek() <= price)
+			{
+				prices.pop();
+				w += weights.pop();
+			}
+
+			prices.push(price);
+			weights.push(w);
+			return w;
+		}
+	}
+}
