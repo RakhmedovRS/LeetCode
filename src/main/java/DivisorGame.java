93c22f2b8dbcdf6e53317d4b2749079f676d474e
rassoll <rassoll32@gmail.com>
2020-05-20 21:31:39 +0300

    Divisor Game

---
+++ src/main/java/DivisorGame.java
@@ -0,0 +1,14 @@
+import common.LeetCode;
+
+/**
+ * @author RakhmedovRS
+ * @created 20-May-20
+ */
+@LeetCode(id = 1025, name = "Divisor Game", url = "https://leetcode.com/problems/divisor-game/")
+public class DivisorGame
+{
+	public boolean divisorGame(int N)
+	{
+		return N % 2 == 0;
+	}
+}
