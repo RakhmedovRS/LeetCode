0fcf55bf01d8975565f7e1b6e6087195d65cf80f
rassoll <rassoll32@gmail.com>
2020-05-29 21:08:24 +0300

    Subtree of Another Tree

---
+++ src/main/java/SubtreeOfAnotherTree.java
@@ -0,0 +1,44 @@
+import common.LeetCode;
+import common.TreeNode;
+
+/**
+ * @author RakhmedovRS
+ * @created 29-May-20
+ */
+@LeetCode(id = 572, name = "Subtree of Another Tree", url = "https://leetcode.com/problems/subtree-of-another-tree/")
+public class SubtreeOfAnotherTree
+{
+	public boolean isSubtree(TreeNode s, TreeNode t)
+	{
+		if (s == null && t == null)
+		{
+			return true;
+		}
+		else if (s == null || t == null)
+		{
+			return false;
+		}
+
+		boolean sub = false;
+		if (s.val == t.val)
+		{
+			sub = equals(s.left, t.left) && equals(s.right, t.right);
+		}
+
+		return sub || (isSubtree(s.left, t) || isSubtree(s.right, t));
+	}
+
+	private boolean equals(TreeNode s, TreeNode t)
+	{
+		if (s == null && t == null)
+		{
+			return true;
+		}
+		else if (s == null || t == null)
+		{
+			return false;
+		}
+
+		return s.val == t.val && equals(s.left, t.left) && equals(s.right, t.right);
+	}
+}
