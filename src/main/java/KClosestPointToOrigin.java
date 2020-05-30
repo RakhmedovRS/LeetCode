e46f61cd7d51bb3bef0302f7067437c584a8cbd0
rassoll <rassoll32@gmail.com>
2020-05-30 14:24:08 +0300

    K Closest Points to Origin

---
+++ src/main/java/KClosestPointToOrigin.java
@@ -0,0 +1,35 @@
+import common.LeetCode;
+
+import java.util.PriorityQueue;
+
+/**
+ * @author RakhmedovRS
+ * @created 30-May-20
+ */
+@LeetCode(id = 973, name = "K Closest Points to Origin", url = "https://leetcode.com/problems/k-closest-points-to-origin/")
+public class KClosestPointToOrigin
+{
+	public int[][] kClosest(int[][] points, int K)
+	{
+		int[][] answer = new int[K][];
+		int[] pows = new int[points.length];
+		for (int i = 0; i < points.length; i++)
+		{
+			pows[i] = points[i][0] * points[i][0] + points[i][1] * points[i][1];
+		}
+
+		PriorityQueue<Integer> minHeap = new PriorityQueue<>();
+		for (int i = 0; i < points.length; i++)
+		{
+			minHeap.add(i);
+		}
+
+		int i = 0;
+		while (i < K)
+		{
+			answer[i++] = points[minHeap.remove()];
+		}
+
+		return answer;
+	}
+}
