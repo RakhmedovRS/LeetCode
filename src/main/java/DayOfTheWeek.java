23222bd6e73b471c8b4c3e5e82dc773664303c74
rassoll <rassoll32@gmail.com>
2020-05-30 15:07:29 +0300

    Day of the Week

---
+++ src/main/java/DayOfTheWeek.java
@@ -0,0 +1,20 @@
+import common.LeetCode;
+
+/**
+ * @author RakhmedovRS
+ * @created 30-May-20
+ */
+@LeetCode(id = 1185, name = "Day of the Week", url = "https://leetcode.com/problems/day-of-the-week/")
+public class DayOfTheWeek
+{
+	public String dayOfTheWeek(int day, int month, int year)
+	{
+		java.time.LocalDate localDate = java.time.LocalDate.of(year, month, day);
+		return localDate.getDayOfWeek().getDisplayName(java.time.format.TextStyle.FULL_STANDALONE, java.util.Locale.ENGLISH);
+	}
+
+	public static void main(String[] args)
+	{
+		System.out.println(new DayOfTheWeek().dayOfTheWeek(31, 8, 2019));
+	}
+}
