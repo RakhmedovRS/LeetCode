ea3ed4a97da34cacbc827378be85b84858fa6fc8
rassoll <rassoll32@gmail.com>
2020-06-14 08:05:32 +0300

    Least Number of Unique Integers after K Removals

---
+++ src/main/java/LeastNumberOfUniqueIntegersAfterKRemovals.java
@@ -0,0 +1,41 @@
+import common.LeetCode;
+
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.PriorityQueue;
+
+/**
+ * @author RakhmedovRS
+ * @created 14-Jun-20
+ */
+@LeetCode(id = 5454, name = "Least Number of Unique Integers after K Removals", url = "https://leetcode.com/problems/least-number-of-unique-integers-after-k-removals/")
+public class LeastNumberOfUniqueIntegersAfterKRemovals
+{
+	public int findLeastNumOfUniqueInts(int[] arr, int k)
+	{
+		Map<Integer, Integer> map = new HashMap<>();
+		for (int num : arr)
+		{
+			map.put(num, map.getOrDefault(num, 0) + 1);
+		}
+
+		PriorityQueue<int[]> priorityQueue = new PriorityQueue<>(Comparator.comparingInt(arr2 -> arr2[1]));
+		for (Map.Entry<Integer, Integer> entry : map.entrySet())
+		{
+			priorityQueue.add(new int[]{entry.getKey(), entry.getValue()});
+		}
+
+		while (!priorityQueue.isEmpty() && k-- > 0)
+		{
+			int[] a = priorityQueue.remove();
+			a[1]--;
+			if (a[1] > 0)
+			{
+				priorityQueue.add(a);
+			}
+		}
+
+		return priorityQueue.size();
+	}
+}
