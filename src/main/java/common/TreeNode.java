9734034011c01c613ffd933adb7c44ea3941a0cb
rassoll <rassoll32@gmail.com>
2020-06-15 19:29:14 +0300

    All Possible Full Binary Trees

--- src/main/java/common/TreeNode.java
+++ src/main/java/common/TreeNode.java
@@ -11,4 +11,11 @@ public class TreeNode
 	public TreeNode right;
 
 	public TreeNode(int x){ val = x; }
+
+	public TreeNode(int val, TreeNode left, TreeNode right)
+	{
+		this.val = val;
+		this.left = left;
+		this.right = right;
+	}
 }
