ae9edfe80a51d7ce93a47c791d64d2a0d0c36b20
rakhmedovrs <rakhmedovrs@gmail.com>
2020-06-19 19:38:05 +0300

    Build an Array With Stack Operations

---
+++ src/main/java/BuildArrayWithStackOperations.java
@@ -0,0 +1,32 @@
+import common.LeetCode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author RakhmedovRS
+ * @created 19-Jun-20
+ */
+@LeetCode(id = 1441, name = "Build an Array With Stack Operations", url = "https://leetcode.com/problems/build-an-array-with-stack-operations/")
+public class BuildArrayWithStackOperations
+{
+	public List<String> buildArray(int[] target, int n)
+	{
+		int currPos = 0;
+		int currVal = 1;
+		List<String> operations = new ArrayList<>();
+		while (currPos < target.length)
+		{
+			while (target[currPos] > currVal++)
+			{
+				operations.add("Push");
+				operations.add("Pop");
+			}
+			operations.add("Push");
+
+			currPos++;
+		}
+
+		return operations;
+	}
+}
