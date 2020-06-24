a91e3937f635bf6dd2772fa437b1a4026b7c2c04
rakhmedovrs <rakhmedovrs@gmail.com>
2020-06-24 18:49:26 +0300

    Minimum Value to Get Positive Step by Step Sum

---
+++ src/main/java/MinimumValueoToGetPositiveStepByStepSum.java
@@ -0,0 +1,22 @@
+import common.LeetCode;
+
+/**
+ * @author RakhmedovRS
+ * @created 24-Jun-20
+ */
+@LeetCode(id = 1413, name = "Minimum Value to Get Positive Step by Step Sum", url = "https://leetcode.com/problems/minimum-value-to-get-positive-step-by-step-sum/")
+public class MinimumValueoToGetPositiveStepByStepSum
+{
+	public int minStartValue(int[] nums)
+	{
+		int sum = 0;
+		int min = Integer.MAX_VALUE;
+		for (int num : nums)
+		{
+			sum += num;
+			min = Math.min(min, sum);
+		}
+
+		return min > 0 ? 1 : min * -1 + 1;
+	}
+}
