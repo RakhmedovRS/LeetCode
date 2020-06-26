76d48d3a43dd60de79ccab68e3175fedb69327ef
rakhmedovrs <rakhmedovrs@gmail.com>
2020-06-26 20:38:57 +0300

    Element Appearing More Than 25% In Sorted Array

---
+++ src/main/java/ElementAppearingMoreThan25PercentInSortedArray.java
@@ -0,0 +1,23 @@
+import common.LeetCode;
+
+/**
+ * @author RakhmedovRS
+ * @created 26-Jun-20
+ */
+@LeetCode(id = 1287, name = "Element Appearing More Than 25% In Sorted Array", url = "https://leetcode.com/problems/element-appearing-more-than-25-in-sorted-array/")
+public class ElementAppearingMoreThan25PercentInSortedArray
+{
+	public int findSpecialInteger(int[] arr)
+	{
+		int window = arr.length / 4 + 1;
+		for (int i = 0; i <= arr.length - window; i++)
+		{
+			if (arr[i] == arr[i + window - 1])
+			{
+				return arr[i];
+			}
+		}
+
+		return arr[0];
+	}
+}
