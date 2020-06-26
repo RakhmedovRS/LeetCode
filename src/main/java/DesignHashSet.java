5cf81983005a2407f0bd31a22f826c808a1898dc
rakhmedovrs <rakhmedovrs@gmail.com>
2020-06-26 20:35:55 +0300

    Design HashSet

---
+++ src/main/java/DesignHashSet.java
@@ -0,0 +1,35 @@
+import common.LeetCode;
+
+/**
+ * @author RakhmedovRS
+ * @created 26-Jun-20
+ */
+@LeetCode(id = 705, name = "Design HashSet", url = "https://leetcode.com/problems/design-hashset/")
+public class DesignHashSet
+{
+
+	class MyHashSet
+	{
+		boolean[] bucket;
+
+		public MyHashSet()
+		{
+			bucket = new boolean[1000000];
+		}
+
+		public void add(int key)
+		{
+			bucket[key] = true;
+		}
+
+		public void remove(int key)
+		{
+			bucket[key] = false;
+		}
+
+		public boolean contains(int key)
+		{
+			return bucket[key];
+		}
+	}
+}
