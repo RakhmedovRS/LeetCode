d32559339b91afd702520f2f5e45158fd2bdfae9
rakhmedovrs <rakhmedovrs@gmail.com>
2020-07-02 20:57:45 +0300

    Longest String Chain

---
+++ src/main/java/LongestStringChain.java
@@ -0,0 +1,46 @@
+import common.LeetCode;
+
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author RakhmedovRS
+ * @created 02-Jul-20
+ */
+@LeetCode(id = 1048, name = "Longest String Chain", url = "https://leetcode.com/problems/longest-string-chain/")
+public class LongestStringChain
+{
+	public int longestStrChain(String[] words)
+	{
+		if (words.length == 0)
+		{
+			return 0;
+		}
+
+		Arrays.sort(words, Comparator.comparingInt(String::length));
+		Map<String, Integer> patternToCount = new HashMap<>(words.length * 3);
+		int max = 0;
+		for (String word : words)
+		{
+			for (int i = 0; i < word.length(); i++)
+			{
+				String newWord = word.substring(0, i) + word.substring(i + 1);
+				if (patternToCount.containsKey(newWord))
+				{
+					patternToCount.put(word, patternToCount.get(newWord) + 1);
+				}
+			}
+
+			if (!patternToCount.containsKey(word))
+			{
+				patternToCount.put(word, 1);
+			}
+
+			max = Math.max(max, patternToCount.get(word));
+		}
+
+		return max;
+	}
+}
