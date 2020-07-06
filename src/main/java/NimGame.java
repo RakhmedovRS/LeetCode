da8221431f828423caf3d5fae2934741b271c0f8
rakhmedovrs <rakhmedovrs@gmail.com>
2020-07-06 18:21:54 +0300

    Nim Game

---
+++ src/main/java/NimGame.java
@@ -0,0 +1,14 @@
+import common.LeetCode;
+
+/**
+ * @author RakhmedovRS
+ * @created 06-Jul-20
+ */
+@LeetCode(id = 292, name = "Nim Game", url = "https://leetcode.com/problems/nim-game/")
+public class NimGame
+{
+	public boolean canWinNim(int n)
+	{
+		return (n % 4 != 0);
+	}
+}
