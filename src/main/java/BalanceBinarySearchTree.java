925969644ed0fa1e7bbbde360f45b8bc08d617d5
rakhmedovrs <rakhmedovrs@gmail.com>
2020-07-17 21:52:24 +0300

    Balance a Binary Search Tree

---
+++ src/main/java/BalanceBinarySearchTree.java
@@ -0,0 +1,51 @@
+import common.LeetCode;
+import common.TreeNode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author RakhmedovRS
+ * @created 17-Jul-20
+ */
+@LeetCode(id = 1382, name = "Balance a Binary Search Tree", url = "https://leetcode.com/problems/balance-a-binary-search-tree/")
+public class BalanceBinarySearchTree
+{
+	public TreeNode balanceBST(TreeNode root)
+	{
+		List<Integer> nodes = new ArrayList<>();
+		inOrder(root, nodes);
+		return rebuildTree(nodes, 0, nodes.size() - 1);
+	}
+
+	private TreeNode rebuildTree(List<Integer> nodes, int left, int right)
+	{
+		if (left == right)
+		{
+			return new TreeNode(nodes.get(left));
+		}
+
+		if (left > right)
+		{
+			return null;
+		}
+
+		int mid = (left + right) / 2;
+		TreeNode node = new TreeNode(nodes.get(mid));
+		node.left = rebuildTree(nodes, left, mid - 1);
+		node.right = rebuildTree(nodes, mid + 1, right);
+		return node;
+	}
+
+	private void inOrder(TreeNode treeNode, List<Integer> nodes)
+	{
+		if (treeNode == null)
+		{
+			return;
+		}
+
+		inOrder(treeNode.left, nodes);
+		nodes.add(treeNode.val);
+		inOrder(treeNode.right, nodes);
+	}
+}
