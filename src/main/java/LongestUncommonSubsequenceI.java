27e0b751ffe74b725dda2c254e8ea4208d3392dd
rakhmedovrs <rakhmedovrs@gmail.com>
2020-07-21 19:23:45 +0300

    Longest Uncommon Subsequence I

---
+++ src/main/java/LongestUncommonSubsequenceI.java
@@ -0,0 +1,19 @@
+import common.LeetCode;
+
+/**
+ * @author RakhmedovRS
+ * @created 21-Jul-20
+ */
+@LeetCode(id = 521, name = "Longest Uncommon Subsequence I", url = "https://leetcode.com/problems/longest-uncommon-subsequence-i/")
+public class LongestUncommonSubsequenceI
+{
+	public int findLUSlength(String a, String b)
+	{
+		if (a.equals(b))
+		{
+			return -1;
+		}
+
+		return Math.max(a.length(), b.length());
+	}
+}
