be6fd70c96d59939ea8df712279869800eb41497
rakhmedovrs <rakhmedovrs@gmail.com>
2020-07-24 20:45:28 +0300

    Koko Eating Bananas

---
+++ src/main/java/KokoEatingBananas.java
@@ -0,0 +1,50 @@
+import common.LeetCode;
+
+/**
+ * @author RakhmedovRS
+ * @created 24-Jul-20
+ */
+@LeetCode(id = 875, name = "Koko Eating Bananas", url = "Koko Eating Bananas")
+public class KokoEatingBananas
+{
+	public int minEatingSpeed(int[] piles, int H)
+	{
+		int min = 1;
+		int max = Integer.MIN_VALUE;
+		for (int num : piles)
+		{
+			max = Math.max(max, num);
+		}
+
+		int middle;
+		while (min < max)
+		{
+			middle = (min + max) / 2;
+			if (check(piles, H, middle))
+			{
+				max = middle;
+			}
+			else
+			{
+				min = middle + 1;
+			}
+		}
+
+		return max;
+	}
+
+	private boolean check(int[] piles, int h, int size)
+	{
+		int needHours = 0;
+		for (int pile : piles)
+		{
+			needHours += pile / size;
+			if (pile % size != 0)
+			{
+				needHours++;
+			}
+		}
+
+		return needHours <= h;
+	}
+}
