e33ede0809619176ecbf5c952be35097e3b1395e
rakhmedovrs <rakhmedovrs@gmail.com>
2020-07-25 19:13:15 +0300

    Count Odd Numbers in an Interval Range

---
+++ src/main/java/CountOddNumbersInIntervalRange.java
@@ -0,0 +1,15 @@
+import common.LeetCode;
+
+/**
+ * @author RakhmedovRS
+ * @created 25-Jul-20
+ */
+@LeetCode(id = 1523, name = "Count Odd Numbers in an Interval Range", url = "https://leetcode.com/problems/count-odd-numbers-in-an-interval-range/")
+public class CountOddNumbersInIntervalRange
+{
+	public int countOdds(int low, int high)
+	{
+		int add = (low % 2 != 0 || high % 2 != 0) ? 1 : 0;
+		return (high - low) / 2 + add;
+	}
+}
