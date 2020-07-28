4f39bcfae364ab545ff4e3a2997695cb3d0e901e
rakhmedovrs <rakhmedovrs@gmail.com>
2020-07-28 21:22:29 +0300

    Largest Perimeter Triangle

---
+++ src/main/java/LargestPerimeterTriangle.java
@@ -0,0 +1,22 @@
+import common.LeetCode;
+
+import java.util.Arrays;
+
+/**
+ * @author RakhmedovRS
+ * @created 28-Jul-20
+ */
+@LeetCode(id = 976, name = "Largest Perimeter Triangle", url = "https://leetcode.com/problems/largest-perimeter-triangle/")
+public class LargestPerimeterTriangle
+{
+	public int largestPerimeter(int[] A) {
+		Arrays.sort(A);
+		for (int i = A.length - 3; i >= 0; i--) {
+			if (A[i] + A[i + 1] > A[i + 2]) {
+				return A[i] + A[i + 1] + A[i + 2];
+			}
+		}
+
+		return 0;
+	}
+}
