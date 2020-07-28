ed36bccae77e632f14fe09996cb0d7c9277e0aa6
rakhmedovrs <rakhmedovrs@gmail.com>
2020-07-28 21:24:16 +0300

    Score After Flipping Matrix

---
+++ src/main/java/ScoreAfterFlippingMatrix.java
@@ -0,0 +1,61 @@
+import common.LeetCode;
+
+/**
+ * @author RakhmedovRS
+ * @created 28-Jul-20
+ */
+@LeetCode(id = 861, name = "Score After Flipping Matrix", url = "https://leetcode.com/problems/score-after-flipping-matrix/")
+public class ScoreAfterFlippingMatrix
+{
+	public int matrixScore(int[][] matrix)
+	{
+
+		for (int[] row : matrix)
+		{
+			if (row[0] == 0)
+			{
+				flip(row);
+			}
+		}
+
+		int minOnesCount = matrix.length % 2 == 0 ? matrix.length / 2 : (matrix.length + 1) / 2;
+		for (int column = 1; column < matrix[0].length; column++)
+		{
+			int count = 0;
+			for (int[] row : matrix)
+			{
+				count += row[column];
+			}
+
+			if (count < minOnesCount)
+			{
+				for (int[] row : matrix)
+				{
+					row[column] = (row[column] + 1) % 2;
+				}
+			}
+		}
+
+		int result = 0;
+		for (int[] row : matrix)
+		{
+			int rowRes = 0;
+			for (int value : row)
+			{
+				rowRes <<= 1;
+				rowRes ^= value;
+			}
+			result += rowRes;
+		}
+
+		return result;
+	}
+
+	private void flip(int[] arr)
+	{
+		for (int i = 0; i < arr.length; i++)
+		{
+			arr[i] = (arr[i] + 1) % 2;
+		}
+	}
+}
