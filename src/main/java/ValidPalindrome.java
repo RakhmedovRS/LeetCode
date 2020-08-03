1a3e6c736ec9c55fafa0cae8f6a1b80d0de6e412
rakhmedovrs <rakhmedovrs@gmail.com>
2020-08-03 20:41:58 +0300

    Valid Palindrome updated

--- src/main/java/ValidPalindrome.java
+++ src/main/java/ValidPalindrome.java
@@ -12,24 +12,21 @@ public class ValidPalindrome
 {
 	public boolean isPalindrome(String s)
 	{
-		if (s == null || s.length() == 0)
+		int left = 0;
+		int right = s.length() - 1;
+		while (left < right)
 		{
-			return true;
-		}
-
-		List<Character> characters = new ArrayList<>();
-		for (char ch : s.toCharArray())
-		{
-			if ((ch >= 'a' && ch <= 'z') || (ch >= 'A' && ch <= 'Z') || (ch >= '0' && ch <= '9'))
+			while (left < s.length() && !Character.isLetterOrDigit(s.charAt(left)))
 			{
-
-				characters.add((ch >= 'A' && ch <= 'Z') ? (char) (ch + 32) : ch);
+				left++;
 			}
-		}
 
-		for (int left = 0, right = characters.size() - 1; left < right; left++, right--)
-		{
-			if (characters.get(left) != characters.get(right))
+			while (right > left && !Character.isLetterOrDigit(s.charAt(right)))
+			{
+				right--;
+			}
+
+			if (left < right && Character.toLowerCase(s.charAt(left++)) != Character.toLowerCase(s.charAt(right--)))
 			{
 				return false;
 			}
