565f7489e4cb907422a6626d084dc5e13e63b08c
rakhmedovrs <rakhmedovrs@gmail.com>
2020-08-06 21:06:40 +0300

    Poor Pigs

---
+++ src/main/java/PoorPigs.java
@@ -0,0 +1,20 @@
+import common.LeetCode;
+
+/**
+ * @author RakhmedovRS
+ * @created 06-Aug-20
+ */
+@LeetCode(id = 458, name = "Poor Pigs", url = "https://leetcode.com/problems/poor-pigs/")
+public class PoorPigs
+{
+	public int poorPigs(int buckets, int minutesToDie, int minutesToTest)
+	{
+		int pigs = 0;
+		while (Math.pow(minutesToTest / minutesToDie + 1, pigs) < buckets)
+		{
+			pigs++;
+		}
+
+		return pigs;
+	}
+}
