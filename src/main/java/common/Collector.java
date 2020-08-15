71fcd6faf9719cb95397bd0b1ed1f4bc63846baa
rakhmedovrs <rakhmedovrs@gmail.com>
2020-08-15 13:39:09 +0300

    Collector updated

--- src/main/java/common/Collector.java
+++ src/main/java/common/Collector.java
@@ -10,6 +10,7 @@ import java.util.Comparator;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 /**
  * @author RakhmedovRS
@@ -19,9 +20,10 @@ public class Collector
 {
 	public static void main(String[] args) throws Exception
 	{
-		List<Map.Entry<LeetCode, String>> annotations =
-			Files.list(Paths.get(Paths.get("").toAbsolutePath().toString() + "\\src\\main\\java"))
-				.map(path ->
+		try (Stream<Path> pathStream = Files.list(Paths.get(Paths.get("").toAbsolutePath().toString() + "\\src\\main\\java")))
+		{
+			List<Map.Entry<LeetCode, String>> annotations =
+				pathStream.map(path ->
 				{
 					try
 					{
@@ -32,34 +34,41 @@ public class Collector
 						return null;
 					}
 				})
-				.filter(clazz -> clazz != null && clazz.isAnnotationPresent(LeetCode.class))
-				.map(clazz -> new AbstractMap.SimpleEntry<>(clazz.getAnnotation(LeetCode.class), clazz.getName()))
-				.sorted(Comparator.comparingInt(entry -> entry.getKey().id()))
-				.collect(Collectors.toList());
+					.filter(clazz -> clazz != null && clazz.isAnnotationPresent(LeetCode.class))
+					.map(clazz -> new AbstractMap.SimpleEntry<>(clazz.getAnnotation(LeetCode.class), clazz.getName()))
+					.sorted(Comparator.comparingInt(entry -> entry.getKey().id()))
+					.collect(Collectors.toList());
 
-		Path output = Paths.get(Paths.get("").toAbsolutePath().toString() + "\\README.MD");
-		try (OutputStreamWriter osw = new OutputStreamWriter(new FileOutputStream(output.toFile()));)
-		{
-			osw.write(String.format("### This file was created automatically by [%s.java](https://github.com/RakhmedovRS/LeetCode/tree/master/src/main/java/common/%s.java)\n",
-				Collector.class.getSimpleName(), Collector.class.getSimpleName()));
-			osw.write(String.format("Count of solved tasks: %s\n", annotations.size()));
-			osw.write("\n");
-			osw.write("| LeetCode ID        | Name           | Solution       |\n");
-			osw.write("| :-----------------:|:--------------:|:--------------:|\n");
-
-			annotations.forEach(entry ->
+			Path output = Paths.get(Paths.get("").toAbsolutePath().toString() + "\\README.MD");
+			try (OutputStreamWriter osw = new OutputStreamWriter(new FileOutputStream(output.toFile()));)
 			{
-				try
-				{
-					String url = String.format("[%s.java](https://github.com/RakhmedovRS/LeetCode/tree/master/src/main/java/%s.java)", entry.getValue(), entry.getValue());
-					LeetCode leetCode = entry.getKey();
-					osw.write(String.format("|%d|[%s](%s)|%s\n", leetCode.id(), leetCode.name(), leetCode.url(), url));
-				}
-				catch (Exception ignore)
-				{
+				osw.write(
+					String.format("### This file was created automatically by [%s.java](https://github.com/RakhmedovRS/LeetCode/tree/master/src/main/java/common/%s.java)%s",
+						Collector.class.getSimpleName(), Collector.class.getSimpleName(), System.lineSeparator()));
+				osw.write(String.format("Count of solved tasks: %s", annotations.size() + System.lineSeparator()));
+				osw.write(System.lineSeparator());
+				osw.write("| LeetCode ID        | Name           | Solution       |" + System.lineSeparator());
+				osw.write("| :-----------------:|:--------------:|:--------------:|" + System.lineSeparator());
 
-				}
-			});
+				String table = annotations
+					.stream()
+					.map(entry ->
+					{
+						try
+						{
+							String url = String.format("[%s.java](https://github.com/RakhmedovRS/LeetCode/tree/master/src/main/java/%s.java)", entry.getValue(), entry.getValue());
+							LeetCode leetCode = entry.getKey();
+							return String.format("|%d|[%s](%s)|%s", leetCode.id(), leetCode.name(), leetCode.url(), url);
+						}
+						catch (Exception ignore)
+						{
+							return "";
+						}
+					})
+					.collect(Collectors.joining(System.lineSeparator()));
+
+				osw.write(table);
+			}
 		}
 	}
 }
