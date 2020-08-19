b1ae17d8c58464fb09574aef950241e72934b63e
rakhmedovrs <rakhmedovrs@gmail.com>
2020-08-19 19:53:14 +0300

    Break a Palindrome

---
+++ src/main/java/BreakPalindrome.java
@@ -0,0 +1,42 @@
+import common.LeetCode;
+
+/**
+ * @author RakhmedovRS
+ * @created 19-Aug-20
+ */
+@LeetCode(id = 1328, name = "Break a Palindrome", url = "https://leetcode.com/problems/break-a-palindrome/")
+public class BreakPalindrome
+{
+	public String breakPalindrome(String palindrome)
+	{
+		if (palindrome.length() == 1)
+		{
+			return "";
+		}
+
+		char[] chars = palindrome.toCharArray();
+		int ch = 0;
+		boolean isPalindrome = true;
+		while (isPalindrome)
+		{
+			int left = 0;
+			int right = chars.length - 1;
+			char newChar = ((char) (ch + 'a'));
+			while (left < right)
+			{
+				if (newChar != chars[right])
+				{
+					isPalindrome = false;
+					chars[left] = (char) Math.min(newChar, chars[right]);
+					chars[right] = (char) Math.max(newChar, chars[right]);
+					break;
+				}
+				left++;
+				right--;
+			}
+			ch++;
+		}
+
+		return String.valueOf(chars);
+	}
+}
