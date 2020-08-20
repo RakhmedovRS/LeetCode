9d9d6d72181a3a718f73a3f90eaaa3757fe6a0d1
rakhmedovrs <rakhmedovrs@gmail.com>
2020-08-20 20:38:33 +0300

    Water and Jug Problem

---
+++ src/main/java/WaterAndJugProblem.java
@@ -0,0 +1,34 @@
+import common.LeetCode;
+
+/**
+ * @author RakhmedovRS
+ * @created 20-Aug-20
+ */
+@LeetCode(id = 365, name = "Water and Jug Problem", url = "https://leetcode.com/problems/water-and-jug-problem/")
+public class WaterAndJugProblem
+{
+	public boolean canMeasureWater(int x, int y, int z)
+	{
+		if (x + y < z)
+		{
+			return false;
+		}
+
+		int gcd = gcd(x, y);
+		if (gcd == 0 && z != 0)
+		{
+			return false;
+		}
+
+		return gcd == 0 || z % gcd == 0;
+	}
+
+	private int gcd(int x, int y)
+	{
+		if (y == 0)
+		{
+			return x;
+		}
+		return gcd(y, x % y);
+	}
+}
