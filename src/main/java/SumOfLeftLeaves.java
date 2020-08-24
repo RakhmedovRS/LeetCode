77c4245357d3f4c8ca9ab140aa0bc83506bf5647
rakhmedovrs <rakhmedovrs@gmail.com>
2020-08-24 20:23:21 +0300

    Sum of Left Leaves

---
+++ src/main/java/SumOfLeftLeaves.java
@@ -0,0 +1,35 @@
+import common.LeetCode;
+import common.TreeNode;
+
+/**
+ * @author RakhmedovRS
+ * @created 24-Aug-20
+ */
+@LeetCode(id = 404, name = "Sum of Left Leaves", url = "https://leetcode.com/problems/sum-of-left-leaves/")
+public class SumOfLeftLeaves
+{
+	public int sumOfLeftLeaves(TreeNode root)
+	{
+		if (root == null)
+		{
+			return 0;
+		}
+
+		return sumOfLeftLeaves(root.left, true) + sumOfLeftLeaves(root.right, false);
+	}
+
+	public int sumOfLeftLeaves(TreeNode root, boolean isLeft)
+	{
+		if (root == null)
+		{
+			return 0;
+		}
+
+		if (root.left == null && root.right == null)
+		{
+			return isLeft ? root.val : 0;
+		}
+
+		return sumOfLeftLeaves(root.left, true) + sumOfLeftLeaves(root.right, false);
+	}
+}
