f5c084f6b51116a0b845f814fed3267fdab05057
rakhmedovrs <rakhmedovrs@gmail.com>
2020-08-31 21:32:29 +0300

    Best Sightseeing Pair

---
+++ src/main/java/BestSightseeingPair.java
@@ -0,0 +1,22 @@
+import common.LeetCode;
+
+/**
+ * @author RakhmedovRS
+ * @created 31-Aug-20
+ */
+@LeetCode(id = 1014, name = "Best Sightseeing Pair", url = "https://leetcode.com/problems/best-sightseeing-pair/")
+public class BestSightseeingPair
+{
+	public int maxScoreSightseeingPair(int[] A)
+	{
+		int max = 0;
+		int current = 0;
+		for (int a : A)
+		{
+			max = Math.max(max, current + a);
+			current = Math.max(current, a) - 1;
+		}
+
+		return max;
+	}
+}
