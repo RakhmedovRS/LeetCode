e8e51e271ae8edd66aec87ea2d4eb0a5ba0a8a91
rakhmedovrs <rakhmedovrs@gmail.com>
2020-09-02 21:06:19 +0300

    Elimination Game

---
+++ src/main/java/EliminationGame.java
@@ -0,0 +1,44 @@
+import common.LeetCode;
+
+/**
+ * @author RakhmedovRS
+ * @created 02-Sep-20
+ */
+@LeetCode(id = 390, name = "Elimination Game", url = "https://leetcode.com/problems/elimination-game/")
+public class EliminationGame
+{
+	public int lastRemaining(int n)
+	{
+		return leftToRight(n);
+	}
+
+	private static int leftToRight(int n)
+	{
+		if (n <= 2)
+		{
+			return n;
+		}
+		return 2 * rightToLeft(n / 2);
+	}
+
+	private static int rightToLeft(int n)
+	{
+		if (n <= 2)
+		{
+			return 1;
+		}
+		if (n % 2 == 1)
+		{
+			return 2 * leftToRight(n / 2);
+		}
+		return 2 * leftToRight(n / 2) - 1;
+	}
+
+	public static void main(String[] args)
+	{
+		for (int i = 1; i <= 100; i++)
+		{
+			System.out.println(i + "-" + new EliminationGame().lastRemaining(i));
+		}
+	}
+}
