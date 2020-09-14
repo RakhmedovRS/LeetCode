948ff796887878e2c9f4af3da8b2977b68b17491
rakhmedovrs <rakhmedovrs@gmail.com>
2020-09-14 21:55:04 +0300

    House Robber updated

--- src/main/java/HouseRobber.java
+++ src/main/java/HouseRobber.java
@@ -13,18 +13,25 @@ public class HouseRobber
 		{
 			return 0;
 		}
-
-		int[] memo = new int[nums.length + 1];
-
-		memo[0] = 0;
-		memo[1] = nums[0];
-
-		for (int i = 1; i < nums.length; i++)
+		else if (nums.length == 1)
 		{
-			memo[i + 1] = Math.max(memo[i], memo[i - 1] + nums[i]);
+			return nums[0];
 		}
 
-		return memo[nums.length];
+		int max = Math.max(nums[0], nums[1]);
+		int[] memo = new int[nums.length];
+		memo[0] = nums[0];
+		memo[1] = nums[1];
+		for (int i = 2; i < nums.length; i++)
+		{
+			for (int j = 0; j < i - 1; j++)
+			{
+				memo[i] = Math.max(memo[i], memo[j] + nums[i]);
+				max = Math.max(max, memo[i]);
+			}
+		}
+
+		return max;
 	}
 
 	public static void main(String[] args)
