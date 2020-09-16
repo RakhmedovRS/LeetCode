3b4d48cf408680f59de3f3636d8564088abb4bc5
rakhmedovrs <rakhmedovrs@gmail.com>
2020-09-16 20:28:49 +0300

    Maximum XOR of Two Numbers in an Array updated

--- src/main/java/MaximumXOROfTwoNumbersInAnArray.java
+++ src/main/java/MaximumXOROfTwoNumbersInAnArray.java
@@ -7,15 +7,51 @@ import common.LeetCode;
 @LeetCode(id = 421, name = "Maximum XOR of Two Numbers in an Array", url = "https://leetcode.com/problems/maximum-xor-of-two-numbers-in-an-array/")
 public class MaximumXOROfTwoNumbersInAnArray
 {
+	class Trie
+	{
+		Trie[] bit = new Trie[2];
+	}
+
 	public int findMaximumXOR(int[] nums)
 	{
-		int max = 0;
-		for (int i = 0; i < nums.length; i++)
+		Trie root = new Trie();
+		for (int num : nums)
 		{
-			for (int j = i + 1; j < nums.length; j++)
+			Trie current = root;
+			int bit;
+			for (int i = 31; i >= 0; i--)
 			{
-				max = Math.max(max, nums[i] ^ nums[j]);
+				bit = (num >> i) & 1;
+				if (current.bit[bit] == null)
+				{
+					current.bit[bit] = new Trie();
+				}
+				current = current.bit[bit];
 			}
+		}
+
+		int max = 0;
+		int sum;
+		for (int num : nums)
+		{
+			Trie current = root;
+			int bit;
+			sum = 0;
+			for (int i = 31; i >= 0; i--)
+			{
+				bit = (num >> i) & 1;
+				if (current.bit[bit ^ 1] != null)
+				{
+					current = current.bit[bit ^ 1];
+					sum += (1 << i);
+				}
+				else
+				{
+					current = current.bit[bit];
+				}
+			}
+
+			max = Math.max(max, sum);
 		}
 
 		return max;
