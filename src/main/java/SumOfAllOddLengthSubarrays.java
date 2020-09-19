ec0ae8e84a4a08dd015afa4ac9d128a600ebf42f
rakhmedovrs <rakhmedovrs@gmail.com>
2020-09-19 19:53:32 +0300

    Sum of All Odd Length Subarrays

---
+++ src/main/java/SumOfAllOddLengthSubarrays.java
@@ -0,0 +1,28 @@
+import common.LeetCode;
+
+/**
+ * @author RakhmedovRS
+ * @created 19-Sep-20
+ */
+@LeetCode(id = 1588, name = "Sum of All Odd Length Subarrays", url = "https://leetcode.com/problems/sum-of-all-odd-length-subarrays/")
+public class SumOfAllOddLengthSubarrays
+{
+	public int sumOddLengthSubarrays(int[] arr)
+	{
+		int result = 0;
+		int len = 1;
+		while (len <= arr.length)
+		{
+			for (int i = 0; i <= arr.length - len; i++)
+			{
+				for (int j = 0; j < len; j++)
+				{
+					result += arr[i + j];
+				}
+			}
+			len += 2;
+		}
+
+		return result;
+	}
+}
