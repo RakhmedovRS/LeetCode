4817cce76ac2d6faeaac2bae9bfa20db51130e9b
rakhmedovrs <rakhmedovrs@gmail.com>
2020-09-22 20:50:21 +0300

    Majority Element II updated

--- src/main/java/MajorityElementII.java
+++ src/main/java/MajorityElementII.java
@@ -4,7 +4,6 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.stream.Collectors;
 
 /**
  * @author RakhmedovRS
@@ -13,90 +12,24 @@ import java.util.stream.Collectors;
 @LeetCode(id = 229, name = "Majority Element II", url = "https://leetcode.com/problems/majority-element-ii/")
 public class MajorityElementII
 {
-	public List<Integer> majorityElement1(int[] nums)
-	{
-		Map<Integer, Integer> memo = new HashMap<>();
-		for (Integer num : nums)
-		{
-			memo.put(num, memo.getOrDefault(num, 0) + 1);
-		}
-
-		return memo.entrySet().stream().filter(entry -> entry.getValue() > nums.length / 3).map(Map.Entry::getKey).collect(Collectors.toList());
-	}
-
 	public List<Integer> majorityElement(int[] nums)
 	{
-		List<Integer> rst = new ArrayList<>();
-		if (nums == null || nums.length == 0)
-		{
-			return rst;
-		}
-
-		int count1 = 0;
-		int count2 = 0;
-		int candidate1 = 0;
-		int candidate2 = 1;
+		List<Integer> answer = new ArrayList<>();
+		Map<Integer, Integer> map = new HashMap<>();
 		for (int num : nums)
 		{
-			if (num == candidate1)
-			{
-				count1++;
-			}
-			else if (num == candidate2)
-			{
-				count2++;
-			}
-			else if (count1 == 0)
-			{
-				candidate1 = num;
-				count1 = 1;
-			}
-			else if (count2 == 0)
-			{
-				candidate2 = num;
-				count2 = 1;
-			}
-			else
-			{
-				count1--;
-				count2--;
-			}
+			map.put(num, map.getOrDefault(num, 0) + 1);
 		}
 
-		count1 = 0;
-		count2 = 0;
-		for (int num : nums)
+		for (Map.Entry<Integer, Integer> entry : map.entrySet())
 		{
-			if (num == candidate1)
+			if (entry.getValue() > nums.length / 3)
 			{
-				count1 += 2;
-			}
-			else
-			{
-				count1--;
-			}
-
-			if (num == candidate2)
-			{
-				count2 += 2;
-			}
-			else
-			{
-				count2--;
+				answer.add(entry.getKey());
 			}
 		}
 
-		if (count1 > 0)
-		{
-			rst.add(candidate1);
-		}
-
-		if (count2 > 0)
-		{
-			rst.add(candidate2);
-		}
-
-		return rst;
+		return answer;
 	}
 
 	public static void main(String[] args)
