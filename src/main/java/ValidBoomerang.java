7662b4f5c7020f43c089fa40340afe002cdc3ca3
rakhmedovrs@gmail.com <QAZmlp123!>
2020-09-24 20:45:41 +0300

    Valid Boomerang

---
+++ src/main/java/ValidBoomerang.java
@@ -0,0 +1,18 @@
+import common.LeetCode;
+
+/**
+ * @author RakhmedovRS
+ * @created 9/24/2020
+ */
+@LeetCode(id = 1037, name = "Valid Boomerang", url = "https://leetcode.com/problems/valid-boomerang/")
+public class ValidBoomerang
+{
+	public boolean isBoomerang(int[][] points)
+	{
+		int[] a = points[0];
+		int[] b = points[1];
+		int[] c = points[2];
+
+		return (a[1] - b[1]) * (c[0] - b[0]) != (c[1] - b[1]) * (a[0] - b[0]);
+	}
+}
