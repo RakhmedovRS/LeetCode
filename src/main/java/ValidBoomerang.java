6ffcfca348004f0da879a700c5b4694d46d72c48
rakhmedovrs@gmail.com <rakhmedovrs@gmail.com>
2020-09-24 21:18:43 +0300

    Valid Boomerang

--- src/main/java/ValidBoomerang.java
+++ src/main/java/ValidBoomerang.java
@@ -12,7 +12,6 @@ public class ValidBoomerang
 		int[] a = points[0];
 		int[] b = points[1];
 		int[] c = points[2];
-
 		return (a[1] - b[1]) * (c[0] - b[0]) != (c[1] - b[1]) * (a[0] - b[0]);
 	}
 }
