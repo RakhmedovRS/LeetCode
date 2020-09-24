713858b227299168b08eb74f4389c3bd69fb6b17
rakhmedovrs@gmail.com <QAZmlp123!>
2020-09-24 20:41:42 +0300

    Minimum Moves to Equal Array Elements II

---
+++ src/main/java/MinimumMovesToEqualArrayElementsII.java
@@ -0,0 +1,24 @@
+import common.LeetCode;
+
+import java.util.Arrays;
+
+/**
+ * @author RakhmedovRS
+ * @created 9/24/2020
+ */
+@LeetCode(id = 462, name = "Minimum Moves to Equal Array Elements II", url = "https://leetcode.com/problems/minimum-moves-to-equal-array-elements-ii/")
+public class MinimumMovesToEqualArrayElementsII
+{
+	public int minMoves2(int[] nums)
+	{
+		int moves = 0;
+		Arrays.sort(nums);
+		int middle = nums[nums.length / 2];
+		for (int i = 0; i < nums.length; i++)
+		{
+			moves += Math.abs(nums[i] - middle);
+		}
+
+		return moves;
+	}
+}
