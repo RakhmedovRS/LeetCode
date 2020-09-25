755161de2cdaca37dd5a6d2ec697c9ed74828eaa
rakhmedovrs@gmail.com <rakhmedovrs@gmail.com>
2020-09-25 18:58:24 +0300

    Largest Number updated

--- src/main/java/LargestNumber.java
+++ src/main/java/LargestNumber.java
@@ -1,8 +1,6 @@
 import common.LeetCode;
 
-import java.util.ArrayList;
-import java.util.Comparator;
-import java.util.List;
+import java.util.Arrays;
 
 /**
  * @author RakhmedovRS
@@ -13,37 +11,29 @@ public class LargestNumber
 {
 	public String largestNumber(int[] nums)
 	{
-		if (nums == null || nums.length == 0)
+		String[] numbers = new String[nums.length];
+		for (int i = 0; i < nums.length; i++)
 		{
-			return "";
+			numbers[i] = String.valueOf(nums[i]);
 		}
 
-		Comparator<String> stringComparator = (str1, str2) ->
+		Arrays.sort(numbers, (num1, num2) ->
 		{
-			String a = str1 + str2;
-			String b = str2 + str1;
-
+			String a = num1 + num2;
+			String b = num2 + num1;
 			return b.compareTo(a);
-		};
+		});
 
-		List<String> values = new ArrayList<>();
-		for (int num : nums)
-		{
-			values.add(String.valueOf(num));
-		}
-
-		values.sort(stringComparator);
 		StringBuilder sb = new StringBuilder();
-		for (String num : values)
+		for (String num : numbers)
 		{
+			if (sb.length() == 0 && num.equals("0"))
+			{
+				continue;
+			}
 			sb.append(num);
 		}
 
-		while (sb.length() > 1 && sb.charAt(0) == '0')
-		{
-			sb.delete(0, 1);
-		}
-
-		return sb.toString();
+		return sb.length() == 0 ? "0" : sb.toString();
 	}
 }
