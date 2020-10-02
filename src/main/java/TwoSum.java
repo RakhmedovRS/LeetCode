0bbd65f366c046fe64d8c4ef6a685c07d43bd9ca
rakhmedovrs@gmail.com <rakhmedovrs@gmail.com>
2020-10-02 21:39:55 +0300

    Two Sum updated

--- src/main/java/TwoSum.java
+++ src/main/java/TwoSum.java
@@ -10,22 +10,26 @@ import java.util.Map;
 @LeetCode(id = 1, name = "Two Sum", url = "https://leetcode.com/problems/two-sum/")
 public class TwoSum
 {
-	public int[] twoSum(int[] nums, int target) {
-		Map<Integer, Integer> map = new HashMap<>();
+	public int[] twoSum(int[] nums, int target)
+	{
+		Map<Integer, Integer> seen = new HashMap<>();
 		for (int i = 0; i < nums.length; i++)
 		{
-			map.put(nums[i], i);
-		}
-
-		for (int i = 0; i < nums.length; i++)
-		{
-			Integer value = map.get(target - nums[i]);
-			if (value != null && value != i)
+			if (seen.containsKey(target - nums[i]))
 			{
-				return new int[]{i, value};
+				if (seen.get(target - nums[i]) != i)
+				{
+					return new int[]{seen.get(target - nums[i]), i};
+				}
+				else
+				{
+					continue;
+				}
 			}
+
+			seen.put(nums[i], i);
 		}
 
-		return null;
+		return new int[0];
 	}
 }
