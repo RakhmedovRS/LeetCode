00c1501378b5bb3720198c23634dff6e74007d04
rakhmedovrs@gmail.com <rakhmedovrs@gmail.com>
2020-10-04 12:52:05 +0300

    Minimum One Bit Operations to Make Integers Zero

---
+++ src/main/java/MinimumOneBitOperationsToMakeIntegersZero.java
@@ -0,0 +1,22 @@
+import common.LeetCode;
+
+/**
+ * @author RakhmedovRS
+ * @created 10/4/2020
+ */
+@LeetCode(id = 1611, name = "Minimum One Bit Operations to Make Integers Zero", url = "https://leetcode.com/problems/minimum-one-bit-operations-to-make-integers-zero/")
+public class MinimumOneBitOperationsToMakeIntegersZero
+{
+	public int minimumOneBitOperations(int n)
+	{
+		int steps = 0;
+		for (int i = 0; i < 31; i++)
+		{
+			if ((n & (1 << i)) != 0)
+			{
+				steps = (int)Math.pow(2, i + 1) - 1 - steps;
+			}
+		}
+		return steps;
+	}
+}
