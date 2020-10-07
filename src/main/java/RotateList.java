eb0bc75893f1778bc8a74f0ab8d48ba27467ccb0
rakhmedovrs@gmail.com <rakhmedovrs@gmail.com>
2020-10-07 19:32:47 +0300

    Rotate List

---
+++ src/main/java/RotateList.java
@@ -0,0 +1,45 @@
+import common.LeetCode;
+import common.ListNode;
+
+import java.util.LinkedList;
+
+/**
+ * @author RakhmedovRS
+ * @created 10/7/2020
+ */
+@LeetCode(id = 61, name = "Rotate List", url = "https://leetcode.com/problems/rotate-list/")
+public class RotateList
+{
+	public ListNode rotateRight(ListNode head, int k)
+	{
+		if (head == null || k == 0)
+		{
+			return head;
+		}
+
+		LinkedList<ListNode> list = new LinkedList<>();
+		while (head != null)
+		{
+			list.addLast(head);
+			head = head.next;
+		}
+
+		k %= list.size();
+		while (k-- > 0)
+		{
+			list.addFirst(list.removeLast());
+		}
+
+		ListNode newHead = list.removeFirst();
+		ListNode current = newHead;
+		while (!list.isEmpty())
+		{
+			current.next = list.remove();
+			current = current.next;
+		}
+
+		current.next = null;
+
+		return newHead;
+	}
+}
