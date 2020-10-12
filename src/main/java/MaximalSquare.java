9f43a6e31f8e21a0b17bcf45871217558512bf50
rakhmedovrs@gmail.com <rakhmedovrs@gmail.com>
2020-10-12 21:04:50 +0300

    Maximal Square updated

--- src/main/java/MaximalSquare.java
+++ src/main/java/MaximalSquare.java
@@ -9,46 +9,32 @@ public class MaximalSquare
 {
 	public int maximalSquare(char[][] matrix)
 	{
-		int result = 0;
-		if (matrix == null || matrix.length == 0)
+		if (matrix.length == 0 || matrix[0].length == 0)
 		{
-			return result;
+			return 0;
 		}
 
 		int rows = matrix.length;
 		int columns = matrix[0].length;
 		int[][] memo = new int[rows][columns];
-		for (int row = 0; row < rows; row++)
+		int max = 0;
+		for (int row = rows - 1; row >= 0; row--)
 		{
-			for (int column = 0; column < columns; column++)
+			for (int column = columns - 1; column >= 0; column--)
 			{
-				memo[row][column] = matrix[row][column] - '0';
-			}
-		}
-
-		for (int row = 1; row < rows; row++)
-		{
-			for (int column = 1; column < columns; column++)
-			{
-				if (memo[row][column] > 0)
+				if (matrix[row][column] == '1')
 				{
-					memo[row][column] += Math.min(memo[row - 1][column - 1],
-						Math.min(memo[row - 1][column], memo[row][column - 1]));
+					memo[row][column]++;
+					if (row <= rows - 2 && column <= columns - 2)
+					{
+						memo[row][column] += Math.min(memo[row + 1][column + 1], Math.min(memo[row + 1][column], memo[row][column + 1]));
+					}
+
+					max = Math.max(max, memo[row][column]);
 				}
 			}
 		}
 
-		for (int row = 0; row < rows; row++)
-		{
-			for (int column = 0; column < columns; column++)
-			{
-				if (memo[row][column] > result)
-				{
-					result = memo[row][column];
-				}
-			}
-		}
-
-		return result * result;
+		return (int) Math.pow(max, 2);
 	}
 }
