f3f52f2631cb0e8fa659414b5fa2f2ffacf250b4
rakhmedovrs@gmail.com <rakhmedovrs@gmail.com>
2020-10-16 22:05:42 +0300

    New 21 Game

---
+++ src/main/java/New21Game.java
@@ -0,0 +1,43 @@
+import common.LeetCode;
+
+/**
+ * @author RakhmedovRS
+ * @created 10/16/2020
+ */
+@LeetCode(id = 837, name = "New 21 Game", url = "https://leetcode.com/problems/new-21-game/")
+public class New21Game
+{
+	public double new21Game(int N, int K, int W)
+	{
+		if (K == 0)
+		{
+			return 1D;
+		}
+
+		double[] dp = new double[N + 1];
+		dp[0] = 1;
+		double sum = 0;
+		for (int i = 1; i < dp.length; i++)
+		{
+			if (i - 1 < K)
+			{
+				sum += dp[i - 1];
+			}
+
+			if (i - 1 - W >= 0)
+			{
+				sum -= dp[i - 1 - W];
+			}
+
+			dp[i] = sum / W;
+		}
+
+		double result = 0D;
+		for (int i = K; i <= N; i++)
+		{
+			result += dp[i];
+		}
+
+		return result;
+	}
+}
