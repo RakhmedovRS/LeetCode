4dd0feac12fb3143466326cfa7a941cb539a25cc
rakhmedovrs@gmail.com <rakhmedovrs@gmail.com>
2020-10-20 21:59:40 +0300

    Binary Tree Coloring Game

---
+++ src/main/java/BinaryTreeColoringGame.java
@@ -0,0 +1,46 @@
+import common.LeetCode;
+import common.TreeNode;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author RakhmedovRS
+ * @created 10/20/2020
+ */
+@LeetCode(id = 1145, name = "Binary Tree Coloring Game", url = "https://leetcode.com/problems/binary-tree-coloring-game/")
+public class BinaryTreeColoringGame
+{
+	public boolean btreeGameWinningMove(TreeNode root, int n, int x)
+	{
+		Map<TreeNode, Integer> nodeCount = new HashMap<>();
+		dfs(root, nodeCount, x);
+		for (int count : nodeCount.values())
+		{
+			if (count > n / 2)
+			{
+				return true;
+			}
+		}
+
+		return false;
+	}
+
+	private int dfs(TreeNode root, Map<TreeNode, Integer> nodeCount, int x)
+	{
+		if (root == null)
+		{
+			return 0;
+		}
+
+		if (root.val == x)
+		{
+			dfs(root.left, nodeCount, x);
+			dfs(root.right, nodeCount, x);
+			return 0;
+		}
+
+		nodeCount.put(root, 1 + dfs(root.left, nodeCount, x) + dfs(root.right, nodeCount, x));
+		return nodeCount.get(root);
+	}
+}
