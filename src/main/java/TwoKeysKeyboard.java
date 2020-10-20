b05f60f190d6ba1a2bb7ac5d179ebf98dc35110b
rakhmedovrs@gmail.com <rakhmedovrs@gmail.com>
2020-10-20 22:01:27 +0300

    2 Keys Keyboard updated

--- src/main/java/TwoKeysKeyboard.java
+++ src/main/java/TwoKeysKeyboard.java
@@ -4,25 +4,41 @@ import common.LeetCode;
  * @author RakhmedovRS
  * @created 13-Jun-20
  */
-@LeetCode(id = 650, name = "Keys Keyboard", url = "https://leetcode.com/problems/2-keys-keyboard/")
+@LeetCode(id = 650, name = "2 Keys Keyboard", url = "https://leetcode.com/problems/2-keys-keyboard/")
 public class TwoKeysKeyboard
 {
 	public int minSteps(int n)
 	{
-		int[] dp = new int[n + 1];
-		for (int i = 2; i <= n; i++)
+		if (n == 1)
 		{
-			dp[i] = i;
-			for (int j = i - 1; j > 1; j--)
+			return 0;
+		}
+
+		Integer[] memo = new Integer[n + 1];
+		return dfs(n, memo);
+	}
+
+	private int dfs(int n, Integer[] memo)
+	{
+		if (n == 1)
+		{
+			return 0;
+		}
+
+		if (memo[n] != null)
+		{
+			return memo[n];
+		}
+
+		memo[n] = Integer.MAX_VALUE;
+		for (int i = 1; i < n; i++)
+		{
+			if (n % i == 0)
 			{
-				if (i % j == 0)
-				{
-					dp[i] = dp[j] + (i / j);
-					break;
-				}
+				memo[n] = Math.min(memo[n], dfs(i, memo) + n / i);
 			}
 		}
 
-		return dp[n];
+		return memo[n];
 	}
 }
