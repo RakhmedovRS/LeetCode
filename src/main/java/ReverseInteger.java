467034a02c9dcd4c8322a1b3ca283ade7966e73e
rakhmedovrs@gmail.com <rakhmedovrs@gmail.com>
2020-11-02 21:48:08 +0300

    Reverse Integer updated

--- src/main/java/ReverseInteger.java
+++ src/main/java/ReverseInteger.java
@@ -9,34 +9,19 @@ public class ReverseInteger
 {
 	public int reverse(int x)
 	{
-		if (x == Integer.MIN_VALUE)
+		long value = 0;
+		while (x != 0)
+		{
+			value *= 10;
+			value += x % 10;
+			x /= 10;
+		}
+
+		if (Math.abs(value) > Integer.MAX_VALUE)
 		{
 			return 0;
 		}
 
-		boolean negative = false;
-		if (x < 0)
-		{
-			x *= -1;
-			negative = true;
-		}
-
-		int currentLen = ("" + x).length();
-		int divider = (int) Math.pow(10, currentLen - 1);
-
-		int result = 0;
-		while (x > 0)
-		{
-			result *= 10;
-			result += x % 10;
-			x /= 10;
-			if (result > Integer.MAX_VALUE / divider)
-			{
-				return 0;
-			}
-			divider /= 10;
-		}
-
-		return negative ? result * -1 : result;
+		return (int) (value);
 	}
 }
