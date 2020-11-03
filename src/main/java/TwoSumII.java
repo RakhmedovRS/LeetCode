4a19bcd46f5928c922f22f1cb8ff511da872fd40
rakhmedovrs@gmail.com <rakhmedovrs@gmail.com>
2020-11-03 20:57:54 +0300

    Two Sum II - Input array is sorted updated

--- src/main/java/TwoSumII.java
+++ src/main/java/TwoSumII.java
@@ -10,56 +10,21 @@ import java.util.Map;
 @LeetCode(id = 167, name = "Two Sum II - Input array is sorted", url = "https://leetcode.com/problems/two-sum-ii-input-array-is-sorted/")
 public class TwoSumII
 {
-	public int[] twoSum(int[] num, int target)
+	public int[] twoSum(int[] numbers, int target)
 	{
-		int[] answer = new int[2];
-		if (num == null || num.length < 2)
-		{
-			return answer;
-		}
-
-		int left = 0, right = num.length - 1;
-		while (left < right)
-		{
-			int v = num[left] + num[right];
-			if (v == target)
-			{
-				answer[0] = left + 1;
-				answer[1] = right + 1;
-				break;
-			}
-			else if (v > target)
-			{
-				right--;
-			}
-			else
-			{
-				left++;
-			}
-		}
-		return answer;
-	}
-
-	public int[] twoSum1(int[] numbers, int target)
-	{
-		Map<Integer, Integer> cache = new HashMap<>();
+		Integer[] memo = new Integer[2001];
+		int index;
 		for (int i = 0; i < numbers.length; i++)
 		{
-			cache.put(numbers[i], i);
-		}
-
-		int[] answer = new int[2];
-		for (int i = 0; i < numbers.length; i++)
-		{
-			Integer idx = cache.get(target - numbers[i]);
-			if (idx != null)
+			index = target - numbers[i] + 1000;
+			if (memo[index] != null)
 			{
-				answer[0] = i + 1;
-				answer[1] = idx + 1;
-				break;
+				return new int[]{memo[index] + 1, i + 1};
 			}
+
+			memo[numbers[i] + 1000] = i;
 		}
 
-		return answer;
+		return new int[0];
 	}
 }
