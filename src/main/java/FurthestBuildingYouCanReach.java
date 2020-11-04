e6a9228dda3594199e658e6f299854ed8cdc2a84
rakhmedovrs@gmail.com <rakhmedovrs@gmail.com>
2020-11-04 20:30:01 +0300

    Furthest Building You Can Reach updated

--- src/main/java/FurthestBuildingYouCanReach.java
+++ src/main/java/FurthestBuildingYouCanReach.java
@@ -37,7 +37,7 @@ public class FurthestBuildingYouCanReach
 
 	public static void main(String[] args)
 	{
-		System.out.println(new FurthestBuildingYouCanReach().furthestBuilding(new int[]{4, 12, 2, 7, 3, 18, 20, 3, 19}, 4, 2));//7
+		System.out.println(new FurthestBuildingYouCanReach().furthestBuilding(new int[]{4, 12, 2, 7, 3, 18, 20, 3, 19}, 4, 2));//4
 		System.out.println(new FurthestBuildingYouCanReach().furthestBuilding(new int[]{4, 2, 7, 6, 9, 14, 12}, 5, 1)); //4
 	}
 }
