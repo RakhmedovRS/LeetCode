209e600702c0b74a821187e7c1e022f99b074a8e
rakhmedovrs@gmail.com <rakhmedovrs@gmail.com>
2020-11-06 20:06:04 +0300

    Escape The Ghosts

---
+++ src/main/java/EscapeTheGhosts.java
@@ -0,0 +1,23 @@
+import common.LeetCode;
+
+/**
+ * @author RakhmedovRS
+ * @created 11/6/2020
+ */
+@LeetCode(id = 789, name = "Escape The Ghosts", url = "https://leetcode.com/problems/escape-the-ghosts/")
+public class EscapeTheGhosts
+{
+	public boolean escapeGhosts(int[][] ghosts, int[] target)
+	{
+		int minMoves = Math.abs(target[0]) + Math.abs(target[1]);
+		for (int[] ghost : ghosts)
+		{
+			if (minMoves >= (Math.abs(target[0] - ghost[0]) + Math.abs(target[1] - ghost[1])))
+			{
+				return false;
+			}
+		}
+
+		return true;
+	}
+}
