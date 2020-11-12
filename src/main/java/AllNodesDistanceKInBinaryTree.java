6ea03f1a4ed44e0c2d7025d4b4f67a1d6634c353
rakhmedovrs@gmail.com <rakhmedovrs@gmail.com>
2020-11-12 20:40:43 +0300

    All Nodes Distance K in Binary Tree updated

--- src/main/java/AllNodesDistanceKInBinaryTree.java
+++ src/main/java/AllNodesDistanceKInBinaryTree.java
@@ -10,74 +10,68 @@ import java.util.*;
 @LeetCode(id = 863, name = "All Nodes Distance K in Binary Tree", url = "https://leetcode.com/problems/all-nodes-distance-k-in-binary-tree/")
 public class AllNodesDistanceKInBinaryTree
 {
-	public List<Integer> distanceK(TreeNode root, TreeNode target, int distance)
+	public List<Integer> distanceK(TreeNode root, TreeNode target, int K)
 	{
-		if (target == null || root == null)
+		List<Integer> answer = new ArrayList<>();
+		Map<TreeNode, List<TreeNode>> graph = new HashMap<>();
+		buildGraph(root, null, graph);
+		Queue<TreeNode> queue = new LinkedList<>();
+		queue.add(target);
+		int currentDistance = 0;
+		int levelSize;
+		TreeNode current;
+		Set<TreeNode> visited = new HashSet<>();
+		while (!queue.isEmpty() && currentDistance <= K)
 		{
-			return Collections.emptyList();
-		}
-
-		if (distance == 0)
-		{
-			return Arrays.asList(target.val);
-		}
-
-		Map<Integer, List<Integer>> graph = new HashMap<>();
-		fillInTheGraph(root, null, graph);
-		if (!graph.containsKey(target.val))
-		{
-			return Collections.emptyList();
-		}
-		Queue<Integer> queue = new LinkedList<>();
-		queue.add(target.val);
-		int size;
-		Set<Integer> visited = new HashSet<>();
-		while (!queue.isEmpty())
-		{
-			size = queue.size();
-			while (size-- > 0)
+			levelSize = queue.size();
+			while (levelSize-- > 0)
 			{
-				int current = queue.remove();
+				current = queue.remove();
 				if (!visited.add(current))
 				{
 					continue;
 				}
 
-				for (int next : graph.getOrDefault(current, Collections.emptyList()))
+				if (currentDistance == K)
 				{
-					if (!visited.contains(next))
-					{
-						queue.add(next);
-					}
+					answer.add(current.val);
 				}
+
+				queue.addAll(graph.getOrDefault(current, Collections.emptyList()));
 			}
 
-			distance--;
-			if (distance == 0)
-			{
-				return new ArrayList<>(queue);
-			}
+			currentDistance++;
 		}
 
-		return Collections.emptyList();
+
+		return answer;
 	}
 
-	private void fillInTheGraph(TreeNode root, TreeNode parent, Map<Integer, List<Integer>> graph)
+	private void buildGraph(TreeNode root, TreeNode prev, Map<TreeNode, List<TreeNode>> graph)
 	{
 		if (root == null)
 		{
 			return;
 		}
 
-		if (parent != null)
+		graph.putIfAbsent(root, new ArrayList<>());
+
+		if (prev != null)
 		{
-			graph.putIfAbsent(parent.val, new ArrayList<>());
-			graph.putIfAbsent(root.val, new ArrayList<>());
-			graph.get(parent.val).add(root.val);
-			graph.get(root.val).add(parent.val);
+			graph.get(root).add(prev);
 		}
 
-		fillInTheGraph(root.left, root, graph);
-		fillInTheGraph(root.right, root, graph);
+		if (root.left != null)
+		{
+			graph.get(root).add(root.left);
+		}
+
+		if (root.right != null)
+		{
+			graph.get(root).add(root.right);
+		}
+
+		buildGraph(root.left, root, graph);
+		buildGraph(root.right, root, graph);
 	}
 }
