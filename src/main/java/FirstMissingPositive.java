0bca1817d980abdc383195af7e848e374b5da1f0
rakhmedovrs@gmail.com <rakhmedovrs@gmail.com>
2020-11-16 21:22:10 +0300

    First Missing Positive updated

--- src/main/java/FirstMissingPositive.java
+++ src/main/java/FirstMissingPositive.java
@@ -1,29 +1,36 @@
+import common.Difficulty;
 import common.LeetCode;
 
 /**
  * @author RakhmedovRS
  * @created 15-Apr-20
  */
-@LeetCode(id = 41, name = "First Missing Positive", url = "https://leetcode.com/problems/first-missing-positive/")
+@LeetCode(
+	id = 41,
+	name = "First Missing Positive",
+	url = "https://leetcode.com/problems/first-missing-positive/",
+	difficulty = Difficulty.HARD
+)
 public class FirstMissingPositive
 {
 	public int firstMissingPositive(int[] nums)
 	{
-		int pos;
-		int temp;
-		for (int i = 0; i < nums.length; i++)
+		if (nums.length == 0)
 		{
-			while (nums[i] != i + 1 && nums[i] >= 1 && nums[i] <= nums.length)
-			{
-				pos = nums[i] - 1;
-				if (nums[pos] == pos + 1)
-				{
-					break;
-				}
+			return 1;
+		}
 
-				temp = nums[i];
-				nums[i] = nums[pos];
-				nums[pos] = temp;
+		for (int i = 0; i < nums.length;)
+		{
+			int num = nums[i];
+			int pos = num - 1;
+			if (0 <= pos && pos < nums.length && nums[pos] != num)
+			{
+				swap(nums, i, pos);
+			}
+			else
+			{
+				i++;
 			}
 		}
 
@@ -34,6 +41,25 @@ public class FirstMissingPositive
 				return i + 1;
 			}
 		}
+
 		return nums.length + 1;
+	}
+
+	private void swap(int[] nums, int i, int j)
+	{
+		int temp = nums[i];
+		nums[i] = nums[j];
+		nums[j] = temp;
+	}
+
+	public static void main(String[] args)
+	{
+		System.out.println(new FirstMissingPositive().firstMissingPositive(new int[]{})); //1
+		System.out.println(new FirstMissingPositive().firstMissingPositive(new int[]{1})); //2
+		System.out.println(new FirstMissingPositive().firstMissingPositive(new int[]{2})); //1
+		System.out.println(new FirstMissingPositive().firstMissingPositive(new int[]{-1, 4, 2, 1, 9, 10})); //3
+		System.out.println(new FirstMissingPositive().firstMissingPositive(new int[]{3, 4, -1, 1})); //2
+		System.out.println(new FirstMissingPositive().firstMissingPositive(new int[]{2, 1})); //3
+		System.out.println(new FirstMissingPositive().firstMissingPositive(new int[]{1, 1})); //2
 	}
 }
