b6b2e6151c26dcebc07e782985e5e55c687ea091
rakhmedovrs@gmail.com <rakhmedovrs@gmail.com>
2020-11-18 20:09:03 +0300

    Range Addition II

---
+++ src/main/java/RangeAdditionII.java
@@ -0,0 +1,29 @@
+import common.Difficulty;
+import common.LeetCode;
+
+/**
+ * @author RakhmedovRS
+ * @created 11/18/2020
+ */
+@LeetCode(
+	id = 598,
+	name = "Range Addition II",
+	url = "https://leetcode.com/problems/range-addition-ii/",
+	difficulty = Difficulty.EASY
+)
+public class RangeAdditionII
+{
+	public int maxCount(int rows, int columns, int[][] ops)
+	{
+		int xMin = rows;
+		int yMin = columns;
+
+		for (int[] op : ops)
+		{
+			xMin = Math.min(xMin, op[0]);
+			yMin = Math.min(yMin, op[1]);
+		}
+
+		return xMin * yMin;
+	}
+}
