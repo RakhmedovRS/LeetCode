6782c429c206e9aa5656fb000e284e7991c4ce75
rakhmedovrs@gmail.com <rakhmedovrs@gmail.com>
2020-11-20 17:46:17 +0300

    Search in Rotated Sorted Array II updated

--- src/main/java/SearchInRotatedSortedArrayII.java
+++ src/main/java/SearchInRotatedSortedArrayII.java
@@ -1,50 +1,63 @@
+import common.Difficulty;
 import common.LeetCode;
 
 /**
  * @author RakhmedovRS
  * @created 20-Jul-20
  */
-@LeetCode(id = 81, name = "Search in Rotated Sorted Array II", url = "https://leetcode.com/problems/search-in-rotated-sorted-array-ii/")
+@LeetCode(
+	id = 81,
+	name = "Search in Rotated Sorted Array II",
+	url = "https://leetcode.com/problems/search-in-rotated-sorted-array-ii/",
+	difficulty = Difficulty.MEDIUM
+)
 public class SearchInRotatedSortedArrayII
 {
 	public boolean search(int[] nums, int target)
 	{
+		if (nums.length == 0)
+		{
+			return false;
+		}
+
 		int left = 0;
 		int right = nums.length - 1;
-		int mid;
+		int middle;
 		while (left <= right)
 		{
-			mid = left + (right - left) / 2;
-			if (nums[mid] == target)
+			middle = left + (right - left) / 2;
+			if (nums[middle] == target)
 			{
 				return true;
 			}
 
-			if (nums[mid] < nums[right] || nums[left] > nums[mid])
+			if (nums[middle] == nums[left])
 			{
-				if (target > nums[mid] && target <= nums[right])
-				{
-					left = mid + 1;
-				}
-				else
-				{
-					right = mid - 1;
-				}
+				left++;
+				continue;
 			}
-			else if (nums[mid] > nums[left] || nums[mid] > nums[right])
+
+			if (nums[left] < nums[middle])
 			{
-				if (target < nums[mid] && target >= nums[left])
+				if (nums[left] <= target && target < nums[middle])
 				{
-					right = mid - 1;
+					right = middle - 1;
 				}
 				else
 				{
-					left = mid + 1;
+					left = middle + 1;
 				}
 			}
 			else
 			{
-				right--;
+				if (nums[middle] < target && target <= nums[right])
+				{
+					left = middle + 1;
+				}
+				else
+				{
+					right = middle - 1;
+				}
 			}
 		}
 
