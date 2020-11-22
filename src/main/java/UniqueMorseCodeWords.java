d72916e617c7b3b276beaa7eec8b5f92b5cfaf89
rakhmedovrs@gmail.com <rakhmedovrs@gmail.com>
2020-11-22 12:09:11 +0300

    Unique Morse Code Words

---
+++ src/main/java/UniqueMorseCodeWords.java
@@ -0,0 +1,42 @@
+import common.Difficulty;
+import common.LeetCode;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * @author RakhmedovRS
+ * @created 11/22/2020
+ */
+@LeetCode(
+	id = 804,
+	name = "Unique Morse Code Words",
+	url = "https://leetcode.com/problems/unique-morse-code-words/",
+	difficulty = Difficulty.EASY
+)
+public class UniqueMorseCodeWords
+{
+	public int uniqueMorseRepresentations(String[] words)
+	{
+		String[] codes = new String[]
+			{
+				".-", "-...", "-.-.", "-..", ".", "..-.", "--.", "....", "..",
+				".---", "-.-", ".-..", "--", "-.", "---", ".--.", "--.-", ".-.",
+				"...", "-", "..-", "...-", ".--", "-..-", "-.--", "--.."
+			};
+
+		Set<String> set = new HashSet<>();
+		for (String word : words)
+		{
+			StringBuilder sb = new StringBuilder();
+			for (char ch : word.toCharArray())
+			{
+				sb.append(codes[ch - 'a']);
+			}
+
+			set.add(sb.toString());
+		}
+
+		return set.size();
+	}
+}
