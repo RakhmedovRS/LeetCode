be6a895415ed470c32d6419f13e75d7ee6e40637
rakhmedovrs@gmail.com <rakhmedovrs@gmail.com>
2020-11-25 21:42:49 +0300

    Smallest Integer Divisible by K

--- src/main/java/SmallestIntegerDivisibleByK.java
+++ src/main/java/SmallestIntegerDivisibleByK.java
@@ -1,24 +1,33 @@
+import common.Difficulty;
 import common.LeetCode;
 
 /**
  * @author RakhmedovRS
  * @created 9/29/2020
  */
-@LeetCode(id = 1015, name = "Smallest Integer Divisible by K", url = "https://leetcode.com/problems/smallest-integer-divisible-by-k/")
+@LeetCode(
+	id = 1015,
+	name = "Smallest Integer Divisible by K",
+	url = "https://leetcode.com/problems/smallest-integer-divisible-by-k/",
+	difficulty = Difficulty.MEDIUM
+)
 public class SmallestIntegerDivisibleByK
 {
 	public int smallestRepunitDivByK(int K)
 	{
-		int reminder = 0;
-		for (int len = 1; len <= K; len++)
+		if (K % 2 == 0 || K % 5 == 0)
 		{
-			reminder = (reminder * 10 + 1) % K;
-			if (reminder == 0)
-			{
-				return len;
-			}
+			return -1;
 		}
 
-		return -1;
+		int len = 1;
+		int number = 1;
+		while (number % K != 0)
+		{
+			number = (number * 10 + 1) % K;
+			len++;
+		}
+
+		return len;
 	}
 }
