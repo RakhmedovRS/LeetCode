a81dc661cc783545170575e285273931769b079e
rakhmedovrs@gmail.com <rakhmedovrs@gmail.com>
2020-11-28 20:08:00 +0300

    Maximum Repeating Substring

---
+++ src/main/java/MaximumRepeatingSubstring.java
@@ -0,0 +1,33 @@
+import common.Difficulty;
+import common.LeetCode;
+
+/**
+ * @author RakhmedovRS
+ * @created 11/28/2020
+ */
+@LeetCode(
+	id = 1668,
+	name = "Maximum Repeating Substring",
+	url = "https://leetcode.com/problems/maximum-repeating-substring/",
+	difficulty = Difficulty.EASY
+)
+public class MaximumRepeatingSubstring
+{
+	public int maxRepeating(String sequence, String word)
+	{
+		int count = 0;
+		StringBuilder sb = new StringBuilder(word);
+		int i = 1;
+		while (sb.length() <= sequence.length())
+		{
+			if (sequence.contains(sb.toString()))
+			{
+				count = i;
+			}
+			i++;
+			sb.append(word);
+		}
+
+		return count;
+	}
+}
