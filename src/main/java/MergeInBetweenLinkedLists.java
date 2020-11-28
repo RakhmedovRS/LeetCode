01b3b9e9ee1dd9507345193e5a907e8d01da76c2
rakhmedovrs@gmail.com <rakhmedovrs@gmail.com>
2020-11-28 20:09:50 +0300

    Merge In Between Linked Lists

---
+++ src/main/java/MergeInBetweenLinkedLists.java
@@ -0,0 +1,46 @@
+import common.Difficulty;
+import common.LeetCode;
+import common.ListNode;
+
+/**
+ * @author RakhmedovRS
+ * @created 11/28/2020
+ */
+@LeetCode(
+	id = 1669,
+	name = "Merge In Between Linked Lists",
+	url = "https://leetcode.com/problems/merge-in-between-linked-lists/",
+	difficulty = Difficulty.MEDIUM
+)
+public class MergeInBetweenLinkedLists
+{
+	public ListNode mergeInBetween(ListNode list1, int a, int b, ListNode list2)
+	{
+		ListNode tailList2 = list2;
+		while (tailList2.next != null)
+		{
+			tailList2 = tailList2.next;
+		}
+
+		ListNode dummy = new ListNode(Integer.MIN_VALUE);
+		dummy.next = list1;
+
+		ListNode prev = dummy;
+		while (prev.next.val != a)
+		{
+			prev = prev.next;
+		}
+
+		ListNode cut = prev.next;
+		prev.next = list2;
+
+		while (cut.val != b)
+		{
+			cut = cut.next;
+		}
+
+		tailList2.next = cut.next;
+
+		return dummy.next;
+	}
+}
