b97071ed682c67de9a7dd7f3f77b9ae6ac6a0477
rakhmedovrs@gmail.com <rakhmedovrs@gmail.com>
2020-12-04 21:20:39 +0300

    The kth Factor of n updated

--- src/main/java/TheKthFactorOfN.java
+++ src/main/java/TheKthFactorOfN.java
@@ -1,24 +1,34 @@
+import common.Difficulty;
 import common.LeetCode;
 
 /**
  * @author RakhmedovRS
  * @created 27-Jun-20
  */
-@LeetCode(id = 1492, name = "The kth Factor of n", url = "https://leetcode.com/problems/the-kth-factor-of-n/")
+@LeetCode(
+	id = 1492,
+	name = "The kth Factor of n",
+	url = "https://leetcode.com/problems/the-kth-factor-of-n/",
+	difficulty = Difficulty.MEDIUM
+)
 public class TheKthFactorOfN
 {
 	public int kthFactor(int n, int k)
 	{
-		for (int i = 1; i <= n; i++)
+		int i = 1;
+		for (; i <= n; i++)
 		{
 			if (n % i == 0)
 			{
-				if (--k == 0)
+				k--;
+
+				if (k == 0)
 				{
-					return i;
+					break;
 				}
 			}
 		}
-		return -1;
+
+		return k == 0 ? i : -1;
 	}
 }
