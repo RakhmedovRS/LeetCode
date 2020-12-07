b9699f5bb8f6b248b788911e4cdd378c6a9aceb2
rakhmedovrs@gmail.com <rakhmedovrs@gmail.com>
2020-12-07 21:34:54 +0300

    Spiral Matrix II

--- src/main/java/SpiralMatrixII.java
+++ src/main/java/SpiralMatrixII.java
@@ -1,3 +1,4 @@
+import common.Difficulty;
 import common.LeetCode;
 
 import java.util.Arrays;
@@ -6,71 +7,50 @@ import java.util.Arrays;
  * @author RakhmedovRS
  * @created 01-Mar-20
  */
-@LeetCode(id = 59, name = "Spiral Matrix II", url = "https://leetcode.com/problems/spiral-matrix-ii/")
+@LeetCode(
+	id = 59,
+	name = "Spiral Matrix II",
+	url = "https://leetcode.com/problems/spiral-matrix-ii/",
+	difficulty = Difficulty.MEDIUM
+)
 public class SpiralMatrixII
 {
-	enum Direction
-	{
-		RIGHT,
-		DOWN,
-		LEFT,
-		UP
-	}
-
 	public int[][] generateMatrix(int n)
 	{
 		int[][] matrix = new int[n][n];
-		int actualValue = 1;
-		Direction currentDirection = Direction.RIGHT;
-		int leftEdge = 0;
-		int rightEdge = n - 1;
-		int upperEdge = 0;
-		int bottomEdge = n - 1;
-		while (leftEdge <= rightEdge && upperEdge <= bottomEdge)
+
+		int upperBound = 0;
+		int lowerBound = n - 1;
+		int leftBound = 0;
+		int rightBound = n - 1;
+
+		int value = 1;
+		n *= n;
+		while (value <= n)
 		{
-			switch (currentDirection)
+			for (int row = upperBound, column = leftBound; column <= rightBound && value <= n; column++)
 			{
-				case RIGHT:
-				{
-					for (int i = leftEdge; i <= rightEdge; i++)
-					{
-						matrix[upperEdge][i] = actualValue++;
-					}
-					currentDirection = Direction.DOWN;
-					upperEdge++;
-					break;
-				}
-				case DOWN:
-				{
-					for (int i = upperEdge; i <= bottomEdge; i++)
-					{
-						matrix[i][rightEdge] = actualValue++;
-					}
-					currentDirection = Direction.LEFT;
-					rightEdge--;
-					break;
-				}
-				case LEFT:
-				{
-					for (int i = rightEdge; i >= leftEdge; i--)
-					{
-						matrix[bottomEdge][i] = actualValue++;
-					}
-					currentDirection = Direction.UP;
-					bottomEdge--;
-					break;
-				}
-				case UP:
-				{
-					for (int i = bottomEdge; i >= upperEdge; i--)
-					{
-						matrix[i][leftEdge] = actualValue++;
-					}
-					currentDirection = Direction.RIGHT;
-					leftEdge++;
-					break;
-				}
+				matrix[row][column] = value++;
 			}
+			upperBound++;
+
+			for (int row = upperBound, column = rightBound; row <= lowerBound && value <= n; row++)
+			{
+				matrix[row][column] = value++;
+			}
+			rightBound--;
+
+			for (int row = lowerBound, column = rightBound; column >= leftBound && value <= n; column--)
+			{
+				matrix[row][column] = value++;
+			}
+			lowerBound--;
+
+			for (int row = lowerBound, column = leftBound; row >= upperBound && value <= n; row--)
+			{
+				matrix[row][column] = value++;
+			}
+			leftBound++;
 		}
 
 		return matrix;
