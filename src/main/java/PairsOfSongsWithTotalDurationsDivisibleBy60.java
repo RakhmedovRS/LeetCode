95d2cd1a3fd0dada7c8a7c4918e9b5b3f3f95f40
rakhmedovrs@gmail.com <rakhmedovrs@gmail.com>
2020-12-08 18:38:01 +0300

    Pairs of Songs With Total Durations Divisible by 60 updated

--- src/main/java/PairsOfSongsWithTotalDurationsDivisibleBy60.java
+++ src/main/java/PairsOfSongsWithTotalDurationsDivisibleBy60.java
@@ -1,31 +1,28 @@
+import common.Difficulty;
 import common.LeetCode;
 
 /**
  * @author RakhmedovRS
  * @created 29-May-20
  */
-@LeetCode(id = 1010, name = "Pairs of Songs With Total Durations Divisible by 60", url = "https://leetcode.com/problems/pairs-of-songs-with-total-durations-divisible-by-60/")
+@LeetCode(
+	id = 1010,
+	name = "Pairs of Songs With Total Durations Divisible by 60",
+	url = "https://leetcode.com/problems/pairs-of-songs-with-total-durations-divisible-by-60/",
+	difficulty = Difficulty.MEDIUM
+)
 public class PairsOfSongsWithTotalDurationsDivisibleBy60
 {
 	public int numPairsDivisibleBy60(int[] time)
 	{
-		int[] lengths = new int[60];
+		int[] memo = new int[61];
+		int pos;
+		int pairs = 0;
 		for (int t : time)
 		{
-			lengths[t % 60]++;
-		}
-		int pairs = 0;
-		for (int i = 0; i <= 30; i++)
-		{
-			if (i == 0 || i == 30)
-			{
-				pairs += lengths[i] * (lengths[i] - 1) / 2;
-			}
-			else
-			{
-				pairs += lengths[i] * lengths[60 - i];
-			}
-
+			pos = t % 60;
+			pairs += memo[pos == 0 ? 0 : 60 - pos];
+			memo[pos]++;
 		}
 
 		return pairs;
