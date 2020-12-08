9fc322cfdccd6d554ae9d2dc8dd9d39ff98e7255
rakhmedovrs@gmail.com <rakhmedovrs@gmail.com>
2020-12-08 18:28:49 +0300

    Missing Element in Sorted Array

---
+++ src/main/java/MissingElementInSortedArray.java
@@ -0,0 +1,41 @@
+import common.Difficulty;
+import common.LeetCode;
+
+/**
+ * @author RakhmedovRS
+ * @created 12/8/2020
+ */
+@LeetCode(
+	id = 1060,
+	name = "Missing Element in Sorted Array",
+	url = "https://leetcode.com/problems/missing-element-in-sorted-array/",
+	difficulty = Difficulty.MEDIUM,
+	premium = true
+)
+public class MissingElementInSortedArray
+{
+	public int missingElement(int[] nums, int k)
+	{
+		int prev = nums[0];
+		int diff;
+		for (int i = 1; i < nums.length; i++)
+		{
+			diff = nums[i] - prev - 1;
+			if (diff > 0)
+			{
+				if (k > diff)
+				{
+					k -= diff;
+				}
+				else
+				{
+					return prev + k;
+				}
+			}
+
+			prev = nums[i];
+		}
+
+		return prev + k;
+	}
+}
