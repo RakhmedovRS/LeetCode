2dd7028c476bcf9633d81f1a276b00a5980c124a
rakhmedovrs@gmail.com <rakhmedovrs@gmail.com>
2020-12-08 18:35:13 +0300

    Missing Ranges updated

--- src/main/java/MissingRanges.java
+++ src/main/java/MissingRanges.java
@@ -2,6 +2,7 @@ import common.Difficulty;
 import common.LeetCode;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 /**
@@ -19,44 +20,35 @@ public class MissingRanges
 {
 	public List<String> findMissingRanges(int[] nums, int lower, int upper)
 	{
-		List<String> ranges = new ArrayList<>();
 		if (nums.length == 0)
 		{
-			ranges.add(getRange(lower, upper));
+			return Arrays.asList(getRange(lower, upper));
 		}
-		else
+
+		List<String> answer = new ArrayList<>();
+		if (lower < nums[0])
 		{
-			if (nums[0] > lower)
-			{
-				ranges.add(getRange(lower, nums[0] - 1));
-			}
+			answer.add(getRange(lower, nums[0] - 1));
+		}
 
-			for (int i = 1; i < nums.length; i++)
+		for (int i = 1; i < nums.length; i++)
+		{
+			if (nums[i] - nums[i - 1] != 1)
 			{
-				if (nums[i - 1] + 1 != nums[i])
-				{
-					ranges.add(getRange(nums[i - 1] + 1, nums[i] - 1));
-				}
-			}
-
-			if (nums[nums.length - 1] < upper)
-			{
-				ranges.add(getRange(nums[nums.length - 1] + 1, upper));
+				answer.add(getRange(nums[i - 1] + 1, nums[i] - 1));
 			}
 		}
 
-		return ranges;
+		if (nums[nums.length - 1] < upper)
+		{
+			answer.add(getRange(nums[nums.length - 1] + 1, upper));
+		}
+
+		return answer;
 	}
 
 	private String getRange(int lower, int upper)
 	{
-		if (lower == upper)
-		{
-			return String.valueOf(lower);
-		}
-		else
-		{
-			return lower + "->" + upper;
-		}
+		return lower == upper ? "" + lower : lower + "->" + upper;
 	}
 }
