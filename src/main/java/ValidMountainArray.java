3bde282aa6bb511cbfd1ad3178fbc394abbda538
rakhmedovrs@gmail.com <rakhmedovrs@gmail.com>
2020-12-10 21:22:43 +0300

    Valid Mountain Array updated

--- src/main/java/ValidMountainArray.java
+++ src/main/java/ValidMountainArray.java
@@ -1,60 +1,55 @@
+import common.Difficulty;
 import common.LeetCode;
 
 /**
  * @author RakhmedovRS
  * @created 17-Mar-20
  */
-@LeetCode(id = 941, name = "Valid Mountain Array", url = "https://leetcode.com/problems/valid-mountain-array/")
+@LeetCode(
+	id = 941,
+	name = "Valid Mountain Array",
+	url = "https://leetcode.com/problems/valid-mountain-array/",
+	difficulty = Difficulty.EASY
+)
 public class ValidMountainArray
 {
-	public boolean validMountainArray(int[] numbers)
+	public boolean validMountainArray(int[] arr)
 	{
-		if (numbers == null || numbers.length <= 2)
+		int[] leftToRight = new int[arr.length];
+		for (int i = 0; i < arr.length; i++)
 		{
-			return false;
-		}
-
-		int max = Integer.MIN_VALUE;
-		int maxIndex = 0;
-		for (int i = 0; i < numbers.length; i++)
-		{
-			if (numbers[i] > max)
+			if (i != 0 && arr[i - 1] < arr[i])
 			{
-				max = numbers[i];
-				maxIndex = i;
+				leftToRight[i] = leftToRight[i - 1] + 1;
+			}
+			else
+			{
+				leftToRight[i] = 1;
 			}
 		}
 
-		if (maxIndex == 0 || maxIndex == numbers.length - 1)
+		int[] rightToLeft = new int[arr.length];
+		for (int i = arr.length - 1; i >= 0; i--)
 		{
-			return false;
-		}
-
-		int prev = maxIndex;
-		int next = maxIndex + 1;
-		while (next < numbers.length)
-		{
-			if (numbers[prev] <= numbers[next])
+			if (i != arr.length - 1 && arr[i] > arr[i + 1])
 			{
-				return false;
+				rightToLeft[i] = rightToLeft[i + 1] + 1;
 			}
-			prev++;
-			next++;
-		}
-
-		prev = maxIndex;
-		next = maxIndex - 1;
-		while (next >= 0)
-		{
-			if (numbers[next] >= numbers[prev])
+			else
 			{
-				return false;
+				rightToLeft[i] = 1;
 			}
-			prev--;
-			next--;
 		}
 
-		return true;
+		for (int i = 1; i < arr.length - 1; i++)
+		{
+			if (leftToRight[i] == i + 1 && rightToLeft[i] == arr.length - i)
+			{
+				return true;
+			}
+		}
+
+		return false;
 	}
 
 	public static void main(String[] args)
