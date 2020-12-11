121b578cb48b436b89863dd5d2b2cf7498c9d902
rakhmedovrs@gmail.com <rakhmedovrs@gmail.com>
2020-12-11 20:39:43 +0300

    Minimum Cost to Connect Sticks

---
+++ src/main/java/MinimumCostToConnectSticks.java
@@ -0,0 +1,42 @@
+import common.Difficulty;
+import common.LeetCode;
+
+import java.util.PriorityQueue;
+
+/**
+ * @author RakhmedovRS
+ * @created 12/11/2020
+ */
+@LeetCode(
+	id = 1167,
+	name = "Minimum Cost to Connect Sticks",
+	url = "https://leetcode.com/problems/minimum-cost-to-connect-sticks/",
+	difficulty = Difficulty.MEDIUM,
+	premium = true
+)
+public class MinimumCostToConnectSticks
+{
+	public int connectSticks(int[] sticks)
+	{
+		PriorityQueue<Integer> minHeap = new PriorityQueue<>();
+		for (int stick : sticks)
+		{
+			minHeap.add(stick);
+		}
+
+		int cost = 0;
+		int x;
+		int y;
+		while (minHeap.size() > 1)
+		{
+			x = minHeap.remove();
+			y = minHeap.remove();
+
+			cost += x + y;
+
+			minHeap.add(x + y);
+		}
+
+		return cost;
+	}
+}
