dea39351cf77ea40f6fa7082f21fe474317ba0cc
rakhmedovrs@gmail.com <rakhmedovrs@gmail.com>
2020-12-11 20:34:24 +0300

    How Many Apples Can You Put into the Basket

---
+++ src/main/java/HowManyApplesCanYouPutIntoTheBasket.java
@@ -0,0 +1,34 @@
+import common.Difficulty;
+import common.LeetCode;
+
+import java.util.Arrays;
+
+/**
+ * @author RakhmedovRS
+ * @created 12/11/2020
+ */
+@LeetCode(
+	id = 1196,
+	name = "How Many Apples Can You Put into the Basket",
+	url = "https://leetcode.com/problems/how-many-apples-can-you-put-into-the-basket/",
+	difficulty = Difficulty.EASY,
+	premium = true
+)
+public class HowManyApplesCanYouPutIntoTheBasket
+{
+	public int maxNumberOfApples(int[] arr)
+	{
+		Arrays.sort(arr);
+		int count = 0;
+		int sum = 5000;
+		for (int i = 0; i < arr.length && sum > 0; i++)
+		{
+			sum -= arr[i];
+			if (sum >= 0)
+			{
+				count++;
+			}
+		}
+		return count;
+	}
+}
