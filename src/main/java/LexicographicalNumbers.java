5892cd3b35c4d8484c70da6a5bc1026a4e29a072
rakhmedovrs@gmail.com <rakhmedovrs@gmail.com>
2020-12-16 21:37:57 +0300

    Lexicographical Numbers

---
+++ src/main/java/LexicographicalNumbers.java
@@ -0,0 +1,46 @@
+import common.Difficulty;
+import common.LeetCode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author RakhmedovRS
+ * @created 12/16/2020
+ */
+@LeetCode(
+	id = 386,
+	name = "Lexicographical Numbers",
+	url = "https://leetcode.com/problems/lexicographical-numbers/",
+	difficulty = Difficulty.MEDIUM
+)
+public class LexicographicalNumbers
+{
+	public List<Integer> lexicalOrder(int n)
+	{
+		List<Integer> answer = new ArrayList<>();
+		dfs(answer, n, 0);
+		return answer;
+	}
+
+	private void dfs(List<Integer> answer, int n, int num)
+	{
+		int next;
+		for (int i = 0; i <= 9; i++)
+		{
+			next = num * 10 + i;
+			if (next == 0)
+			{
+				continue;
+			}
+
+			if (next > n)
+			{
+				return;
+			}
+
+			answer.add(next);
+			dfs(answer, n, next);
+		}
+	}
+}
