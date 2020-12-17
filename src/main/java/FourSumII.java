529cf72ae274539bbfe977ee3aa4e8653b29f536
rakhmedovrs@gmail.com <rakhmedovrs@gmail.com>
2020-12-17 19:21:08 +0300

    4Sum II updated

--- src/main/java/FourSumII.java
+++ src/main/java/FourSumII.java
@@ -1,3 +1,4 @@
+import common.Difficulty;
 import common.LeetCode;
 
 import java.util.HashMap;
@@ -7,23 +8,37 @@ import java.util.Map;
  * @author RakhmedovRS
  * @created 19-Aug-20
  */
-@LeetCode(id = 454, name = "4Sum II", url = "https://leetcode.com/problems/4sum-ii/")
+@LeetCode(
+	id = 454,
+	name = "4Sum II",
+	url = "https://leetcode.com/problems/4sum-ii/",
+	difficulty = Difficulty.MEDIUM
+)
 public class FourSumII
 {
-	public int fourSumCount(int[] A, int[] B, int[] C, int[] D) {
+	public int fourSumCount(int[] A, int[] B, int[] C, int[] D)
+	{
 		int count = 0;
-		Map<Integer, Integer> abMap = new HashMap<>();
-		for (int a : A) {
-			for (int b : B) {
-				abMap.put(a + b, abMap.getOrDefault(a + b, 0) + 1);
+		Map<Integer, Integer> ab = new HashMap<>();
+		int sum;
+		for (int a : A)
+		{
+			for (int b : B)
+			{
+				sum = a + b;
+				ab.put(sum, ab.getOrDefault(sum, 0) + 1);
 			}
 		}
 
-		for (int c : C) {
-			for (int d : D) {
-				count += abMap.getOrDefault(-(c + d), 0);
+		for (int c : C)
+		{
+			for (int d : D)
+			{
+				sum = c + d;
+				count += ab.getOrDefault(-sum, 0);
 			}
 		}
+
 		return count;
 	}
 }
