9afd4461a2c9e0ee00fc6329e8792fbc95245bfd
rakhmedovrs@gmail.com <rakhmedovrs@gmail.com>
2020-12-25 21:57:40 +0300

    Shortest Palindrome

---
+++ src/main/java/ShortestPalindrome.java
@@ -0,0 +1,29 @@
+import common.Difficulty;
+import common.LeetCode;
+
+/**
+ * @author RakhmedovRS
+ * @created 12/25/2020
+ */
+@LeetCode(
+	id = 214,
+	name = "Shortest Palindrome",
+	url = "https://leetcode.com/problems/shortest-palindrome/",
+	difficulty = Difficulty.HARD
+)
+public class ShortestPalindrome
+{
+	public String shortestPalindrome(String origin)
+	{
+		String reversed = new StringBuilder(origin).reverse().toString();
+		for (int i = 0; i <= reversed.length(); i++)
+		{
+			if (origin.startsWith(reversed.substring(i)))
+			{
+				return reversed.substring(0, i) + origin;
+			}
+		}
+
+		return reversed + origin;
+	}
+}
