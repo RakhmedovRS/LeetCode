d040b5132bbc29fd1af259296ec263808d1c4c21
rakhmedovrs@gmail.com <rakhmedovrs@gmail.com>
2020-12-26 19:15:37 +0300

    Binary String With Substrings Representing 1 To N

---
+++ src/main/java/BinaryStringWithSubstringsRepresentingOneToN.java
@@ -0,0 +1,28 @@
+import common.Difficulty;
+import common.LeetCode;
+
+/**
+ * @author RakhmedovRS
+ * @created 12/26/2020
+ */
+@LeetCode(
+	id = 1016,
+	name = "Binary String With Substrings Representing 1 To N",
+	url = "https://leetcode.com/problems/binary-string-with-substrings-representing-1-to-n/",
+	difficulty = Difficulty.MEDIUM
+)
+public class BinaryStringWithSubstringsRepresentingOneToN
+{
+	public boolean queryString(String S, int N)
+	{
+		for (int i = 1; i <= N; i++)
+		{
+			if (!S.contains(Integer.toBinaryString(i)))
+			{
+				return false;
+			}
+		}
+
+		return true;
+	}
+}
