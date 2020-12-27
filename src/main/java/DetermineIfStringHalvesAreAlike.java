4375e757923562c9de95b470e35f02eeb51420f2
rakhmedovrs@gmail.com <rakhmedovrs@gmail.com>
2020-12-27 11:43:25 +0300

    Determine if String Halves Are Alike

---
+++ src/main/java/DetermineIfStringHalvesAreAlike.java
@@ -0,0 +1,42 @@
+import common.Difficulty;
+import common.LeetCode;
+
+/**
+ * @author RakhmedovRS
+ * @created 12/27/2020
+ */
+@LeetCode(
+	id = 1704,
+	name = "Determine if String Halves Are Alike",
+	url = "https://leetcode.com/problems/determine-if-string-halves-are-alike/",
+	difficulty = Difficulty.EASY
+)
+public class DetermineIfStringHalvesAreAlike
+{
+	public boolean halvesAreAlike(String s)
+	{
+		int a = 0;
+		for (int i = s.length() / 2; i < s.length(); i++)
+		{
+			char ch = Character.toLowerCase(s.charAt(i));
+
+			if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u')
+			{
+				a++;
+			}
+		}
+
+		int b = 0;
+		for (int i = 0; i < s.length() / 2; i++)
+		{
+			char ch = Character.toLowerCase(s.charAt(i));
+
+			if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u')
+			{
+				b++;
+			}
+		}
+
+		return a == b;
+	}
+}
