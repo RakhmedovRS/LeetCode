5cc92b8913a9438dd23d7d8a42ac5be58b3f90f1
rakhmedovrs@gmail.com <rakhmedovrs@gmail.com>
2020-12-31 13:14:12 +0300

    Largest Rectangle in Histogram updated

--- src/main/java/LargestRectangleInHistogram.java
+++ src/main/java/LargestRectangleInHistogram.java
@@ -1,3 +1,4 @@
+import common.Difficulty;
 import common.LeetCode;
 
 import java.util.Deque;
@@ -7,32 +8,39 @@ import java.util.LinkedList;
  * @author RakhmedovRS
  * @created 26-Feb-20
  */
-@LeetCode(id = 82, name = "Largest Rectangle in Histogram", url = "https://leetcode.com/problems/largest-rectangle-in-histogram/")
+@LeetCode(
+	id = 82,
+	name = "Largest Rectangle in Histogram",
+	url = "https://leetcode.com/problems/largest-rectangle-in-histogram/",
+	difficulty = Difficulty.HARD
+)
 public class LargestRectangleInHistogram
 {
 	public int largestRectangleArea(int[] heights)
 	{
+		int n = heights.length;
 		int maxArea = 0;
-		if (heights == null || heights.length == 0)
+		Deque<Integer> deque = new LinkedList<>();
+		deque.addLast(-1);
+		int height;
+		int width;
+		for (int i = 0; i < n; i++)
 		{
-			return maxArea;
+			while (deque.getLast() != -1 && heights[deque.getLast()] >= heights[i])
+			{
+				height = heights[deque.removeLast()];
+				width = i - deque.getLast() - 1;
+				maxArea = Math.max(maxArea, height * width);
+			}
+
+			deque.addLast(i);
 		}
 
-		Deque<Integer> stack = new LinkedList<>();
-		for (int i = 0; i <= heights.length; i++)
+		while (deque.getLast() != -1)
 		{
-			if (i == heights.length || (!stack.isEmpty() && heights[stack.peek()] > heights[i]))
-			{
-				while (!stack.isEmpty() && (i == heights.length || heights[stack.peek()] > heights[i]))
-				{
-					int curr = heights[stack.pop()] * (stack.isEmpty() ? i : (i - stack.peek() - 1));
-					if (curr > maxArea)
-					{
-						maxArea = curr;
-					}
-				}
-			}
-			stack.push(i);
+			height = heights[deque.removeLast()];
+			width = n - deque.getLast() - 1;
+			maxArea = Math.max(maxArea, height * width);
 		}
 
 		return maxArea;
