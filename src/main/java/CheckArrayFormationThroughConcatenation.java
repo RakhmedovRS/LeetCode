5bb137890fea1ea5a184231751d208877cffbc54
rakhmedovrs@gmail.com <rakhmedovrs@gmail.com>
2021-01-01 17:58:07 +0300

    Check Array Formation Through Concatenation updated

--- src/main/java/CheckArrayFormationThroughConcatenation.java
+++ src/main/java/CheckArrayFormationThroughConcatenation.java
@@ -1,5 +1,7 @@
+import common.Difficulty;
 import common.LeetCode;
 
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -7,24 +9,44 @@ import java.util.Map;
  * @author RakhmedovRS
  * @created 11/1/2020
  */
-@LeetCode(id = 1640, name = "Check Array Formation Through Concatenation", url = "https://leetcode.com/problems/check-array-formation-through-concatenation/")
+@LeetCode(
+	id = 1640,
+	name = "Check Array Formation Through Concatenation",
+	url = "https://leetcode.com/problems/check-array-formation-through-concatenation/",
+	difficulty = Difficulty.EASY
+)
 public class CheckArrayFormationThroughConcatenation
 {
 	public boolean canFormArray(int[] arr, int[][] pieces)
 	{
-		Map<Integer, Integer> map = new HashMap<>();
+		int[] memo = new int[101];
+		Arrays.fill(memo, -1);
+
 		for (int i = 0; i < arr.length; i++)
 		{
-			map.put(arr[i], i);
+			memo[arr[i]] = i;
 		}
 
-		int index;
+		int i;
+		int j;
 		for (int[] piece : pieces)
 		{
-			index = map.getOrDefault(piece[0], arr.length);
-			for (int i = 0; i < piece.length; i++)
+			if (piece.length > 0)
 			{
-				if (index == arr.length || piece[i] != arr[index++])
+				i = 0;
+				j = memo[piece[0]];
+				while (i < piece.length && j != -1 && j < arr.length)
+				{
+					if (arr[j] != piece[i])
+					{
+						return false;
+					}
+
+					j++;
+					memo[piece[i++]] = -1;
+				}
+
+				if (i < piece.length)
 				{
 					return false;
 				}
