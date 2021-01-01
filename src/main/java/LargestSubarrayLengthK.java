74c3917e70d8d27ec87d9613d4b17ee35f5d71fc
rakhmedovrs@gmail.com <rakhmedovrs@gmail.com>
2021-01-01 17:06:09 +0300

    Largest Subarray Length K

---
+++ src/main/java/LargestSubarrayLengthK.java
@@ -0,0 +1,36 @@
+import common.Difficulty;
+import common.LeetCode;
+
+/**
+ * @author RakhmedovRS
+ * @created 1/1/2021
+ */
+@LeetCode(
+	id = 1708,
+	name = "Largest Subarray Length K",
+	url = "https://leetcode.com/problems/largest-subarray-length-k/",
+	difficulty = Difficulty.MEDIUM,
+	premium = true
+)
+public class LargestSubarrayLengthK
+{
+	public int[] largestSubarray(int[] nums, int k)
+	{
+		int maxPos = 0;
+		for (int i = 0; i <= nums.length - k; i++)
+		{
+			if (nums[i] > nums[maxPos])
+			{
+				maxPos = i;
+			}
+		}
+
+		int[] answer = new int[k];
+		for (int i = 0; i < k; i++)
+		{
+			answer[i] = nums[maxPos++];
+		}
+
+		return answer;
+	}
+}
