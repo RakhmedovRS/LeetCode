5beb8aa4d3a6df1ad8b0fb0ffea72bfe90826e67
rakhmedovrs@gmail.com <rakhmedovrs@gmail.com>
2021-01-04 12:14:37 +0300

    Merge Two Sorted Lists updated

--- src/main/java/MergeTwoSortedLists.java
+++ src/main/java/MergeTwoSortedLists.java
@@ -1,3 +1,4 @@
+import common.Difficulty;
 import common.LeetCode;
 import common.ListNode;
 
@@ -5,46 +6,46 @@ import common.ListNode;
  * @author RakhmedovRS
  * @created 14-Mar-20
  */
-@LeetCode(id = 21, name = "Merge Two Sorted Lists", url = "https://leetcode.com/problems/merge-two-sorted-lists/")
+@LeetCode(
+	id = 21,
+	name = "Merge Two Sorted Lists",
+	url = "https://leetcode.com/problems/merge-two-sorted-lists/",
+	difficulty = Difficulty.EASY
+)
 public class MergeTwoSortedLists
 {
-public ListNode mergeTwoLists(ListNode l1, ListNode l2)
+	public ListNode mergeTwoLists(ListNode l1, ListNode l2)
 	{
-		if (l1 == null && l2 == null)
-		{
-			return null;
-		}
-
 		ListNode dummy = new ListNode(0);
 		ListNode current = dummy;
-
 		while (l1 != null && l2 != null)
 		{
-			if (l1.val > l2.val)
+			if (l1.val <= l2.val)
 			{
-				current.next = l2;
-				l2 = l2.next;
+				current.next = l1;
+				current = current.next;
+				l1 = l1.next;
 			}
 			else
 			{
-				current.next = l1;
-				l1 = l1.next;
+				current.next = l2;
+				current = current.next;
+				l2 = l2.next;
 			}
-			current = current.next;
 		}
 
 		while (l1 != null)
 		{
 			current.next = l1;
-			l1 = l1.next;
 			current = current.next;
+			l1 = l1.next;
 		}
 
 		while (l2 != null)
 		{
 			current.next = l2;
-			l2 = l2.next;
 			current = current.next;
+			l2 = l2.next;
 		}
 
 		return dummy.next;
