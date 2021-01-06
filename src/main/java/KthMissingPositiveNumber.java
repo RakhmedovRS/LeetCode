f074a5e82088009495b5cafb51609cf37ab1aae1
rakhmedovrs@gmail.com <rakhmedovrs@gmail.com>
2021-01-06 11:31:11 +0300

    Kth Missing Positive Number updated

--- src/main/java/KthMissingPositiveNumber.java
+++ src/main/java/KthMissingPositiveNumber.java
@@ -1,36 +1,49 @@
+import common.Difficulty;
 import common.LeetCode;
 
-import java.util.HashSet;
-import java.util.Set;
 
 /**
  * @author RakhmedovRS
  * @created 08-Aug-20
  */
-@LeetCode(id = 1539, name = "Kth Missing Positive Number", url = "https://leetcode.com/problems/kth-missing-positive-number/")
+@LeetCode(
+	id = 1539,
+	name = "Kth Missing Positive Number",
+	url = "https://leetcode.com/problems/kth-missing-positive-number/",
+	difficulty = Difficulty.EASY)
 public class KthMissingPositiveNumber
 {
 	public int findKthPositive(int[] arr, int k)
 	{
-		Set<Integer> set = new HashSet<>();
-		for (int num : arr)
+		int diff;
+		int prev = 0;
+		for (int curr : arr)
 		{
-			set.add(num);
-		}
-		int i = 1;
-		for (; i < Integer.MAX_VALUE; i++)
-		{
-			if (!set.contains(i))
+			diff = curr - prev;
+			if (diff != 1)
 			{
-				k--;
+				if (diff > k)
+				{
+					return prev + k;
+				}
+				else
+				{
+					k -= diff - 1;
+				}
 			}
 
-			if (k == 0)
-			{
-				break;
-			}
+			prev = curr;
 		}
 
-		return i;
+		return prev + k;
+	}
+
+	public static void main(String[] args)
+	{
+		KthMissingPositiveNumber clazz = new KthMissingPositiveNumber();
+		System.out.println(clazz.findKthPositive(new int[]{8, 17, 23, 34, 37, 42}, 16)); //18
+		System.out.println(clazz.findKthPositive(new int[]{2, 3, 4, 7, 11}, 5)); //9
+		System.out.println(clazz.findKthPositive(new int[]{1, 2, 3, 4}, 2)); //6
+		System.out.println(clazz.findKthPositive(new int[]{2}, 1)); //1
 	}
 }
