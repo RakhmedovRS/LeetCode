2ba91bb25a34317d048dfb82ba7151340a32ee1e
rakhmedovrs@gmail.com <rakhmedovrs@gmail.com>
2021-01-08 13:10:51 +0300

    Palindrome Number

---
+++ src/main/java/PalindromeNumber.java
@@ -0,0 +1,34 @@
+import common.Difficulty;
+import common.LeetCode;
+
+/**
+ * @author RakhmedovRS
+ * @created 1/8/2021
+ */
+@LeetCode(
+	id = 9,
+	name = "Palindrome Number",
+	url = "https://leetcode.com/problems/palindrome-number/",
+	difficulty = Difficulty.EASY
+)
+public class PalindromeNumber
+{
+	public boolean isPalindrome(int x)
+	{
+		if (x < 0)
+		{
+			return false;
+		}
+
+		long reversed = 0L;
+		int temp = x;
+		while (temp > 0)
+		{
+			reversed *= 10;
+			reversed += temp % 10;
+			temp /= 10;
+		}
+
+		return reversed == x;
+	}
+}
