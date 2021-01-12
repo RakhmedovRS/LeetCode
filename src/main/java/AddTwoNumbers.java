449adf25ab61c7a8cce5ccc586667272c7b59ca2
rakhmedovrs@gmail.com <rakhmedovrs@gmail.com>
2021-01-12 21:54:26 +0300

    Add Two Numbers updated

--- src/main/java/AddTwoNumbers.java
+++ src/main/java/AddTwoNumbers.java
@@ -1,3 +1,4 @@
+import common.Difficulty;
 import common.LeetCode;
 import common.ListNode;
 
@@ -5,45 +6,39 @@ import common.ListNode;
  * @author RakhmedovRS
  * @created 01-Apr-20
  */
-@LeetCode(id = 2, name = "Add Two Numbers", url = "https://leetcode.com/problems/add-two-numbers/")
+@LeetCode(
+	id = 2,
+	name = "Add Two Numbers",
+	url = "https://leetcode.com/problems/add-two-numbers/",
+	difficulty = Difficulty.MEDIUM
+)
 public class AddTwoNumbers
 {
 	public ListNode addTwoNumbers(ListNode l1, ListNode l2)
 	{
 		ListNode dummy = new ListNode(0);
-		ListNode current = dummy;
-		boolean hasRest = false;
-		while (l1 != null || l2 != null || hasRest)
+		ListNode prev = dummy;
+		int reminder = 0;
+		int result;
+		while (l1 != null || l2 != null || reminder > 0)
 		{
-			int l1Value = 0;
+			result = 0;
 			if (l1 != null)
 			{
-				l1Value = l1.val;
+				result += l1.val;
 				l1 = l1.next;
 			}
 
-			int l2Value = 0;
 			if (l2 != null)
 			{
-				l2Value = l2.val;
+				result += l2.val;
 				l2 = l2.next;
 			}
 
-			ListNode node = new ListNode(0);
-			node.val += l1Value + l2Value;
-			if (hasRest)
-			{
-				node.val += 1;
-				hasRest = false;
-			}
-
-			if (node.val >= 10)
-			{
-				node.val %= 10;
-				hasRest = true;
-			}
-			current.next = node;
-			current = current.next;
+			result += reminder;
+			prev.next = new ListNode(result % 10);
+			reminder = result / 10;
+			prev = prev.next;
 		}
 
 		return dummy.next;
