46bf4c173d452bb4360ee8a01675036413b6c484
rakhmedovrs@gmail.com <rakhmedovrs@gmail.com>
2021-01-13 20:03:24 +0300

    Inorder Successor in BST II

---
+++ src/main/java/InorderSuccessorInBSTII.java
@@ -0,0 +1,54 @@
+import common.Difficulty;
+import common.LeetCode;
+
+/**
+ * @author RakhmedovRS
+ * @created 1/13/2021
+ */
+@LeetCode(
+	id = 510,
+	name = "Inorder Successor in BST II",
+	url = "https://leetcode.com/problems/inorder-successor-in-bst-ii/",
+	difficulty = Difficulty.MEDIUM,
+	premium = true
+)
+public class InorderSuccessorInBSTII
+{
+	class Node
+	{
+		public int val;
+		public Node left;
+		public Node right;
+		public Node parent;
+	}
+
+	public Node inorderSuccessor(Node node)
+	{
+		if (node.right != null)
+		{
+			Node n = node.right;
+			while (n.left != null)
+			{
+				n = n.left;
+			}
+
+			return n;
+		}
+		else
+		{
+			Node parent;
+			while (node != null)
+			{
+				parent = node.parent;
+				if (parent != null && parent.left == node)
+				{
+					return parent;
+				}
+
+				node = parent;
+			}
+		}
+
+		return null;
+	}
+}
