b523964fb1790d0339b9addba4b04485a027452b
rakhmedovrs@gmail.com <rakhmedovrs@gmail.com>
2021-01-15 19:57:22 +0300

    Get Maximum in Generated Array updated

--- src/main/java/GetMaximumInGeneratedArray.java
+++ src/main/java/GetMaximumInGeneratedArray.java
@@ -1,42 +1,41 @@
+import common.Difficulty;
 import common.LeetCode;
 
 /**
  * @author RakhmedovRS
  * @created 11/8/2020
  */
-@LeetCode(id = 1646, name = "Get Maximum in Generated Array", url = "https://leetcode.com/problems/get-maximum-in-generated-array/")
+@LeetCode(
+	id = 1646,
+	name = "Get Maximum in Generated Array",
+	url = "https://leetcode.com/problems/get-maximum-in-generated-array/",
+	difficulty = Difficulty.EASY
+)
 public class GetMaximumInGeneratedArray
 {
 	public int getMaximumGenerated(int n)
 	{
-		if (n == 0)
+		if (n <= 1)
 		{
-			return 0;
-		}
-		else if (n == 1)
-		{
-			return 1;
-		}
-
-		int[] values = new int[n + 1];
-		values[0] = 0;
-		values[1] = 1;
-		for (int i = 2; i < values.length; i++)
-		{
-			if (i % 2 == 0)
-			{
-				values[i] = values[i / 2];
-			}
-			else
-			{
-				values[i] = values[i / 2] + values[i / 2 + 1];
-			}
+			return n;
 		}
 
 		int max = 0;
-		for (int val : values)
+		int[] memo = new int[n + 1];
+		memo[0] = 0;
+		memo[1] = 1;
+		for (int i = 2; i <= n; i++)
 		{
-			max = Math.max(max, val);
+			if (i % 2 == 0)
+			{
+				memo[i] = memo[i / 2];
+			}
+			else
+			{
+				memo[i] = memo[i / 2] + memo[i / 2 + 1];
+			}
+
+			max = Math.max(max, memo[i]);
 		}
 
 		return max;
