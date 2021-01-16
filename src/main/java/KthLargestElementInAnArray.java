f746fbec8f4729a2f41b4f77f37485fcc9f2dde6
rakhmedovrs@gmail.com <rakhmedovrs@gmail.com>
2021-01-16 15:18:57 +0300

    Kth Largest Element in an Array updated

--- src/main/java/KthLargestElementInAnArray.java
+++ src/main/java/KthLargestElementInAnArray.java
@@ -1,49 +1,23 @@
+import common.Difficulty;
 import common.LeetCode;
+
+import java.util.Arrays;
 
 /**
  * @author RakhmedovRS
  * @created 06-Feb-20
  */
-@LeetCode(id = 215, name = "Kth Largest Element in an Array", url = "https://leetcode.com/problems/kth-largest-element-in-an-array/")
+@LeetCode(
+	id = 215,
+	name = "Kth Largest Element in an Array",
+	url = "https://leetcode.com/problems/kth-largest-element-in-an-array/",
+	difficulty = Difficulty.EASY
+)
 public class KthLargestElementInAnArray
 {
-	private void swap(int[] nums, int left, int right)
+	public int findKthLargest(int[] nums, int k)
 	{
-		int temp = nums[left];
-		nums[left] = nums[right];
-		nums[right] = temp;
-	}
-
-	private int partition(int[] nums, int left, int right)
-	{
-		int pivot = nums[right];
-		int low = left;
-		for (int i = left; i < right; i++)
-		{
-			if (nums[i] < pivot)
-			{
-				swap(nums, i , low);
-				low++;
-			}
-		}
-
-		swap(nums, low, right);
-		return low;
-	}
-
-	private void quickSort(int[] nums, int left, int right)
-	{
-		if (left >= right)
-		{
-			return;
-		}
-		int pivot = partition(nums, left, right);
-		quickSort(nums, left, pivot - 1);
-		quickSort(nums, pivot + 1, right);
-	}
-
-	public int findKthLargest(int[] nums, int k) {
-		quickSort(nums, 0, nums.length - 1);
+		Arrays.sort(nums);
 		return nums[nums.length - k];
 	}
 }
