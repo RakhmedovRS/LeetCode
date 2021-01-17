5a5b00473895f33a6ce9016b6901170c98d0f1ac
rakhmedovrs@gmail.com <rakhmedovrs@gmail.com>
2021-01-17 18:16:15 +0300

    Prime Palindrome

---
+++ src/main/java/PrimePalindrome.java
@@ -0,0 +1,67 @@
+import common.Difficulty;
+import common.LeetCode;
+
+/**
+ * @author RakhmedovRS
+ * @created 1/17/2021
+ */
+@LeetCode(
+	id = 866,
+	name = "Prime Palindrome",
+	url = "https://leetcode.com/problems/prime-palindrome/",
+	difficulty = Difficulty.MEDIUM
+)
+public class PrimePalindrome
+{
+	public int primePalindrome(int N)
+	{
+		while (true)
+		{
+			if (isPrime(N) && N == reverseNumber(N))
+			{
+				return N;
+			}
+			N++;
+			if (10_000_000 < N && N < 100_000_000)
+			{
+				N = 100_000_000;
+			}
+		}
+	}
+
+	private boolean isPrime(int number)
+	{
+		if (number == 1)
+		{
+			return false;
+		}
+		int root = (int) Math.sqrt(number);
+		for (int i = 2; i <= root; i++)
+		{
+			if (number % i == 0)
+			{
+				return false;
+			}
+		}
+
+		return true;
+	}
+
+	private int reverseNumber(int number)
+	{
+		int num = 0;
+		while (number > 0)
+		{
+			num *= 10;
+			num += number % 10;
+			number /= 10;
+		}
+		return num;
+	}
+
+	public static void main(String[] args)
+	{
+		PrimePalindrome clazz = new PrimePalindrome();
+		System.out.println(clazz.primePalindrome(8));
+	}
+}
