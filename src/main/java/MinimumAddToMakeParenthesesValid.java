c97db1aa516d6c2ae263f8a3e01fe4c7e9af50c2
rakhmedovrs@gmail.com <rakhmedovrs@gmail.com>
2021-01-22 18:47:51 +0300

    Minimum Add to Make Parentheses Valid

---
+++ src/main/java/MinimumAddToMakeParenthesesValid.java
@@ -0,0 +1,50 @@
+import common.Difficulty;
+import common.LeetCode;
+
+import java.util.LinkedList;
+
+/**
+ * @author RakhmedovRS
+ * @created 1/22/2021
+ */
+@LeetCode(
+	id = 921,
+	name = "Minimum Add to Make Parentheses Valid",
+	url = "https://leetcode.com/problems/minimum-add-to-make-parentheses-valid/",
+	difficulty = Difficulty.MEDIUM
+)
+public class MinimumAddToMakeParenthesesValid
+{
+	public int minAddToMakeValid(String S)
+	{
+		int additions = 0;
+		LinkedList<Character> stack = new LinkedList<>();
+		for (char ch : S.toCharArray())
+		{
+			if (ch == '(')
+			{
+				if (!stack.isEmpty() && stack.peek() == ')')
+				{
+					additions += stack.size();
+					stack.clear();
+				}
+				stack.push(ch);
+			}
+			else
+			{
+				if (!stack.isEmpty() && stack.peek() == '(')
+				{
+					stack.pop();
+				}
+				else
+				{
+					stack.push(ch);
+				}
+			}
+		}
+
+		additions += stack.size();
+
+		return additions;
+	}
+}
