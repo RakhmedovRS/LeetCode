f142f7443b5c7c62f9da1a4eabcba7b2e96cef0b
rakhmedovrs@gmail.com <rakhmedovrs@gmail.com>
2021-01-22 18:48:35 +0300

    One Edit Distance updated

--- src/main/java/OneEditDistance.java
+++ src/main/java/OneEditDistance.java
@@ -25,43 +25,39 @@ public class OneEditDistance
 			return true;
 		}
 
-		return dfs(0, 0, s, t, false, new Boolean[s.length()][t.length()]);
+		Boolean[][] memo = new Boolean[s.length()][t.length()];
+		return dfs(0, 0, s.toCharArray(), t.toCharArray(), memo, false);
 	}
 
-	private boolean dfs(int sPos, int tPos, String source, String target, boolean edited, Boolean[][] memo)
+	private boolean dfs(int posA, int posB, char[] aChars, char[] bChars, Boolean[][] memo, boolean seenMismatch)
 	{
-		if (sPos == source.length())
+		if (posA == aChars.length)
 		{
-			return target.length() - tPos == (edited ? 0 : 1);
+			return bChars.length - posB == (seenMismatch ? 0 : 1);
 		}
-		else if (tPos == target.length())
+		else if (posB == bChars.length)
 		{
-			return source.length() - sPos == (edited ? 0 : 1);
-		}
-
-		if (memo[sPos][tPos] != null)
-		{
-			return memo[sPos][tPos];
+			return aChars.length - posA == (seenMismatch ? 0 : 1);
 		}
 
-		if (source.charAt(sPos) == target.charAt(tPos))
+		if (memo[posA][posB] != null)
 		{
-			memo[sPos][tPos] = dfs(sPos + 1, tPos + 1, source, target, edited, memo);
-		}
-		else
-		{
-			if (edited)
-			{
-				memo[sPos][tPos] = false;
-				return false;
-			}
-
-			boolean a = dfs(sPos + 1, tPos + 1, source, target, true, memo);
-			boolean b = dfs(sPos + 1, tPos, source, target, true, memo);
-			boolean c = dfs(sPos, tPos + 1, source, target, true, memo);
-			memo[sPos][tPos] = a || b || c;
+			return memo[posA][posB];
 		}
 
-		return memo[sPos][tPos];
+		boolean result = false;
+		if (aChars[posA] == bChars[posB])
+		{
+			result = dfs(posA + 1, posB + 1, aChars, bChars, memo, seenMismatch);
+		}
+		else if (!seenMismatch)
+		{
+			result = dfs(posA + 1, posB + 1, aChars, bChars, memo, true)
+				|| dfs(posA + 1, posB, aChars, bChars, memo, true)
+				|| dfs(posA, posB + 1, aChars, bChars, memo, true);
+		}
+
+		memo[posA][posB] = result;
+		return result;
 	}
 }
