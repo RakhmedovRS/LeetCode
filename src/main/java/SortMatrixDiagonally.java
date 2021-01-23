a457bbf8d9b989ee986cac3d1ebbffa1e726898e
rakhmedovrs@gmail.com <rakhmedovrs@gmail.com>
2021-01-23 12:18:04 +0300

    Sort the Matrix Diagonally updated

--- src/main/java/SortMatrixDiagonally.java
+++ src/main/java/SortMatrixDiagonally.java
@@ -1,3 +1,4 @@
+import common.Difficulty;
 import common.LeetCode;
 
 import java.util.ArrayList;
@@ -8,46 +9,52 @@ import java.util.List;
  * @author RakhmedovRS
  * @created 15-Jun-20
  */
-@LeetCode(id = 1329, name = "Sort the Matrix Diagonally", url = "https://leetcode.com/problems/sort-the-matrix-diagonally/")
+@LeetCode(
+	id = 1329,
+	name = "Sort the Matrix Diagonally",
+	url = "https://leetcode.com/problems/sort-the-matrix-diagonally/",
+	difficulty = Difficulty.MEDIUM
+)
 public class SortMatrixDiagonally
 {
-	public int[][] diagonalSort(int[][] mat)
+	public int[][] diagonalSort(int[][] matrix)
 	{
-		List<Integer> values = new ArrayList<>();
-		int rows = mat.length;
-		int columns = mat[0].length;
-
+		int rows = matrix.length;
+		int columns = matrix[0].length;
 		for (int row = 0; row < rows; row++)
 		{
-			sortDiagonal(mat, row, 0, rows, columns);
+			List<Integer> nums = new ArrayList<>();
+			for (int r = row, c = 0; r < rows && c < columns; r++,c++)
+			{
+				nums.add(matrix[r][c]);
+			}
+
+			nums.sort(null);
+
+			int pos = 0;
+			for (int r = row, c = 0; r < rows && c < columns; r++,c++)
+			{
+				matrix[r][c] = nums.get(pos++);
+			}
 		}
 
 		for (int column = 1; column < columns; column++)
 		{
-			sortDiagonal(mat, 0, column, rows, columns);
+			List<Integer> nums = new ArrayList<>();
+			for (int r = 0, c = column; r < rows && c < columns; r++,c++)
+			{
+				nums.add(matrix[r][c]);
+			}
+
+			nums.sort(null);
+
+			int pos = 0;
+			for (int r = 0, c = column; r < rows && c < columns; r++,c++)
+			{
+				matrix[r][c] = nums.get(pos++);
+			}
 		}
 
-		return mat;
-	}
-
-	private void sortDiagonal(int[][] mat, int row, int column, int rows, int columns)
-	{
-		List<Integer> values = new ArrayList<>();
-		int r = row;
-		int c = column;
-		while (r < rows && c < columns)
-		{
-			values.add(mat[r++][c++]);
-		}
-
-		values.sort(Comparator.naturalOrder());
-
-		r = row;
-		c = column;
-		int i = 0;
-		while (r < rows && c < columns)
-		{
-			mat[r++][c++] = values.get(i++);
-		}
+		return matrix;
 	}
 }
