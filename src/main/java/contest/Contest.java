4a62932297369e1480b4e62ce90dc6516d3bda7c
rakhmedovrs@gmail.com <rakhmedovrs@gmail.com>
2021-01-24 12:22:12 +0300

    Contest updated

--- src/main/java/contest/Contest.java
+++ src/main/java/contest/Contest.java
@@ -1,13 +1,8 @@
 package contest;
 
-import common.ListNode;
-
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
-import java.util.Arrays;
-import java.util.Comparator;
-import java.util.PriorityQueue;
 import java.util.stream.Collectors;
 
 /**
@@ -17,52 +12,11 @@ import java.util.stream.Collectors;
 public class Contest
 {
 
-	public ListNode mergeKLists(ListNode[] lists)
-	{
-		if (lists == null || lists.length == 0)
-		{
-			return null;
-		}
 
-		ListNode dummy = new ListNode(0);
-		ListNode prev = dummy;
-		PriorityQueue<ListNode> minHeap = new PriorityQueue<>(Comparator.comparingInt(head -> head.val));
-		for (ListNode node : lists)
-		{
-			if (node != null)
-			{
-				minHeap.add(node);
-			}
-		}
-
-		ListNode current;
-		ListNode next;
-		while (!minHeap.isEmpty())
-		{
-			current = minHeap.remove();
-
-			if(current == null)
-			{
-				continue;
-			}
-
-			next = current.next;
-			current.next = null;
-			prev.next = current;
-			prev = current;
-			if (next != null)
-			{
-				minHeap.add(next);
-			}
-		}
-
-		return dummy.next;
-	}
 
 	public static void main(String[] args) throws Exception
 	{
 		Contest clazz = new Contest();
-		System.out.println(clazz.mergeKLists(new ListNode[]{}));
 	}
 
 	private static String readBigTestCase() throws FileNotFoundException
