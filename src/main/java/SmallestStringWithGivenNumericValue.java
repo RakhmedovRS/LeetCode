5ddc2173e3f362c460b6265e028aa57ae2eaf354
rakhmedovrs <rakhmedov.r.sak@sberbank.ru>
2021-01-28 12:20:43 +0200

    Smallest String With A Given Numeric Value updated

--- src/main/java/SmallestStringWithGivenNumericValue.java
+++ src/main/java/SmallestStringWithGivenNumericValue.java
@@ -1,35 +1,44 @@
 import common.Difficulty;
 import common.LeetCode;
+import contest.Contest;
 
 /**
  * @author RakhmedovRS
  * @created 11/22/2020
  */
 @LeetCode(
-	id = 1663,
-	name = "Smallest String With A Given Numeric Value",
-	url = "https://leetcode.com/problems/smallest-string-with-a-given-numeric-value/",
-	difficulty = Difficulty.MEDIUM
+        id = 1663,
+        name = "Smallest String With A Given Numeric Value",
+        url = "https://leetcode.com/problems/smallest-string-with-a-given-numeric-value/",
+        difficulty = Difficulty.MEDIUM
 )
 public class SmallestStringWithGivenNumericValue
 {
-	public String getSmallestString(int n, int k)
-	{
-		StringBuilder sb = new StringBuilder();
-		while (n > 0)
-		{
-			int ch = 1;
-			while (k - ch > ((n - 1) * 26))
-			{
-				ch++;
-			}
+    public String getSmallestString(int n, int k)
+    {
+        char[] chars = new char[n];
+        int max;
+        for (int i = 1; i <= n; i++)
+        {
+            max = 26 * (n - i);
+            for (int ch = 1; ch <= 26; ch++)
+            {
+                if (k - ch <= max)
+                {
+                    chars[i - 1] = (char) (ch - 1 + 'a');
+                    k -= ch;
+                    break;
+                }
+            }
+        }
 
-			sb.append((char) (ch - 1 + 'a'));
+        return String.valueOf(chars);
+    }
 
-			k -= ch;
-			n--;
-		}
-
-		return sb.toString();
-	}
+    public static void main(String[] args) throws Exception
+    {
+        SmallestStringWithGivenNumericValue clazz = new SmallestStringWithGivenNumericValue();
+        System.out.println(clazz.getSmallestString(3, 27));
+        System.out.println(clazz.getSmallestString(5, 73));
+    }
 }
