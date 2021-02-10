c54bb289905318595f7842b129d262fd2789bd83
rakhmedovrs@gmail.com <rakhmedovrs@gmail.com>
2021-02-10 20:37:50 +0300

    Employee Free Time updated

--- src/main/java/EmployeeFreeTime.java
+++ src/main/java/EmployeeFreeTime.java
@@ -2,6 +2,7 @@ import common.Difficulty;
 import common.LeetCode;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 /**
@@ -17,7 +18,7 @@ import java.util.List;
 )
 public class EmployeeFreeTime
 {
-	class Interval
+	static class Interval
 	{
 		public int start;
 		public int end;
@@ -82,5 +83,54 @@ public class EmployeeFreeTime
 		}
 
 		return intervals;
+	}
+
+	public static void main(String[] args)
+	{
+		EmployeeFreeTime clazz = new EmployeeFreeTime();
+		System.out.println(clazz.employeeFreeTime(
+			Arrays.asList(
+				Arrays.asList(new Interval(1, 2), new Interval(5, 6)),
+				Arrays.asList(new Interval(1, 3)),
+				Arrays.asList(new Interval(4, 10))
+			)
+		));
+
+		System.out.println(clazz.employeeFreeTime(
+			Arrays.asList(
+				Arrays.asList(
+					new Interval(11, 31), new Interval(43, 51),
+					new Interval(53, 59), new Interval(61, 62),
+					new Interval(68, 79)
+				),
+				Arrays.asList(
+					new Interval(6, 36), new Interval(40, 55),
+					new Interval(87, 88), new Interval(89, 93),
+					new Interval(97, 98)
+				),
+				Arrays.asList(
+					new Interval(6, 38), new Interval(49, 62),
+					new Interval(71, 73), new Interval(85, 91),
+					new Interval(98, 99)
+				),
+				Arrays.asList(
+					new Interval(2, 4), new Interval(38, 40),
+					new Interval(61, 65), new Interval(67, 80),
+					new Interval(82, 89)
+				),
+				Arrays.asList(
+					new Interval(8, 18), new Interval(25, 29),
+					new Interval(54, 58), new Interval(65, 74),
+					new Interval(85, 93)
+				)
+			)
+		));
+
+		System.out.println(clazz.employeeFreeTime(
+			Arrays.asList(
+				Arrays.asList(new Interval(43, 76), new Interval(86, 91)),
+				Arrays.asList(new Interval(17, 20), new Interval(29, 30))
+			)
+		));
 	}
 }
