a3337b06576c22cdfe136acb93b256ad60815b49
rakhmedovrs <rakhmedov.r.sak@sberbank.ru>
2021-02-13 11:38:16 +0300

    Shortest Path in Binary Matrix

--- src/main/java/ShortestPathInBinaryMatrix.java
+++ src/main/java/ShortestPathInBinaryMatrix.java
@@ -1,5 +1,7 @@
+import common.Difficulty;
 import common.LeetCode;
 
+import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.Queue;
 
@@ -7,51 +9,76 @@ import java.util.Queue;
  * @author RakhmedovRS
  * @created 22-May-20
  */
-@LeetCode(id = 1091, name = "Shortest Path in Binary Matrix", url = "https://leetcode.com/problems/shortest-path-in-binary-matrix/")
+@LeetCode(
+        id = 1091,
+        name = "Shortest Path in Binary Matrix",
+        url = "https://leetcode.com/problems/shortest-path-in-binary-matrix/",
+        difficulty = Difficulty.MEDIUM
+)
 public class ShortestPathInBinaryMatrix
 {
-	private int[][] directions = new int[][]{{0, -1}, {0, 1}, {-1, 0}, {1, 0}, {-1, -1}, {-1, 1}, {1, -1}, {1, 1}};
+    public int shortestPathBinaryMatrix(int[][] grid)
+    {
+        int rows = grid.length;
+        int columns = grid[0].length;
+        if (grid[0][0] == 1)
+        {
+            return -1;
+        }
 
-	public int shortestPathBinaryMatrix(int[][] grid)
-	{
-		int rows = grid.length;
-		int columns = grid[0].length;
-		if (grid[0][0] == 1 || grid[rows - 1][columns - 1] == 1)
-		{
-			return -1;
-		}
+        int[][] memo = new int[rows][columns];
+        for (int[] row : memo)
+        {
+            Arrays.fill(row, Integer.MAX_VALUE);
+        }
 
-		boolean[][] visited = new boolean[rows][columns];
-		visited[0][0] = true;
-		Queue<int[]> queue = new LinkedList<>();
-		queue.add(new int[]{0, 0});
-		int steps = 0;
-		while (!queue.isEmpty())
-		{
-			int size = queue.size();
-			while (size-- > 0)
-			{
-				int[] vertex = queue.poll();
-				if (vertex[0] == rows - 1 && vertex[1] == columns - 1)
-				{
-					return steps + 1;
-				}
+        Queue<int[]> queue = new LinkedList<>();
+        queue.add(new int[]{0, 0, 1});
+        int[] current;
+        int row;
+        int column;
+        int nextRow;
+        int nextColumn;
+        int path;
+        while (!queue.isEmpty())
+        {
+            current = queue.remove();
+            row = current[0];
+            column = current[1];
+            path = current[2];
 
-				for (int[] direction : directions)
-				{
-					int x = vertex[0] + direction[0];
-					int y = vertex[1] + direction[1];
+            if (path >= memo[row][column])
+            {
+                continue;
+            }
 
-					if (x >= 0 && x < rows && y >= 0 && y < columns && !visited[x][y] && grid[x][y] == 0)
-					{
-						visited[x][y] = true;
-						queue.add(new int[]{x, y});
-					}
-				}
-			}
-			steps++;
-		}
+            memo[row][column] = path;
 
-		return -1;
-	}
+            for (int r = -1; r <= 1; r++)
+            {
+                for (int c = -1; c <= 1; c++)
+                {
+                    if (r == 0 && c == 0)
+                    {
+                        continue;
+                    }
+
+                    nextRow = row + r;
+                    nextColumn = column + c;
+                    if (nextRow >= 0
+                            && nextRow < rows
+                            && nextColumn >= 0
+                            && nextColumn < columns
+                            && path + 1 < memo[nextRow][nextColumn]
+                            && grid[nextRow][nextColumn] == 0
+                    )
+                    {
+                        queue.add(new int[]{nextRow, nextColumn, path + 1});
+                    }
+                }
+            }
+        }
+
+        return memo[rows - 1][columns - 1] == Integer.MAX_VALUE ? -1 : memo[rows - 1][columns - 1];
+    }
 }
