0e9eab920f8182e91c852d2a32f5d5fb987b3f30
rakhmedovrs@gmail.com <rakhmedovrs@gmail.com>
2021-02-15 18:07:51 +0300

    The K Weakest Rows in a Matrix updated

--- src/main/java/TheKWeakestRowsInMatrix.java
+++ src/main/java/TheKWeakestRowsInMatrix.java
@@ -1,74 +1,53 @@
+import common.Difficulty;
 import common.LeetCode;
 
-import java.util.HashMap;
-import java.util.Map;
 import java.util.PriorityQueue;
 
 /**
  * @author RakhmedovRS
  * @created 26-May-20
  */
-@LeetCode(id = 1337, name = "The K Weakest Rows in a Matrix", url = "https://leetcode.com/problems/the-k-weakest-rows-in-a-matrix/")
+@LeetCode(
+	id = 1337,
+	name = "The K Weakest Rows in a Matrix",
+	url = "https://leetcode.com/problems/the-k-weakest-rows-in-a-matrix/",
+	difficulty = Difficulty.EASY
+)
 public class TheKWeakestRowsInMatrix
 {
 	public int[] kWeakestRows(int[][] mat, int k)
 	{
-		Map<Integer, Integer> memo = new HashMap<>();
+		PriorityQueue<int[]> maxHeap = new PriorityQueue<>((a, b) ->
+		{
+			if (a[1] == b[1])
+			{
+				return b[0] - a[0];
+			}
+			return b[1] - a[1];
+		});
+
+		int count;
 		for (int i = 0; i < mat.length; i++)
 		{
-			memo.put(i, sum(mat[i]));
+			count = 0;
+			for (int j = 0; j < mat[i].length; j++)
+			{
+				count += mat[i][j];
+			}
+
+			maxHeap.add(new int[]{i, count});
+			if (maxHeap.size() > k)
+			{
+				maxHeap.remove();
+			}
 		}
 
 		int[] answer = new int[k];
-		PriorityQueue<Integer> minHeap = new PriorityQueue<>((v1, v2) ->
+		for (int i = k - 1; i >= 0; i--)
 		{
-			int a = memo.get(v1);
-			int b = memo.get(v2);
-			if (a == b)
-			{
-				return v1 - v2;
-			}
-			else
-			{
-				return a - b;
-			}
-		});
-		minHeap.addAll(memo.keySet());
-		int i = 0;
-		while (i < k)
-		{
-			answer[i++] = minHeap.poll();
+			answer[i] = maxHeap.remove()[0];
 		}
+
 		return answer;
-	}
-
-	private int sum(int[] line)
-	{
-		if (line[0] == 0)
-		{
-			return 0;
-		}
-		else if (line[line.length - 1] == 1)
-		{
-			return line.length;
-		}
-
-		int left = 0;
-		int right = line.length - 1;
-		int middle;
-		while (left <= right)
-		{
-			middle = left + (right - left) / 2;
-			if (line[middle] == 1)
-			{
-				left = middle + 1;
-			}
-			else
-			{
-				right = middle - 1;
-			}
-		}
-
-		return left;
 	}
 }
