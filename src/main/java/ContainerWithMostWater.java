6e9d0373c099dd55a12da572896eea3f43f4dfb2
rakhmedovrs@gmail.com <rakhmedovrs@gmail.com>
2021-02-17 22:38:17 +0300

    Container With Most Water updated

--- src/main/java/ContainerWithMostWater.java
+++ src/main/java/ContainerWithMostWater.java
@@ -1,32 +1,33 @@
+import common.Difficulty;
 import common.LeetCode;
 
 /**
  * @author RakhmedovRS
  * @created 06-Apr-20
  */
-@LeetCode(id = 11, name = "Container With Most Water", url = "https://leetcode.com/problems/container-with-most-water/")
+@LeetCode(
+	id = 11,
+	name = "Container With Most Water",
+	url = "https://leetcode.com/problems/container-with-most-water/",
+	difficulty = Difficulty.MEDIUM
+)
 public class ContainerWithMostWater
 {
 	public int maxArea(int[] height)
 	{
-		int maxArea = 0;
-		if (height == null || height.length <= 1)
-		{
-			return maxArea;
-		}
-
 		int left = 0;
 		int right = height.length - 1;
+		int maxArea = 0;
 		while (left < right)
 		{
-			maxArea = Math.max(maxArea, ((right - left) * Math.min(height[left], height[right])));
-			if (height[left] > height[right])
+			maxArea = Math.max(maxArea, (right - left) * Math.min(height[left], height[right]));
+			if (height[left] <= height[right])
 			{
-				right--;
+				left++;
 			}
 			else
 			{
-				left++;
+				right--;
 			}
 		}
 
@@ -37,6 +38,6 @@ public class ContainerWithMostWater
 	{
 		System.out.println(new ContainerWithMostWater().maxArea(new int[]{1, 2}));
 		System.out.println(new ContainerWithMostWater().maxArea(new int[]{1, 8, 6, 2, 5, 4, 8, 3, 7}));
-		System.out.println(new ContainerWithMostWater().maxArea(new int[]{2,3,4,5,18,17,6}));
+		System.out.println(new ContainerWithMostWater().maxArea(new int[]{2, 3, 4, 5, 18, 17, 6}));
 	}
 }
