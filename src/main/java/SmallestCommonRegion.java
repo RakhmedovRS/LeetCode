3ac3f9b8bd00f3de31ba9f4841cf29eaef11a99f
rakhmedovrs <rakhmedov.r.sak@sberbank.ru>
2021-02-20 11:09:14 +0300

    Smallest Common Region

---
+++ src/main/java/SmallestCommonRegion.java
@@ -0,0 +1,53 @@
+import common.Difficulty;
+import common.LeetCode;
+
+import java.util.*;
+
+/**
+ * @author RakhmedovRS
+ * @version 1.0
+ * @since 20.02.2021
+ */
+@LeetCode(
+        id = 1257,
+        name = "Smallest Common Region",
+        url = "https://leetcode.com/problems/smallest-common-region/",
+        difficulty = Difficulty.MEDIUM,
+        premium = true
+)
+public class SmallestCommonRegion
+{
+
+    public String findSmallestRegion(List<List<String>> regions, String region1, String region2)
+    {
+        Map<String, String> parents = new HashMap<>();
+        for (List<String> regionsList : regions)
+        {
+            String parentRegion = regionsList.get(0);
+            for (int i = 1; i < regionsList.size(); i++)
+            {
+                String region = regionsList.get(i);
+                parents.put(region, parentRegion);
+            }
+        }
+
+        Set<String> visited = new HashSet<>();
+        while (region1 != null)
+        {
+            visited.add(region1);
+            region1 = parents.get(region1);
+        }
+
+        while (region2 != null)
+        {
+            if (!visited.add(region2))
+            {
+                return region2;
+            }
+
+            region2 = parents.get(region2);
+        }
+
+        return "";
+    }
+}
