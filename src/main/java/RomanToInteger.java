46e931cad45167a9b3d9adab4cf140f933cf070d
rakhmedovrs <rakhmedov.r.sak@sberbank.ru>
2021-02-20 11:46:40 +0300

    Roman to Integer updated

--- src/main/java/RomanToInteger.java
+++ src/main/java/RomanToInteger.java
@@ -1,3 +1,4 @@
+import common.Difficulty;
 import common.LeetCode;
 
 import java.util.HashMap;
@@ -7,43 +8,46 @@ import java.util.Map;
  * @author RakhmedovRS
  * @created 24-Feb-20
  */
-@LeetCode(id = 13, name = "Roman to Integer", url = "https://leetcode.com/problems/roman-to-integer/")
+@LeetCode(
+		id = 13,
+		name = "Roman to Integer",
+		url = "https://leetcode.com/problems/roman-to-integer/",
+		difficulty = Difficulty.EASY
+)
 public class RomanToInteger
 {
-	static Map<String, Integer> dictionary = new HashMap<>();
-
-	static
-	{
-		dictionary.put("M", 1000);
-		dictionary.put("CM", 900);
-		dictionary.put("D", 500);
-		dictionary.put("CD", 400);
-		dictionary.put("C", 100);
-		dictionary.put("XC", 90);
-		dictionary.put("L", 50);
-		dictionary.put("XL", 40);
-		dictionary.put("X", 10);
-		dictionary.put("IX", 9);
-		dictionary.put("V", 5);
-		dictionary.put("IV", 4);
-		dictionary.put("I", 1);
-	}
-
 	public int romanToInt(String s)
 	{
-		int result = 0;
-		for (int i = 0; i < s.length(); )
+		Map<Character, Integer> map = new HashMap<>();
+		map.put('I', 1);
+		map.put('V', 5);
+		map.put('X', 10);
+		map.put('L', 50);
+		map.put('C', 100);
+		map.put('D', 500);
+		map.put('M', 1000);
+
+		int number = 0;
+		char[] chars = s.toCharArray();
+		for (int i = 0; i < chars.length;)
 		{
-			if (i + 1 < s.length() && dictionary.containsKey(s.substring(i, i + 2)))
+			if (i + 1 < chars.length && map.get(chars[i]) < map.get(chars[i + 1]))
 			{
-				result += dictionary.get(s.substring(i, i += 2));
+				number += map.get(chars[i + 1]) - map.get(chars[i]);
+				i += 2;
+			}
+			else if (i + 2 < chars.length && chars[i] == 'I' && chars[i + 1] == 'I' && chars[i + 2] == 'I')
+			{
+				number += 3;
+				i += 3;
 			}
 			else
 			{
-				result += dictionary.get(s.substring(i, ++i));
+				number += map.get(chars[i]);
+				i++;
 			}
 		}
 
-		return result;
+		return number;
 	}
 }
