d7c479afc185d122fbbee9e7ff59042e0f5aca42
rakhmedovrs@gmail.com <rakhmedovrs@gmail.com>
2021-02-22 17:20:12 +0300

    Longest Word in Dictionary updated

--- src/main/java/LongestWordInDictionary.java
+++ src/main/java/LongestWordInDictionary.java
@@ -1,40 +1,88 @@
+import common.Difficulty;
 import common.LeetCode;
-
-import java.util.Arrays;
-import java.util.HashSet;
-import java.util.Set;
 
 /**
  * @author RakhmedovRS
  * @created 24-Jun-20
  */
-@LeetCode(id = 720, name = "Longest Word in Dictionary", url = "https://leetcode.com/problems/longest-word-in-dictionary/")
+@LeetCode(
+	id = 720,
+	name = "Longest Word in Dictionary",
+	url = "https://leetcode.com/problems/longest-word-in-dictionary/",
+	difficulty = Difficulty.EASY
+)
 public class LongestWordInDictionary
 {
+	class Trie
+	{
+		Trie[] children = new Trie[26];
+		boolean end;
+	}
+
+	private void addToTrie(Trie root, char[] chars)
+	{
+		for (char ch : chars)
+		{
+			if (root.children[ch - 'a'] == null)
+			{
+				root.children[ch - 'a'] = new Trie();
+			}
+
+			root = root.children[ch - 'a'];
+		}
+		root.end = true;
+	}
+
 	public String longestWord(String[] words)
 	{
-		Arrays.sort(words);
-		Set<String> seen = new HashSet<>();
-		seen.add("");
-		String answer = "";
+		Trie root = new Trie();
 		for (String word : words)
 		{
-			if (seen.contains(word.substring(0, word.length() - 1)))
-			{
-				if (word.length() > answer.length())
-				{
-					answer = word;
-				}
+			addToTrie(root, word.toCharArray());
+		}
 
-				seen.add(word);
+		String[] answer = new String[]{""};
+		dfs(root, new StringBuilder(), answer);
+
+		return answer[0];
+	}
+
+	private void dfs(Trie root, StringBuilder sb, String[] answer)
+	{
+		if (root == null)
+		{
+			return;
+		}
+
+		if (sb.length() > answer[0].length())
+		{
+			answer[0] = sb.toString();
+		}
+		else if (sb.length() == answer[0].length())
+		{
+			String temp = sb.toString();
+			if (temp.compareTo(answer[0]) < 0)
+			{
+				answer[0] = temp;
 			}
 		}
 
-		return answer;
+		for (int i = 0; i < root.children.length; i++)
+		{
+			if (root.children[i] != null && root.children[i].end)
+			{
+				sb.append((char) (i + 'a'));
+				dfs(root.children[i], sb, answer);
+				sb.deleteCharAt(sb.length() - 1);
+			}
+		}
 	}
 
 	public static void main(String[] args)
 	{
-		System.out.println(new LongestWordInDictionary().longestWord(new String[]{"a", "banana", "app", "appl", "ap", "apply", "apple"}));
+		LongestWordInDictionary clazz = new LongestWordInDictionary();
+		System.out.println(clazz.longestWord(new String[]{"yo", "ew", "fc", "zrc", "yodn", "fcm", "qm", "qmo", "fcmz", "z", "ewq", "yod", "ewqz", "y"}));
+		System.out.println(clazz.longestWord(new String[]{"b", "br", "bre", "brea", "break", "breakf", "breakfa", "breakfas", "breakfast", "l", "lu", "lun", "lunc", "lunch", "d", "di", "din", "dinn", "dinne", "dinner"}));
+		System.out.println(clazz.longestWord(new String[]{"a", "banana", "app", "appl", "ap", "apply", "apple"}));
 	}
 }
