a982b8e89f04af764a75aefd62b667a23e6c8074
rakhmedovrs@gmail.com <rakhmedovrs@gmail.com>
2021-02-26 21:19:34 +0300

    Find the Kth Smallest Sum of a Matrix With Sorted Rows

---
+++ src/main/java/FindTheKthSmallestSumOfMatrixWithSortedRows.java
@@ -0,0 +1,46 @@
+import common.Difficulty;
+import common.LeetCode;
+
+import java.util.Comparator;
+import java.util.PriorityQueue;
+
+/**
+ * @author RakhmedovRS
+ * @created 2/26/2021
+ */
+@LeetCode(
+	id = 1439,
+	name = "Find the Kth Smallest Sum of a Matrix With Sorted Rows",
+	url = "https://leetcode.com/problems/find-the-kth-smallest-sum-of-a-matrix-with-sorted-rows/",
+	difficulty = Difficulty.HARD
+)
+public class FindTheKthSmallestSumOfMatrixWithSortedRows
+{
+	public int kthSmallest(int[][] mat, int k)
+	{
+		PriorityQueue<Integer> maxHeap = new PriorityQueue<>(Comparator.reverseOrder());
+		maxHeap.add(0);
+		for (int[] row : mat)
+		{
+			int currentSum;
+			PriorityQueue<Integer> nextLevelMaxHeap = new PriorityQueue<>(Comparator.reverseOrder());
+			while (!maxHeap.isEmpty())
+			{
+				currentSum = maxHeap.remove();
+
+				for (int val : row)
+				{
+					nextLevelMaxHeap.add(currentSum + val);
+					if (nextLevelMaxHeap.size() > k)
+					{
+						nextLevelMaxHeap.remove();
+					}
+				}
+			}
+
+			maxHeap = nextLevelMaxHeap;
+		}
+
+		return maxHeap.peek();
+	}
+}
