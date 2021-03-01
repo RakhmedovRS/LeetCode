b94c1235730e2479878afb19d463bd6337ce6094
rakhmedovrs@gmail.com <rakhmedovrs@gmail.com>
2021-03-01 16:13:55 +0300

    Reverse Linked List updated

--- src/main/java/ReverseLinkedList.java
+++ src/main/java/ReverseLinkedList.java
@@ -1,3 +1,4 @@
+import common.Difficulty;
 import common.LeetCode;
 import common.ListNode;
 
@@ -5,29 +6,27 @@ import common.ListNode;
  * @author RakhmedovRS
  * @created 14-Mar-20
  */
-@LeetCode(id = 206, name = "Reverse Linked List", url ="https://leetcode.com/problems/reverse-linked-list/")
+@LeetCode(
+	id = 206,
+	name = "Reverse Linked List",
+	url ="https://leetcode.com/problems/reverse-linked-list/",
+	difficulty = Difficulty.EASY
+)
 public class ReverseLinkedList
 {
 	public ListNode reverseList(ListNode head)
 	{
-		if (head == null || head.next == null)
-		{
-			return head;
-		}
-
 		ListNode prev = null;
-		ListNode next = head.next;
+		ListNode next = head;
 		while (next != null)
 		{
+			next = head.next;
 			head.next = prev;
 			prev = head;
 			head = next;
-			next = next.next;
 		}
 
-		head.next = prev;
-
-		return head;
+		return prev;
 	}
 
 	public static void main(String[] args)
