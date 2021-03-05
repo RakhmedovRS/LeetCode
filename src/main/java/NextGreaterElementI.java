1c9c09beabefefd02a4c74648bc132bb5c5d777e
rakhmedovrs@gmail.com <rakhmedovrs@gmail.com>
2021-03-05 21:51:13 +0300

    Next Greater Element I updated

--- src/main/java/NextGreaterElementI.java
+++ src/main/java/NextGreaterElementI.java
@@ -1,35 +1,40 @@
+import common.Difficulty;
 import common.LeetCode;
 
-import java.util.Deque;
-import java.util.HashMap;
-import java.util.LinkedList;
-import java.util.Map;
+import java.util.*;
 
 /**
  * @author RakhmedovRS
  * @created 11-May-20
  */
-@LeetCode(id = 496, name = "Next Greater Element I", url = "https://leetcode.com/problems/next-greater-element-i/")
+@LeetCode(
+	id = 496,
+	name = "Next Greater Element I",
+	url = "https://leetcode.com/problems/next-greater-element-i/",
+	difficulty = Difficulty.EASY
+)
 public class NextGreaterElementI
 {
 	public int[] nextGreaterElement(int[] nums1, int[] nums2)
 	{
-		Map<Integer, Integer> valueToPosition = new HashMap<>();
-		Deque<Integer> stack = new LinkedList<>();
-		for (int num : nums2)
+		int[] memo = new int[10_001];
+		Arrays.fill(memo , -1);
+		LinkedList<Integer> list = new LinkedList<>();
+		for (int i = 0; i < nums2.length; i++)
 		{
-			if (!stack.isEmpty() && stack.peek() < num)
+			while (!list.isEmpty() && nums2[list.getFirst()] < nums2[i])
 			{
-				valueToPosition.put(stack.pop(), num);
+				memo[nums2[list.removeFirst()]] = nums2[i];
 			}
-			stack.push(num);
+			list.addFirst(i);
 		}
 
-		for (int i = 0; i < nums1.length; i++)
+		int[] answer = new int[nums1.length];
+		for (int i = 0; i < answer.length; i++)
 		{
-			nums1[i] = valueToPosition.getOrDefault(nums1[i], -1);
+			answer[i] = memo[nums1[i]];
 		}
 
-		return nums1;
+		return answer;
 	}
 }
