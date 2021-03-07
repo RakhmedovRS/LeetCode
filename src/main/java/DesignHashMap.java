2f89781d62cd4ff65f7107957b094e87b3ad5b23
rakhmedovrs@gmail.com <rakhmedovrs@gmail.com>
2021-03-07 13:24:32 +0300

    Design HashMap updated

--- src/main/java/DesignHashMap.java
+++ src/main/java/DesignHashMap.java
@@ -1,127 +1,59 @@
+import common.Difficulty;
 import common.LeetCode;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Arrays;
 
 /**
  * @author RakhmedovRS
  * @created 16-Mar-20
  */
-@LeetCode(id = 706, name = "Design HashMap", url = "https://leetcode.com/problems/design-hashmap/")
+@LeetCode(
+	id = 706,
+	name = "Design HashMap",
+	url = "https://leetcode.com/problems/design-hashmap/",
+	difficulty = Difficulty.EASY
+)
 public class DesignHashMap
 {
-	static class MyHashMap
+	class MyHashMap
 	{
-		private class Pair
-		{
-			private int key;
-			private int value;
 
-			public Pair(int key, int value)
-			{
-				this.key = key;
-				this.value = value;
-			}
-		}
-
-		private List<List<Pair>> inner;
-		private int maxSize;
-		private int currentSize;
+		int[] values;
 
 		public MyHashMap()
 		{
-			maxSize = 13;
-			currentSize = 0;
-			inner = new ArrayList<>(maxSize);
-			for (int i = 0; i < maxSize; i++)
-			{
-				inner.add(new ArrayList<>());
-			}
+			values = new int[1000000];
+			Arrays.fill(values, -1);
 		}
 
+		/**
+		 * value will always be non-negative.
+		 */
 		public void put(int key, int value)
 		{
-			int hashCode = hashCode(key);
-			List<Pair> bucket = inner.get(hashCode);
-
-			boolean found = false;
-			for (Pair pair : bucket)
-			{
-				if (pair.key == key)
-				{
-					pair.value = value;
-					found = true;
-					break;
-				}
-			}
-
-			if (!found)
-			{
-				bucket.add(new Pair(key, value));
-				currentSize++;
-			}
-
-			if (1D * currentSize / maxSize >= 0.5D)
-			{
-				rehash();
-			}
+			values[key] = value;
 		}
 
+		/**
+		 * Returns the value to which the specified key is mapped, or -1 if this map contains no mapping for the key
+		 */
 		public int get(int key)
 		{
-			List<Pair> bucket = inner.get(hashCode(key));
-			for (Pair pair : bucket)
-			{
-				if (pair.key == key)
-				{
-					return pair.value;
-				}
-			}
-			return -1;
+			return values[key];
 		}
 
+		/**
+		 * Removes the mapping of the specified value key if this map contains a mapping for the key
+		 */
 		public void remove(int key)
 		{
-			List<Pair> bucket = inner.get(hashCode(key));
-			for (int i = 0; i < bucket.size(); i++)
-			{
-				if (bucket.get(i).key == key)
-				{
-					bucket.remove(i);
-					currentSize--;
-					break;
-				}
-			}
-		}
-
-		private int hashCode(int key)
-		{
-			return 17 * key % maxSize;
-		}
-
-		private void rehash()
-		{
-			maxSize <<= 1;
-			List<List<Pair>> old = inner;
-			inner = new ArrayList<>(maxSize);
-			for (int i = 0; i < maxSize; i++)
-			{
-				inner.add(new ArrayList<>());
-			}
-
-			for (List<Pair> pairs : old)
-			{
-				for (Pair pair : pairs)
-				{
-					inner.get(hashCode(pair.key)).add(pair);
-				}
-			}
+			values[key] = -1;
 		}
 	}
 
 	public static void main(String[] args)
 	{
-		MyHashMap hashMap = new MyHashMap();
+		MyHashMap hashMap = new DesignHashMap().new MyHashMap();
 		hashMap.put(1, 1);
 		hashMap.put(2, 2);
 		hashMap.get(1);            // returns 1
