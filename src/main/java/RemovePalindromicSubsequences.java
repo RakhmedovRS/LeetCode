aa913e32aa073f4e96d8d519e2e3c9daa4ef5a0f
rakhmedovrs@gmail.com <rakhmedovrs@gmail.com>
2021-03-08 12:39:28 +0300

    Remove Palindromic Subsequences updated

--- src/main/java/RemovePalindromicSubsequences.java
+++ src/main/java/RemovePalindromicSubsequences.java
@@ -1,15 +1,20 @@
+import common.Difficulty;
 import common.LeetCode;
 
 /**
  * @author RakhmedovRS
  * @created 22-Jul-20
  */
-@LeetCode(id = 1332, name = "Remove Palindromic Subsequences", url = "https://leetcode.com/problems/remove-palindromic-subsequences/")
+@LeetCode(id = 1332,
+	name = "Remove Palindromic Subsequences",
+	url = "https://leetcode.com/problems/remove-palindromic-subsequences/",
+	difficulty = Difficulty.EASY
+)
 public class RemovePalindromicSubsequences
 {
 	public int removePalindromeSub(String s)
 	{
-		if (s == null || s.isEmpty())
+		if (s.isEmpty())
 		{
 			return 0;
 		}
@@ -19,25 +24,6 @@ public class RemovePalindromicSubsequences
 			return 1;
 		}
 
-		boolean containsA = false;
-		boolean containsB = false;
-		for (char ch : s.toCharArray())
-		{
-			if (ch == 'a')
-			{
-				containsA = true;
-			}
-			else
-			{
-				containsB = true;
-			}
-
-			if (containsA && containsB)
-			{
-				break;
-			}
-		}
-
-		return (containsA ? 1 : 0) + (containsB ? 1 : 0);
+		return 2;
 	}
 }
