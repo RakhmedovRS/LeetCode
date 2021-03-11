2a702e57b50c27170e9ac9a096e33f88307937d5
rakhmedovrs@gmail.com <rakhmedovrs@gmail.com>
2021-03-11 20:26:52 +0300

    Ugly Number II updated

--- src/main/java/UglyNumberII.java
+++ src/main/java/UglyNumberII.java
@@ -1,27 +1,32 @@
+import common.Difficulty;
 import common.LeetCode;
+
+import java.util.TreeSet;
 
 /**
  * @author RakhmedovRS
  * @created 04-Jul-20
  */
-@LeetCode(id = 264, name = "Ugly Number II", url = "https://leetcode.com/problems/ugly-number-ii/")
+@LeetCode(
+	id = 264,
+	name = "Ugly Number II",
+	url = "https://leetcode.com/problems/ugly-number-ii/",
+	difficulty = Difficulty.MEDIUM
+)
 public class UglyNumberII
 {
 	public int nthUglyNumber(int n)
 	{
-		long[] ugly = new long[n + 1];
-		ugly[0] = 1;
-		int twoIdx = 0;
-		int threeIdx = 0;
-		int fiveIdx = 0;
-		for (int i = 1; i < n; i++)
+		TreeSet<Long> treeSet = new TreeSet<>();
+		treeSet.add(1L);
+		while (--n > 0)
 		{
-			ugly[i] = Math.min(ugly[twoIdx] * 2, Math.min(ugly[threeIdx] * 3, ugly[fiveIdx] * 5));
-			twoIdx += ugly[twoIdx] * 2 == ugly[i] ? 1 : 0;
-			threeIdx += ugly[threeIdx] * 3 == ugly[i] ? 1 : 0;
-			fiveIdx += ugly[fiveIdx] * 5 == ugly[i] ? 1 : 0;
+			Long first = treeSet.pollFirst();
+			treeSet.add(first * 2);
+			treeSet.add(first * 3);
+			treeSet.add(first * 5);
 		}
 
-		return (int) ugly[n - 1];
+		return (int) (long) treeSet.first();
 	}
 }
