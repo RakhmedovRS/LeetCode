62c7135c1fd9688503fa25b6b1edc8e5683dc2a7
rakhmedovrs@gmail.com <rakhmedovrs@gmail.com>
2021-03-15 20:38:36 +0300

    Encode and Decode TinyURL updated

--- src/main/java/EncodeAndDecodeTinyURL.java
+++ src/main/java/EncodeAndDecodeTinyURL.java
@@ -1,3 +1,4 @@
+import common.Difficulty;
 import common.LeetCode;
 
 import java.util.HashMap;
@@ -8,43 +9,58 @@ import java.util.Random;
  * @author RakhmedovRS
  * @created 03-Jun-20
  */
-@LeetCode(id = 535, name = "Encode and Decode TinyURL", url = "https://leetcode.com/problems/encode-and-decode-tinyurl/")
+@LeetCode(
+	id = 535,
+	name = "Encode and Decode TinyURL",
+	url = "https://leetcode.com/problems/encode-and-decode-tinyurl/",
+	difficulty = Difficulty.MEDIUM
+)
 public class EncodeAndDecodeTinyURL
 {
 	public class Codec
 	{
-		char[][] dictionary = new char[][]
-			{
-				"0123456789".toCharArray(),
-				"abcdefghijklmnopqrstuvwxyz".toCharArray(),
-				"ABCDEFGHIJKLMNOPQRSTUVWXYZ".toCharArray()
-			};
-
-		Map<String, String> urls = new HashMap<>();
 		Random random = new Random();
+		Map<String, String> encoded = new HashMap<>();
+		Map<String, String> decoded = new HashMap<>();
 
+		// Encodes a URL to a shortened URL.
 		public String encode(String longUrl)
 		{
-			StringBuilder sb = new StringBuilder();
-			while (sb.length() < 6)
+			if (encoded.containsKey(longUrl))
 			{
-				char[] line = dictionary[random.nextInt(dictionary.length)];
-				sb.append(line[random.nextInt(line.length)]);
+				return encoded.get(longUrl);
 			}
 
-			String candidate = sb.toString();
-			if (urls.containsKey(candidate))
-			{
-				return encode(longUrl);
-			}
+			String tiny = generate();
+			encoded.put(longUrl, tiny);
+			decoded.put(tiny, longUrl);
 
-			urls.put(candidate, longUrl);
-			return candidate;
+			return tiny;
 		}
 
+		// Decodes a shortened URL to its original URL.
 		public String decode(String shortUrl)
 		{
-			return urls.get(shortUrl);
+			return decoded.get(shortUrl);
+		}
+
+		private String generate()
+		{
+			char[] chars = new char[6];
+			while (true)
+			{
+				for (int i = 0; i < chars.length; i++)
+				{
+					chars[i] = (char) (random.nextInt(27) + 'a');
+				}
+
+				if (!decoded.containsKey(String.valueOf(chars)))
+				{
+					break;
+				}
+			}
+
+			return String.valueOf(chars);
 		}
 	}
 }
