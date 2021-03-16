64d0036614d98ffe7dd2a3c98d51623656efd44f
rakhmedovrs <rakhmedov.r.sak@sberbank.ru>
2021-03-16 18:14:21 +0300

    Best Time to Buy and Sell Stock with Transaction Fee updated

--- src/main/java/BestTimeToBuyAndSellStockWithTransactionFee.java
+++ src/main/java/BestTimeToBuyAndSellStockWithTransactionFee.java
@@ -1,55 +1,67 @@
+import common.Difficulty;
 import common.LeetCode;
+import contest.Contest;
 
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * @author RakhmedovRS
  * @created 02-Sep-20
  */
-@LeetCode(id = 714, name = "Best Time to Buy and Sell Stock with Transaction Fee", url = "https://leetcode.com/problems/best-time-to-buy-and-sell-stock-with-transaction-fee/")
+@LeetCode(
+		id = 714,
+		name = "Best Time to Buy and Sell Stock with Transaction Fee",
+		url = "https://leetcode.com/problems/best-time-to-buy-and-sell-stock-with-transaction-fee/",
+		difficulty = Difficulty.MEDIUM
+)
 public class BestTimeToBuyAndSellStockWithTransactionFee
 {
-	public int maxProfit(int[] prices, int fee)
+	enum State
 	{
-		if (prices.length < 2)
-		{
-			return 0;
-		}
+		CAN_BUY(0),
+		MUST_SELL(1);
 
-		int[][] memo = new int[prices.length][2];
-		for (int[] row : memo)
-		{
-			Arrays.fill(row, -1);
-		}
+		int code;
 
-		return dfs(prices, 0, fee, memo, 1);
+		State(int code)
+		{
+			this.code = code;
+		}
 	}
 
-	private int dfs(int[] prices, int pos, int fee, int[][] memo, int canBuy)
+	public int maxProfit(int[] prices, int fee)
+	{
+		return dfs(prices, fee, new Integer[prices.length][2], State.CAN_BUY, 0);
+	}
+
+	private int dfs(int[] prices, int fee, Integer[][] memo, State state, int pos)
 	{
 		if (pos == prices.length)
 		{
 			return 0;
 		}
 
-		if (memo[pos][canBuy] != -1)
+		if (memo[pos][state.code] != null)
 		{
-			return memo[pos][canBuy];
+			return memo[pos][state.code];
 		}
 
-		int max;
-		if (canBuy == 1)
+
+		if (state == State.CAN_BUY)
 		{
-			max = Math.max(-prices[pos] + dfs(prices, pos + 1, fee, memo, 0),
-				dfs(prices, pos + 1, fee, memo, canBuy));
+			memo[pos][state.code] = Math.max(-prices[pos] - fee + dfs(prices, fee, memo, State.MUST_SELL, pos + 1),
+					dfs(prices, fee, memo, state, pos + 1));
 		}
 		else
 		{
-			max = Math.max(prices[pos] - fee + dfs(prices, pos + 1, fee, memo, 1),
-				dfs(prices, pos + 1, fee, memo, canBuy));
+			memo[pos][state.code] = Math.max(prices[pos] + dfs(prices, fee, memo, State.CAN_BUY, pos + 1),
+					dfs(prices, fee, memo, state, pos + 1));
 		}
 
-		memo[pos][canBuy] = max;
-		return max;
+		memo[pos][state.code] = Math.max(memo[pos][state.code], 0);
+
+		return memo[pos][state.code];
 	}
 }
