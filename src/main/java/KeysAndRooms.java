c2419c0ab2195db3d4ff2d2047694c63b3e2a3b2
rakhmedovrs@gmail.com <rakhmedovrs@gmail.com>
2021-03-19 17:58:06 +0300

    Keys and Rooms updated

--- src/main/java/KeysAndRooms.java
+++ src/main/java/KeysAndRooms.java
@@ -1,33 +1,40 @@
+import common.Difficulty;
 import common.LeetCode;
 
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 
 /**
  * @author RakhmedovRS
  * @created 29-Jun-20
  */
-@LeetCode(id = 841, name = "Keys and Rooms", url = "https://leetcode.com/problems/keys-and-rooms/")
+@LeetCode(
+	id = 841,
+	name = "Keys and Rooms",
+	url = "https://leetcode.com/problems/keys-and-rooms/",
+	difficulty = Difficulty.MEDIUM
+)
 public class KeysAndRooms
 {
 	public boolean canVisitAllRooms(List<List<Integer>> rooms)
 	{
-		Set<Integer> visited = new HashSet<>();
-		traverse(rooms, 0, visited);
-		return visited.size() == rooms.size();
-	}
+		Set<Integer> visitedRooms = new HashSet<>();
+		Queue<Integer> queue = new LinkedList<>();
+		queue.add(0);
+		visitedRooms.add(0);
 
-	private void traverse(List<List<Integer>> rooms, int position, Set<Integer> visited)
-	{
-		if (!visited.add(position))
+		int currentRoom;
+		while (!queue.isEmpty())
 		{
-			return;
+			currentRoom = queue.remove();
+			for (int nextRoom : rooms.get(currentRoom))
+			{
+				if (visitedRooms.add(nextRoom))
+				{
+					queue.add(nextRoom);
+				}
+			}
 		}
 
-		for (int nextPosition : rooms.get(position))
-		{
-			traverse(rooms, nextPosition, visited);
-		}
+		return visitedRooms.size() == rooms.size();
 	}
 }
