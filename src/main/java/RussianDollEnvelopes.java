a308430afc85de8874a11737c3285977d2be0e98
rakhmedovrs@gmail.com <rakhmedovrs@gmail.com>
2021-03-30 14:26:54 +0300

    Russian Doll Envelopes updated

--- src/main/java/RussianDollEnvelopes.java
+++ src/main/java/RussianDollEnvelopes.java
@@ -1,3 +1,4 @@
+import common.Difficulty;
 import common.LeetCode;
 
 import java.util.Arrays;
@@ -6,41 +7,39 @@ import java.util.Arrays;
  * @author RakhmedovRS
  * @created 09-Jun-20
  */
-@LeetCode(id = 354, name = "Russian Doll Envelopes", url = "https://leetcode.com/problems/russian-doll-envelopes/")
+@LeetCode(
+	id = 354,
+	name = "Russian Doll Envelopes",
+	url = "https://leetcode.com/problems/russian-doll-envelopes/",
+	difficulty = Difficulty.MEDIUM
+)
 public class RussianDollEnvelopes
 {
 	public int maxEnvelopes(int[][] envelopes)
 	{
-		if (envelopes == null || envelopes.length == 0)
-		{
-			return 0;
-		}
-
 		int max = 1;
-		Arrays.sort(envelopes, (e1, e2) ->
+		Arrays.sort(envelopes, (a, b) ->
 		{
-			if (e1[0] == e2[0])
+			if (a[0] == b[0])
 			{
-				return e1[1] - e2[1];
+				return a[1] - b[1];
 			}
-			else
-			{
-				return e1[0] - e2[0];
-			}
+
+			return a[0] - b[0];
 		});
 
 		int[] memo = new int[envelopes.length];
 		Arrays.fill(memo, 1);
-		for (int i = 1; i < memo.length; i++)
+		for (int i = 1; i < envelopes.length; i++)
 		{
 			for (int j = 0; j < i; j++)
 			{
 				if (envelopes[j][0] < envelopes[i][0] && envelopes[j][1] < envelopes[i][1])
 				{
-					memo[i] = Math.max(memo[i], memo[j] + 1);
-					max = Math.max(max, memo[i]);
+					memo[i] = Math.max(memo[j] + 1, memo[i]);
 				}
 			}
+			max = Math.max(max, memo[i]);
 		}
 
 		return max;
