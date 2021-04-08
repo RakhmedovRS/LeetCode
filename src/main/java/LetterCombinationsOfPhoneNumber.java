29bea792b3183d2ea39bc74b6750a96433d9225b
rakhmedovrs@gmail.com <rakhmedovrs@gmail.com>
2021-04-08 15:18:10 -0700

    Letter Combinations of a Phone Number updated

--- src/main/java/LetterCombinationsOfPhoneNumber.java
+++ src/main/java/LetterCombinationsOfPhoneNumber.java
@@ -1,53 +1,47 @@
+import common.Difficulty;
 import common.LeetCode;
 
 import java.util.*;
-import java.util.stream.Collectors;
 
 /**
  * @author RakhmedovRS
  * @created 07-Apr-20
  */
-@LeetCode(id = 17, name = "Letter Combinations of a Phone Number", url = "https://leetcode.com/problems/letter-combinations-of-a-phone-number/")
+@LeetCode(
+	id = 17,
+	name = "Letter Combinations of a Phone Number",
+	url = "https://leetcode.com/problems/letter-combinations-of-a-phone-number/",
+	difficulty = Difficulty.MEDIUM
+)
 public class LetterCombinationsOfPhoneNumber
 {
-	private Map<Character, List<Character>> getDictionary()
-	{
-		Map<Character, List<Character>> dictionary = new HashMap<>();
-		dictionary.put('2', Arrays.asList('a', 'b', 'c'));
-		dictionary.put('3', Arrays.asList('d', 'e', 'f'));
-		dictionary.put('4', Arrays.asList('g', 'h', 'i'));
-		dictionary.put('5', Arrays.asList('j', 'k', 'l'));
-		dictionary.put('6', Arrays.asList('m', 'n', 'o'));
-		dictionary.put('7', Arrays.asList('p', 'q', 'r', 's'));
-		dictionary.put('8', Arrays.asList('t', 'u', 'v'));
-		dictionary.put('9', Arrays.asList('w', 'x', 'y', 'z'));
-		return dictionary;
-	}
-
 	public List<String> letterCombinations(String digits)
 	{
-		if (digits == null || digits.length() == 0)
+		if (digits.isEmpty())
 		{
-			return new ArrayList<>();
+			return Collections.emptyList();
 		}
-		Map<Character, List<Character>> dictionary = getDictionary();
-		LinkedList<StringBuilder> temp = new LinkedList<>();
-		temp.addFirst(new StringBuilder());
-		for (int pos = 0; pos < digits.length(); pos++)
+		char[][] chars = new char[][]{{}, {}, {'a', 'b', 'c'}, {'d', 'e', 'f'}, {'g', 'h', 'i'}, {'j', 'k', 'l'}, {'m', 'n', 'o'}, {'p', 'q', 'r', 's'}, {'t', 'u', 'v'}, {'w', 'x', 'y', 'z'}};
+		return dfs(0, digits.toCharArray(), chars);
+	}
+
+	private List<String> dfs(int pos, char[] digits, char[][] chars)
+	{
+		if (pos == digits.length)
 		{
-			List<Character> list = dictionary.get(digits.charAt(pos));
-			int size = temp.size();
-			while (size-- > 0)
+			return Arrays.asList("");
+		}
+
+		List<String> next = dfs(pos + 1, digits, chars);
+		List<String> result = new ArrayList<>();
+		for (char ch : chars[digits[pos] - '0'])
+		{
+			for (String nextStr : next)
 			{
-				StringBuilder sample = temp.removeFirst();
-				for (Character ch : list)
-				{
-					temp.add(new StringBuilder(sample).append(ch));
-				}
+				result.add(ch + nextStr);
 			}
 		}
 
-
-		return temp.stream().map(StringBuilder::toString).collect(Collectors.toList());
+		return result;
 	}
 }
