9c747422ab47072cd91158fa7413d4d84d3eec77
rakhmedovrs@gmail.com <rakhmedovrs@gmail.com>
2021-04-10 21:46:33 -0700

    Sign of the Product of an Array

---
+++ src/main/java/SignOfTheProductOfArray.java
@@ -0,0 +1,26 @@
+import common.Difficulty;
+import common.LeetCode;
+
+/**
+ * @author RakhmedovRS
+ * @created 4/10/2021
+ */
+@LeetCode(
+	id = 1822,
+	name = "Sign of the Product of an Array",
+	url = "https://leetcode.com/problems/sign-of-the-product-of-an-array/",
+	difficulty = Difficulty.EASY
+)
+public class SignOfTheProductOfArray
+{
+	public int arraySign(int[] nums)
+	{
+		int res = 1;
+		for (int num : nums)
+		{
+			res *= Integer.compare(num, 0);
+		}
+
+		return res;
+	}
+}
