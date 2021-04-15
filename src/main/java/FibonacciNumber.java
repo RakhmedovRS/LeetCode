7b407e470ddd5082ea1ddb892959cfa876474c12
rakhmedovrs@gmail.com <rakhmedovrs@gmail.com>
2021-04-15 14:19:27 -0700

    Fibonacci Number updated

--- src/main/java/FibonacciNumber.java
+++ src/main/java/FibonacciNumber.java
@@ -1,23 +1,29 @@
+import common.Difficulty;
 import common.LeetCode;
 
 /**
  * @author RakhmedovRS
  * @created 13-Feb-20
  */
-@LeetCode(id = 509, name = "Fibonacci Number", url = "https://leetcode.com/problems/fibonacci-number/")
+@LeetCode(
+	id = 509,
+	name = "Fibonacci Number",
+	url = "https://leetcode.com/problems/fibonacci-number/",
+	difficulty = Difficulty.EASY
+)
 public class FibonacciNumber
 {
-	static int[] FIB = new int[31];
-	static {
-		FIB[0] = 0;
-		FIB[1] = 1;
-		for (int i = 2; i < FIB.length; i++)
-		{
-			FIB[i] = FIB[i-2] + FIB[i-1];
-		}
-	}
+	public int fib(int n)
+	{
+		int[] memo = new int[Math.max(3, n + 1)];
+		memo[0] = 0;
+		memo[1] = 1;
 
-	public int fib(int N) {
-		return FIB[N];
+		for (int i = 2; i < memo.length; i++)
+		{
+			memo[i] = memo[i - 2] + memo[i - 1];
+		}
+
+		return memo[n];
 	}
 }
