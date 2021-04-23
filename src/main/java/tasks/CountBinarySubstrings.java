517500b52f8f34fc8027180711f9fcd6235397a0
rakhmedovrs@gmail.com <rakhmedovrs@gmail.com>
2021-04-23 10:49:50 -0700

    Count Binary Substrings updated

--- src/main/java/tasks/CountBinarySubstrings.java
+++ src/main/java/tasks/CountBinarySubstrings.java
@@ -1,36 +1,44 @@
 package tasks;
 
+import common.Difficulty;
 import common.LeetCode;
 
 /**
  * @author RakhmedovRS
  * @created 12-Jul-20
  */
-@LeetCode(id = 696, name = "Count Binary Substrings", url = "https://leetcode.com/problems/count-binary-substrings/")
+@LeetCode(
+	id = 696,
+	name = "Count Binary Substrings",
+	url = "https://leetcode.com/problems/count-binary-substrings/",
+	difficulty = Difficulty.EASY
+)
 public class CountBinarySubstrings
 {
 	public int countBinarySubstrings(String s)
 	{
 		int count = 0;
-		int groups = 0;
-		char prev = s.charAt(0);
-		int prevCount = 0;
-		for (char ch : s.toCharArray())
+
+		char[] chars = s.toCharArray();
+		for (int left = 0, right = 1; right < s.length(); left++, right++)
 		{
-			if (ch == prev)
+			if (chars[left] != chars[right])
 			{
 				count++;
-			}
-			else
-			{
-				groups += Math.min(count, prevCount);
-				prev = ch;
-				prevCount = count;
-				count = 1;
+				for (int shift = 1; left - shift >= 0 && right + shift < s.length() && chars[left] == chars[left - shift] && chars[right] == chars[right + shift]; shift++)
+				{
+					count++;
+				}
 			}
 		}
-		groups += Math.min(count, prevCount);
 
-		return groups;
+		return count;
+	}
+
+	public static void main(String[] args)
+	{
+		CountBinarySubstrings clazz = new CountBinarySubstrings();
+		System.out.println(clazz.countBinarySubstrings("00110"));
+		System.out.println(clazz.countBinarySubstrings("10101"));
 	}
 }
