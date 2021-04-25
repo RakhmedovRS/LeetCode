aaa337d6fff88da27d7fe7c8a7e02eb27bcaacfa
rakhmedovrs@gmail.com <rakhmedovrs@gmail.com>
2021-04-25 10:14:21 -0700

    Sum of Digits in Base

---
+++ src/main/java/tasks/SumOfDigitsInBaseK.java
@@ -0,0 +1,29 @@
+package tasks;
+
+import common.Difficulty;
+import common.LeetCode;
+
+/**
+ * @author RakhmedovRS
+ * @created 4/25/2021
+ */
+@LeetCode(
+	id = 1837,
+	name = "Sum of Digits in Base K",
+	url = "https://leetcode.com/problems/sum-of-digits-in-base-k/",
+	difficulty = Difficulty.EASY
+)
+public class SumOfDigitsInBaseK
+{
+	public int sumBase(int n, int k)
+	{
+		int sum = 0;
+		while (n >= k)
+		{
+			sum += n % k;
+			n /= k;
+		}
+
+		return sum + n;
+	}
+}
