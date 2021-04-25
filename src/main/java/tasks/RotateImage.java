b60971f06c36b78ab8a04a43004322d2245f83f4
rakhmedovrs@gmail.com <rakhmedovrs@gmail.com>
2021-04-25 11:19:47 -0700

    Rotate Image updated

--- src/main/java/tasks/RotateImage.java
+++ src/main/java/tasks/RotateImage.java
@@ -1,46 +1,72 @@
 package tasks;
 
+import common.Difficulty;
 import common.LeetCode;
 
 /**
  * @author RakhmedovRS
  * @created 29-Mar-20
  */
-@LeetCode(id = 48, name = "Rotate Image", url = "https://leetcode.com/problems/rotate-image/")
+@LeetCode(
+	id = 48,
+	name = "Rotate Image",
+	url = "https://leetcode.com/problems/rotate-image/",
+	difficulty = Difficulty.MEDIUM
+)
 public class RotateImage
 {
 	public void rotate(int[][] matrix)
 	{
-		if (matrix == null || matrix.length == 0)
+		int rows = matrix.length;
+		if (rows <= 1)
 		{
 			return;
 		}
+		int columns = matrix[0].length;
 
-		int size = matrix.length;
-
-		for (int row = 0; row < size; row++)
-		{
-			for (int column = row; column < size; column++)
-			{
-				int temp = matrix[row][column];
-				matrix[row][column] = matrix[column][row];
-				matrix[column][row] = temp;
-			}
-		}
-
-		int left;
-		int right;
+		int temp;
 		for (int[] row : matrix)
 		{
-			left = 0;
-			right = row.length - 1;
+			int left = 0;
+			int right = columns - 1;
 			while (left < right)
 			{
-				int temp = row[left];
+				temp = row[left];
 				row[left] = row[right];
 				row[right] = temp;
 				left++;
 				right--;
+			}
+		}
+
+		int[] left = new int[]{rows - 2, 0};
+		int[] right = new int[]{rows - 1, 1};
+		while (left[1] != right[1])
+		{
+			for (int r1 = left[0], c1 = left[1], r2 = right[0], c2 = right[1]; r1 < r2 && c1 < c2; r1++, c1++, r2--, c2--)
+			{
+
+				temp = matrix[r1][c1];
+				matrix[r1][c1] = matrix[r2][c2];
+				matrix[r2][c2] = temp;
+			}
+
+			if (left[0] == 0)
+			{
+				left[1]++;
+			}
+			else
+			{
+				left[0]--;
+			}
+
+			if (right[1] == columns - 1)
+			{
+				right[0]--;
+			}
+			else
+			{
+				right[1]++;
 			}
 		}
 	}
