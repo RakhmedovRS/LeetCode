2750dc281d4ac042009ac6680dbb9b6e701b5664
rakhmedovrs@gmail.com <rakhmedovrs@gmail.com>
2021-04-27 12:32:30 -0700

    Power of Three updated

--- src/main/java/tasks/PowerOfThree.java
+++ src/main/java/tasks/PowerOfThree.java
@@ -1,28 +1,32 @@
 package tasks;
 
+import common.Difficulty;
 import common.LeetCode;
 
 /**
  * @author RakhmedovRS
  * @created 20-Mar-20
  */
-@LeetCode(id = 326, name = "Power of Three", url = "https://leetcode.com/problems/power-of-three/")
+@LeetCode(
+	id = 326,
+	name = "Power of Three",
+	url = "https://leetcode.com/problems/power-of-three/",
+	difficulty = Difficulty.EASY
+)
 public class PowerOfThree
 {
 	public boolean isPowerOfThree(int n)
 	{
-		if (n == 1)
+		if (n == 0 || n == 2)
+		{
+			return false;
+		}
+
+		if (n == 1 || n == 3)
 		{
 			return true;
 		}
 
-		if (n == 0 || n % 3 != 0)
-		{
-			return false;
-		}
-		else
-		{
-			return isPowerOfThree(n / 3);
-		}
+		return n % 3 == 0 && isPowerOfThree(n / 3);
 	}
 }
