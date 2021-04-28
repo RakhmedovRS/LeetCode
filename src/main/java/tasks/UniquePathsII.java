6a56c677bbe860e37cf9d38868bca16e1657f7b8
rakhmedovrs@gmail.com <rakhmedovrs@gmail.com>
2021-04-28 10:41:28 -0700

    Unique Paths II updated

--- src/main/java/tasks/UniquePathsII.java
+++ src/main/java/tasks/UniquePathsII.java
@@ -1,42 +1,66 @@
 package tasks;
 
+import common.Difficulty;
 import common.LeetCode;
 
 /**
  * @author RakhmedovRS
  * @created 24-Feb-20
  */
-@LeetCode(id = 63, name = "Unique Paths II", url = "https://leetcode.com/problems/unique-paths-ii/")
+@LeetCode(
+	id = 63,
+	name = "Unique Paths II",
+	url = "https://leetcode.com/problems/unique-paths-ii/",
+	difficulty = Difficulty.MEDIUM
+)
 public class UniquePathsII
 {
 	public int uniquePathsWithObstacles(int[][] obstacleGrid)
 	{
-		if (obstacleGrid[0][0] == 1)
+		int rows = obstacleGrid.length;
+		if (rows == 0)
+		{
+			return 0;
+		}
+		int columns = obstacleGrid[0].length;
+
+		if (obstacleGrid[rows - 1][columns - 1] == 1 || obstacleGrid[0][0] == 1)
 		{
 			return 0;
 		}
 
-		int[][] grid = new int[obstacleGrid.length][obstacleGrid[0].length];
-		grid[0][0] = 1;
-		for (int column = 1; column < obstacleGrid[0].length; column++)
+		int[][] memo = new int[rows][columns];
+		for (int i = 0; i < columns; i++)
 		{
-			grid[0][column] = obstacleGrid[0][column] == 1 ? 0 : grid[0][column - 1];
-		}
-
-		for (int row = 1; row < obstacleGrid.length; row++)
-		{
-			grid[row][0] = obstacleGrid[row][0] == 1 ? 0 : grid[row - 1][0];
-		}
-
-		for (int row = 1; row < obstacleGrid.length; row++)
-		{
-			for (int column = 1; column < obstacleGrid[0].length; column++)
+			if (obstacleGrid[0][i] == 1)
 			{
-				grid[row][column] = obstacleGrid[row][column] == 1 ? 0 : grid[row - 1][column] + grid[row][column - 1];
+				break;
+			}
+			memo[0][i] = 1;
+		}
+
+		for (int i = 0; i < rows; i++)
+		{
+			if (obstacleGrid[i][0] == 1)
+			{
+				break;
+			}
+
+			memo[i][0] = 1;
+		}
+
+		for (int row = 1; row < rows; row++)
+		{
+			for (int column = 1; column < columns; column++)
+			{
+				if (obstacleGrid[row][column] != 1)
+				{
+					memo[row][column] += memo[row - 1][column] + memo[row][column - 1];
+				}
 			}
 		}
 
-		return grid[obstacleGrid.length - 1][obstacleGrid[0].length - 1];
+		return memo[rows - 1][columns - 1];
 	}
 
 	public static void main(String[] args)
