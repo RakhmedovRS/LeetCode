41f861abb2acdb8a55068d4b2fd054b8ef0f01f4
rakhmedovrs@gmail.com <rakhmedovrs@gmail.com>
2021-04-29 15:38:38 -0700

    Find First and Last Position of Element in Sorted Array updated

--- src/main/java/tasks/FindFirstAndLastPositionOfElementInSortedArray.java
+++ src/main/java/tasks/FindFirstAndLastPositionOfElementInSortedArray.java
@@ -1,62 +1,63 @@
 package tasks;
 
+import common.Difficulty;
 import common.LeetCode;
 
 /**
  * @author RakhmedovRS
  * @created 08-Apr-20
  */
-@LeetCode(id = 34, name = "Find First and Last Position of Element in Sorted Array", url = "https://leetcode.com/problems/find-first-and-last-position-of-element-in-sorted-array/")
+@LeetCode(
+	id = 34,
+	name = "Find First and Last Position of Element in Sorted Array",
+	url = "https://leetcode.com/problems/find-first-and-last-position-of-element-in-sorted-array/",
+	difficulty = Difficulty.MEDIUM
+)
 public class FindFirstAndLastPositionOfElementInSortedArray
 {
 	public int[] searchRange(int[] nums, int target)
 	{
-		int[] result = new int[]{-1, -1};
-		if (nums == null || nums.length == 0)
+		int[] answer = new int[]{-1, -1};
+		answer[0] = binarySearch(nums, target, true);
+		if (answer[0] != -1)
 		{
-			return result;
+			answer[1] = binarySearch(nums, target, false);
 		}
 
-		int middle = binarySearch(nums, 0, nums.length - 1, target);
-		if (middle != -1)
-		{
-			result[0] = middle;
-			result[1] = middle;
-
-			while ((middle = binarySearch(nums, 0, result[0] - 1, target)) != -1)
-			{
-				result[0] = middle;
-			}
-
-			while ((middle = binarySearch(nums, result[1] + 1, nums.length - 1, target)) != -1)
-			{
-				result[1] = middle;
-			}
-
-		}
-
-		return result;
+		return answer;
 	}
 
-	private int binarySearch(int[] nums, int left, int right, int target)
+	private int binarySearch(int[] nums, int target, boolean lookingForTheFirstPos)
 	{
-		if (left > right)
+		int index = -1;
+		int left = 0;
+		int middle;
+		int right = nums.length - 1;
+		while (left <= right)
 		{
-			return -1;
+			middle = (left + right) / 2;
+			if (nums[middle] == target)
+			{
+				index = middle;
+				if (lookingForTheFirstPos)
+				{
+					right = middle - 1;
+				}
+				else
+				{
+					left = middle + 1;
+				}
+			}
+			else if (nums[middle] < target)
+			{
+				left = middle + 1;
+			}
+			else
+			{
+				right = middle - 1;
+			}
 		}
-		int middle = left + (right - left) / 2;
 
-		if (nums[middle] == target)
-		{
-			return middle;
-		}
-		else if (nums[middle] > target)
-		{
-			return binarySearch(nums, left, middle - 1, target);
-		}
-		else
-		{
-			return binarySearch(nums, middle + 1, right, target);
-		}
+		return index;
 	}
 }
