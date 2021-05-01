0c2160bb1513effa800499889a9143d325407534
rakhmedovrs@gmail.com <rakhmedovrs@gmail.com>
2021-05-01 09:04:37 -0700

    Seat Reservation Manager

---
+++ src/main/java/tasks/SeatReservationManager.java
@@ -0,0 +1,43 @@
+package tasks;
+
+import common.Difficulty;
+import common.LeetCode;
+
+import java.util.PriorityQueue;
+
+/**
+ * @author RakhmedovRS
+ * @created 5/1/2021
+ */
+@LeetCode(
+	id = 1845,
+	name = "Seat Reservation Manager",
+	url = "https://leetcode.com/problems/seat-reservation-manager/",
+	difficulty = Difficulty.MEDIUM
+)
+public class SeatReservationManager
+{
+	class SeatManager
+	{
+
+		PriorityQueue<Integer> seats;
+		public SeatManager(int n)
+		{
+			seats = new PriorityQueue<>();
+			for (int i = 1; i <= n; i++)
+			{
+				seats.add(i);
+			}
+		}
+
+		public int reserve()
+		{
+			return seats.remove();
+		}
+
+		public void unreserve(int seatNumber)
+		{
+			seats.add(seatNumber);
+		}
+	}
+}
