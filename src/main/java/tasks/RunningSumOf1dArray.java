b1f67cf8dea1592661c568dfe0fbe429d6f8a149
rakhmedovrs@gmail.com <rakhmedovrs@gmail.com>
2021-05-03 08:55:33 -0700

    Running Sum of 1d Array updated

--- src/main/java/tasks/RunningSumOf1dArray.java
+++ src/main/java/tasks/RunningSumOf1dArray.java
@@ -1,20 +1,22 @@
 package tasks;
 
+import common.Difficulty;
 import common.LeetCode;
 
 /**
  * @author RakhmedovRS
  * @created 14-Jun-20
  */
-@LeetCode(id = 1480, name = "Running Sum of 1d Array", url = "https://leetcode.com/problems/running-sum-of-1d-array/")
+@LeetCode(
+	id = 1480,
+	name = "Running Sum of 1d Array",
+	url = "https://leetcode.com/problems/running-sum-of-1d-array/",
+	difficulty = Difficulty.EASY
+)
 public class RunningSumOf1dArray
 {
 	public int[] runningSum(int[] nums)
 	{
-		if (nums == null)
-		{
-			return nums;
-		}
 		for (int i = 1; i < nums.length; i++)
 		{
 			nums[i] += nums[i - 1];
