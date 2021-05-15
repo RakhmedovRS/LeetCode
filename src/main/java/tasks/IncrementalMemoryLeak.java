1b3983a73577e879815cdc23a937ecefe098a345
rakhmedovrs@gmail.com <rakhmedovrs@gmail.com>
2021-05-15 10:47:24 -0700

    Incremental Memory Leak

---
+++ src/main/java/tasks/IncrementalMemoryLeak.java
@@ -0,0 +1,44 @@
+package tasks;
+
+import common.Difficulty;
+import common.LeetCode;
+
+/**
+ * @author RakhmedovRS
+ * @created 5/15/2021
+ */
+@LeetCode(
+	id = 1860,
+	name = "Incremental Memory Leak",
+	url = "https://leetcode.com/problems/incremental-memory-leak/",
+	difficulty = Difficulty.MEDIUM
+)
+public class IncrementalMemoryLeak
+{
+	public int[] memLeak(int memory1, int memory2)
+	{
+		for (int i = 1; i <= Integer.MAX_VALUE - 1; i++)
+		{
+			if (memory1 >= memory2)
+			{
+				if (memory1 < i)
+				{
+					return new int[]{i, memory1, memory2};
+				}
+
+				memory1 -= i;
+			}
+			else
+			{
+				if (memory2 < i)
+				{
+					return new int[]{i, memory1, memory2};
+				}
+
+				memory2 -= i;
+			}
+		}
+
+		return null;
+	}
+}
