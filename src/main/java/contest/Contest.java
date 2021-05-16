f7e4cf1d1f03b29ef8db74d4d61b028cefec1e73
rakhmedovrs@gmail.com <rakhmedovrs@gmail.com>
2021-05-15 19:25:59 -0700

    Contest updated

--- src/main/java/contest/Contest.java
+++ src/main/java/contest/Contest.java
@@ -1,8 +1,8 @@
 package contest;
 
 import java.io.BufferedReader;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.IOException;
 import java.util.stream.Collectors;
 
 /**
@@ -13,13 +13,18 @@ public class Contest
 {
 
 
+
 	public static void main(String[] args) throws Exception
 	{
 		Contest clazz = new Contest();
 	}
 
-	private static String readBigTestCase() throws FileNotFoundException
+	private static String readBigTestCase() throws IOException
 	{
-		return new BufferedReader(new FileReader(Thread.currentThread().getContextClassLoader().getResource("TestCase.txt").getFile())).lines().collect(Collectors.joining());
+		try(BufferedReader br = new BufferedReader(new FileReader(Thread.currentThread().getContextClassLoader().getResource("TestCase.txt").getFile())))
+		{
+			return br.lines().collect(Collectors.joining());
+		}
+
 	}
 }
