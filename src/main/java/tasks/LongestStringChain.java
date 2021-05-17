3cfc165284409ce19459643b52c3b8fdf12913c7
rakhmedovrs@gmail.com <rakhmedovrs@gmail.com>
2021-05-17 08:01:33 -0700

    Longest String Chain updated

--- src/main/java/tasks/LongestStringChain.java
+++ src/main/java/tasks/LongestStringChain.java
@@ -1,9 +1,9 @@
 package tasks;
 
+import common.Difficulty;
 import common.LeetCode;
 
 import java.util.Arrays;
-import java.util.Comparator;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -11,38 +11,53 @@ import java.util.Map;
  * @author RakhmedovRS
  * @created 02-Jul-20
  */
-@LeetCode(id = 1048, name = "Longest String Chain", url = "https://leetcode.com/problems/longest-string-chain/")
+@LeetCode(
+	id = 1048,
+	name = "Longest String Chain",
+	url = "https://leetcode.com/problems/longest-string-chain/",
+	difficulty = Difficulty.MEDIUM
+)
 public class LongestStringChain
 {
 	public int longestStrChain(String[] words)
 	{
-		if (words.length == 0)
+		Arrays.sort(words, (a, b) ->
 		{
-			return 0;
-		}
+			if (a.length() == b.length())
+			{
+				return a.compareTo(b);
+			}
 
-		Arrays.sort(words, Comparator.comparingInt(String::length));
-		Map<String, Integer> patternToCount = new HashMap<>(words.length * 3);
-		int max = 0;
+			return a.length() - b.length();
+		});
+
+		int count;
+		int maxCount = 0;
+		String tempWord;
+		Map<String, Integer> wordToCount = new HashMap<>();
+
 		for (String word : words)
 		{
+			count = 0;
 			for (int i = 0; i < word.length(); i++)
 			{
-				String newWord = word.substring(0, i) + word.substring(i + 1);
-				if (patternToCount.containsKey(newWord))
+				StringBuilder sb = new StringBuilder();
+				for (int j = 0; j < word.length(); j++)
 				{
-					patternToCount.put(word, patternToCount.get(newWord) + 1);
+					if (i != j)
+					{
+						sb.append(word.charAt(j));
+					}
 				}
+
+				tempWord = sb.toString();
+				count = Math.max(count, wordToCount.getOrDefault(tempWord, 0) + 1);
 			}
 
-			if (!patternToCount.containsKey(word))
-			{
-				patternToCount.put(word, 1);
-			}
-
-			max = Math.max(max, patternToCount.get(word));
+			wordToCount.put(word, count);
+			maxCount = Math.max(maxCount, count);
 		}
 
-		return max;
+		return maxCount;
 	}
 }
