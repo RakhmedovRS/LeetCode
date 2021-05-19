7e505459da2ca2caf11de851e07f1dba9f283c24
rakhmedovrs@gmail.com <rakhmedovrs@gmail.com>
2021-05-19 15:12:35 -0700

    Concatenated Words updated

--- src/main/java/tasks/ConcatenatedWords.java
+++ src/main/java/tasks/ConcatenatedWords.java
@@ -1,77 +1,49 @@
 package tasks;
 
+import common.Difficulty;
 import common.LeetCode;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 /**
  * @author RakhmedovRS
  * @created 31-Jul-20
  */
-@LeetCode(id = 472, name = "Concatenated Words", url = "https://leetcode.com/problems/concatenated-words/")
+@LeetCode(
+	id = 472,
+	name = "Concatenated Words",
+	url = "https://leetcode.com/problems/concatenated-words/",
+	difficulty = Difficulty.HARD
+)
 public class ConcatenatedWords
 {
-	class Node
-	{
-		Node[] child = new Node[26];
-		boolean end = false;
-	}
-
-	public Node buildTrie(String[] words)
-	{
-		Node root = new Node();
-		for (String word : words)
-		{
-			Node temp = root;
-			for (char ch : word.toCharArray())
-			{
-				if (temp.child[ch - 'a'] == null)
-				{
-					temp.child[ch - 'a'] = new Node();
-				}
-
-				temp = temp.child[ch - 'a'];
-			}
-
-			temp.end = true;
-		}
-
-		return root;
-	}
-
 	public List<String> findAllConcatenatedWordsInADict(String[] words)
 	{
-		Node root = buildTrie(words);
+		Arrays.sort(words, Comparator.comparingInt(String::length));
+		Set<String> seen = new HashSet<>();
 		List<String> answer = new ArrayList<>();
 		for (String word : words)
 		{
-			if (canBeSplited(root, word, 0, 0))
+			if (dfs(0, word, seen))
 			{
 				answer.add(word);
 			}
+			seen.add(word);
 		}
 
 		return answer;
 	}
 
-	private boolean canBeSplited(Node root, String word, int pos, int words)
+	private boolean dfs(int pos, String word, Set<String> seen)
 	{
-		if (pos == word.length())
+		if (pos >= word.length() && word.length() > 0)
 		{
-			return words > 1;
+			return true;
 		}
 
-		Node current = root;
 		for (int i = pos; i < word.length(); i++)
 		{
-			current = current.child[word.charAt(i) - 'a'];
-			if (current == null)
-			{
-				return false;
-			}
-
-			if (current.end && canBeSplited(root, word, i + 1, words+1))
+			if (seen.contains(word.substring(pos, i + 1)) && dfs(i + 1, word, seen))
 			{
 				return true;
 			}
@@ -83,7 +55,7 @@ public class ConcatenatedWords
 	public static void main(String[] args)
 	{
 		System.out.println(new ConcatenatedWords().findAllConcatenatedWordsInADict(
-			new String[]{"cat","cats","catsdogcats","dog","dogcatsdog","hippopotamuses","rat","ratcatdogcat"})
+			new String[]{"cat", "cats", "catsdogcats", "dog", "dogcatsdog", "hippopotamuses", "rat", "ratcatdogcat"})
 		);
 	}
 }
