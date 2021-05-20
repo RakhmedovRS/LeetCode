67365b2821b9e33f02f0da8bb016fe00794b02a6
rakhmedovrs@gmail.com <rakhmedovrs@gmail.com>
2021-05-20 07:43:48 -0700

    Binary Tree Level Order Traversal updated

--- src/main/java/tasks/BinaryTreeLevelOrderTraversal.java
+++ src/main/java/tasks/BinaryTreeLevelOrderTraversal.java
@@ -1,51 +1,60 @@
 package tasks;
 
+import common.Difficulty;
 import common.LeetCode;
 import common.TreeNode;
 
 import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Queue;
 
 /**
  * @author RakhmedovRS
  * @created 09-Mar-20
  */
-@LeetCode(id = 102, name = "Binary Tree Level Order Traversal", url = "https://leetcode.com/problems/binary-tree-level-order-traversal/")
+@LeetCode(
+	id = 102,
+	name = "Binary Tree Level Order Traversal",
+	url = "https://leetcode.com/problems/binary-tree-level-order-traversal/",
+	difficulty = Difficulty.MEDIUM
+)
 public class BinaryTreeLevelOrderTraversal
 {
 	public List<List<Integer>> levelOrder(TreeNode root)
 	{
-		if (root == null)
-		{
-			return new ArrayList<>();
-		}
+		List<List<Integer>> answer = new ArrayList<>();
+		Queue<TreeNode> queue = new LinkedList<>();
+		queue.add(root);
 
-		List<List<Integer>> result = new LinkedList<>();
-		LinkedList<TreeNode> tempo = new LinkedList<>();
-		tempo.addLast(root);
-		while (!tempo.isEmpty())
+		int size;
+		TreeNode current;
+		while (!queue.isEmpty())
 		{
-			int currentLevelSize = tempo.size();
-			LinkedList<Integer> currentLevelValues = new LinkedList<>();
-			while (currentLevelSize > 0)
+			size = queue.size();
+			List<Integer> list = new ArrayList<>();
+			while (size-- > 0)
 			{
-				TreeNode node = tempo.removeFirst();
-				if (node.left != null)
+				current = queue.remove();
+				if (current == null)
 				{
-					tempo.addLast(node.left);
+					continue;
 				}
-				if (node.right != null)
-				{
-					tempo.addLast(node.right);
-				}
-				currentLevelValues.addLast(node.val);
 
-				currentLevelSize--;
+				list.add(current.val);
+
+				queue.add(current.left);
+				queue.add(current.right);
+
 			}
-			result.add(currentLevelValues);
+
+			if (!list.isEmpty())
+			{
+				answer.add(list);
+			}
 		}
 
-		return result;
+
+		return answer;
 	}
 }
