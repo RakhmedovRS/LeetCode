5dd24154afd76b67a5d4fe1d0f577e22bad2c9d0
rakhmedovrs@gmail.com <rakhmedovrs@gmail.com>
2021-05-24 07:51:24 -0700

    To Lower Case

---
+++ src/main/java/tasks/ToLowerCase.java
@@ -0,0 +1,28 @@
+package tasks;
+
+import common.Difficulty;
+import common.LeetCode;
+
+/**
+ * @author RakhmedovRS
+ * @created 5/24/2021
+ */
+@LeetCode(
+	id = 709,
+	name = "To Lower Case",
+	url = "https://leetcode.com/problems/to-lower-case/",
+	difficulty = Difficulty.EASY
+)
+public class ToLowerCase
+{
+	public String toLowerCase(String s)
+	{
+		StringBuilder sb = new StringBuilder();
+		for (char ch : s.toCharArray())
+		{
+			sb.append(Character.toLowerCase(ch));
+		}
+
+		return sb.toString();
+	}
+}
