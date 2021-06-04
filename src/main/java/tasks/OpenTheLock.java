a4f8ceae14bb4c87d3f6adb2886b308ec399db39
rakhmedovrs@gmail.com <rakhmedovrs@gmail.com>
2021-06-04 08:04:56 -0700

    Open the Lock updated

--- src/main/java/tasks/OpenTheLock.java
+++ src/main/java/tasks/OpenTheLock.java
@@ -1,5 +1,6 @@
 package tasks;
 
+import common.Difficulty;
 import common.LeetCode;
 
 import java.util.*;
@@ -8,109 +9,67 @@ import java.util.*;
  * @author RakhmedovRS
  * @created 07-Aug-20
  */
-@LeetCode(id = 752, name = "Open the Lock", url = "https://leetcode.com/problems/open-the-lock/")
+@LeetCode(
+	id = 752,
+	name = "Open the Lock",
+	url = "https://leetcode.com/problems/open-the-lock/",
+	difficulty = Difficulty.MEDIUM
+)
 public class OpenTheLock
 {
 	public int openLock(String[] deadends, String target)
 	{
-		Set<Integer> deadTable = new HashSet<>();
-		for (String deadend : deadends)
-		{
-			int num = 0;
-			for (int i = 0; i < 4; i++)
-			{
-				num *= 10;
-				num += deadend.charAt(i) - '0';
-			}
-			deadTable.add(num);
-		}
+		Set<String> visited = new HashSet<>(Arrays.asList(deadends));
 
-		if (deadTable.contains(0))
-		{
-			return -1;
-		}
-
-		int[] start = new int[]{
-			target.charAt(0) - '0',
-			target.charAt(1) - '0',
-			target.charAt(2) - '0',
-			target.charAt(3) - '0'
-		};
-
-		if (deadTable.contains(convertToNumber(start)))
-		{
-			return -1;
-		}
-
-		Queue<int[]> combinations = new LinkedList<>();
-		combinations.add(start);
+		Queue<String> queue = new LinkedList<>();
+		queue.add("0000");
 		int size;
 		int steps = 0;
-		Set<Integer> visited = new HashSet<>();
-		while (!combinations.isEmpty())
+		String current;
+		char[] chars;
+		while (!queue.isEmpty())
 		{
-			size = combinations.size();
+			size = queue.size();
 			while (size-- > 0)
 			{
-				int[] current = combinations.remove();
-				if (!visited.add(convertToNumber(current)))
+				current = queue.remove();
+				if (!visited.add(current))
 				{
 					continue;
 				}
 
-				for (int pos = 0; pos < 4; pos++)
+				if (current.equals(target))
 				{
-					int[] next = Arrays.copyOf(current, 4);
-					next[pos]--;
-					if (next[pos] < 0)
+					return steps;
+				}
+
+				chars = current.toCharArray();
+				char original;
+				for (int i = 0; i < chars.length; i++)
+				{
+					original = chars[i];
+
+					chars[i] = chars[i] == '9' ? '0' : (char) (chars[i] + 1);
+					if (!visited.contains(String.valueOf(chars)))
 					{
-						next[pos] = 9;
+						queue.add(String.valueOf(chars));
 					}
 
-					if (next[0] == 0 && next[1] == 0 && next[2] == 0 && next[3] == 0)
+					chars[i] = original;
+
+					chars[i] = chars[i] == '0' ? '9' : (char) (chars[i] - 1);
+					if (!visited.contains(String.valueOf(chars)))
 					{
-						return steps + 1;
+						queue.add(String.valueOf(chars));
 					}
 
-					int newNum = convertToNumber(next);
-					if (!deadTable.contains(newNum) && !visited.contains(newNum))
-					{
-						combinations.add(next);
-					}
-
-					next = Arrays.copyOf(current, 4);
-					next[pos]++;
-					if (next[pos] > 9)
-					{
-						next[pos] = 0;
-					}
-
-					if (next[0] == 0 && next[1] == 0 && next[2] == 0 && next[3] == 0)
-					{
-						return steps + 1;
-					}
-
-					newNum = convertToNumber(next);
-					if (!deadTable.contains(newNum) && !visited.contains(newNum))
-					{
-						combinations.add(next);
-					}
+					chars[i] = original;
 				}
 			}
+
 			steps++;
 		}
 
 		return -1;
-	}
-
-	private int convertToNumber(int[] digits)
-	{
-		int num = 0;
-		for (int i = 0; i < 4; i++)
-		{
-			num *= 10;
-			num += digits[i];
-		}
-		return num;
 	}
 }
