fb1c90ef36ace27aa96a3409e7949e93dd421ba4
rakhmedovrs@gmail.com <rakhmedovrs@gmail.com>
2021-06-07 07:37:42 -0700

    Min Cost Climbing Stairs updated

--- src/main/java/tasks/MinCostClimbingStairs.java
+++ src/main/java/tasks/MinCostClimbingStairs.java
@@ -1,30 +1,28 @@
 package tasks;
 
+import common.Difficulty;
 import common.LeetCode;
 
 /**
  * @author RakhmedovRS
  * @created 10-Apr-20
  */
-@LeetCode(id = 746, name = "Min Cost Climbing Stairs", url = "https://leetcode.com/problems/min-cost-climbing-stairs/")
+@LeetCode(
+	id = 746,
+	name = "Min Cost Climbing Stairs",
+	url = "https://leetcode.com/problems/min-cost-climbing-stairs/",
+	difficulty = Difficulty.EASY
+)
 public class MinCostClimbingStairs
 {
 	public int minCostClimbingStairs(int[] cost)
 	{
-		if (cost == null || cost.length == 0)
-		{
-			return 0;
-		}
-
-		int[] memo = new int[cost.length];
-		memo[0] = cost[0];
-		memo[1] = cost[1];
 		for (int i = 2; i < cost.length; i++)
 		{
-			memo[i] = Math.min(memo[i - 2], memo[i - 1]) + cost[i];
+			cost[i] += Math.min(cost[i - 2], cost[i - 1]);
 		}
 
-		return Math.min(memo[memo.length - 2], memo[memo.length - 1]);
+		return Math.min(cost[cost.length - 2], cost[cost.length - 1]);
 	}
 
 	public static void main(String[] args)
