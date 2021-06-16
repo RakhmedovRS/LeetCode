628ce2b8234834d792561c8cd7b527a05f669101
rakhmedovrs@gmail.com <rakhmedovrs@gmail.com>
2021-06-16 10:27:01 -0700

    Redistribute Characters to Make All Strings Equal

---
+++ src/main/java/tasks/RedistributeCharactersToMakeAllStringsEqual.java
@@ -0,0 +1,39 @@
+package tasks;
+
+import common.Difficulty;
+import common.LeetCode;
+
+/**
+ * @author RakhmedovRS
+ * @created 6/16/2021
+ */
+@LeetCode(
+	id = 1897,
+	name = "Redistribute Characters to Make All Strings Equal",
+	url = "https://leetcode.com/problems/redistribute-characters-to-make-all-strings-equal/",
+	difficulty = Difficulty.EASY
+)
+public class RedistributeCharactersToMakeAllStringsEqual
+{
+	public boolean makeEqual(String[] words)
+	{
+		int[] memo = new int[26];
+		for (String word : words)
+		{
+			for (char ch : word.toCharArray())
+			{
+				memo[ch - 'a']++;
+			}
+		}
+
+		for (int count : memo)
+		{
+			if (count % words.length != 0)
+			{
+				return false;
+			}
+		}
+
+		return true;
+	}
+}
