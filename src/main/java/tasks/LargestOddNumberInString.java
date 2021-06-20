4ad8a5d04fac02593d992fd41ee4d79785b31c41
rakhmedovrs@gmail.com <rakhmedovrs@gmail.com>
2021-06-20 10:02:09 -0700

    Largest Odd Number in String

---
+++ src/main/java/tasks/LargestOddNumberInString.java
@@ -0,0 +1,30 @@
+package tasks;
+
+import common.Difficulty;
+import common.LeetCode;
+
+/**
+ * @author RakhmedovRS
+ * @created 6/20/2021
+ */
+@LeetCode(
+	id = 1903,
+	name = "Largest Odd Number in String",
+	url = "https://leetcode.com/problems/largest-odd-number-in-string/",
+	difficulty = Difficulty.EASY
+)
+public class LargestOddNumberInString
+{
+	public String largestOddNumber(String num)
+	{
+		for (int i = num.length() - 1; i >= 0; i--)
+		{
+			if ((num.charAt(i) - '0') % 2 != 0)
+			{
+				return num.substring(0, i + 1);
+			}
+		}
+
+		return "";
+	}
+}
