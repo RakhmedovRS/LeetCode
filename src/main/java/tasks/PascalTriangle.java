ea578ab5569e5db0cf34c64bb68d3d684f98c34c
rakhmedovrs@gmail.com <rakhmedovrs@gmail.com>
2021-06-21 08:18:31 -0700

    Pascal's Triangle updated

--- src/main/java/tasks/PascalTriangle.java
+++ src/main/java/tasks/PascalTriangle.java
@@ -1,5 +1,6 @@
 package tasks;
 
+import common.Difficulty;
 import common.LeetCode;
 
 import java.util.ArrayList;
@@ -10,48 +11,43 @@ import java.util.List;
  * @author RakhmedovRS
  * @created 26-Mar-20
  */
-@LeetCode(id = 118, name = "Pascal's tasks.Triangle", url = "https://leetcode.com/problems/pascals-triangle/")
+@LeetCode(
+	id = 118,
+	name = "Pascal's Triangle",
+	url = "https://leetcode.com/problems/pascals-triangle/",
+	difficulty = Difficulty.EASY
+)
 public class PascalTriangle
 {
 	public List<List<Integer>> generate(int numRows)
 	{
-		List<List<Integer>> result = new ArrayList<>();
-		if (numRows == 0)
+		List<List<Integer>> lists = new ArrayList<>();
+		if (numRows >= 1)
 		{
-			return result;
+			lists.add(Arrays.asList(1));
 		}
 
-		result.add(Arrays.asList(1));
-		if (numRows == 1)
+		if (numRows >= 2)
 		{
-			return result;
+			lists.add(Arrays.asList(1,1));
 		}
 
-		result.add(Arrays.asList(1, 1));
-		if (numRows == 2)
+		if (numRows >= 3)
 		{
-			return result;
-		}
-
-		result.add(Arrays.asList(1, 2, 1));
-		if (numRows == 3)
-		{
-			return result;
-		}
-
-		for (int row = 3; row < numRows; row++)
-		{
-			List<Integer> newRow = new ArrayList<>(3 + 1);
-			newRow.add(1);
-			for (int position = 1; position < row; position++)
+			for (int i = 2; i < numRows; i++)
 			{
-				newRow.add(result.get(row - 1).get(position - 1) + result.get(row - 1).get(position));
+				lists.add(new ArrayList<>());
+				lists.get(i).add(1);
+				for (int j = 1; j < i; j++)
+				{
+					lists.get(i).add(lists.get(i - 1).get(j - 1) + lists.get(i - 1).get(j));
+				}
+
+				lists.get(i).add(1);
 			}
-			newRow.add(1);
-			result.add(newRow);
 		}
 
-		return result;
+		return lists;
 	}
 
 	public static void main(String[] args)
