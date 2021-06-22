828b1580a0ba65b58c1f7d1dd0a6f5b7c5d8b0aa
rakhmedovrs@gmail.com <rakhmedovrs@gmail.com>
2021-06-22 08:19:55 -0700

    Number of Matching Subsequences updated

--- src/main/java/tasks/NumberOfMatchingSubsequences.java
+++ src/main/java/tasks/NumberOfMatchingSubsequences.java
@@ -1,43 +1,53 @@
 package tasks;
 
+import common.Difficulty;
 import common.LeetCode;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.TreeSet;
 
 /**
  * @author RakhmedovRS
  * @created 15-Jul-20
  */
-@LeetCode(id = 792, name = "Number of Matching Subsequences", url = "https://leetcode.com/problems/number-of-matching-subsequences/")
+@LeetCode(
+	id = 792,
+	name = "Number of Matching Subsequences",
+	url = "https://leetcode.com/problems/number-of-matching-subsequences/",
+	difficulty = Difficulty.MEDIUM
+)
 public class NumberOfMatchingSubsequences
 {
-	public int numMatchingSubseq(String S, String[] words)
+	public int numMatchingSubseq(String s, String[] words)
 	{
-		int matches = 0;
-		Map<String, Boolean> memo = new HashMap<>();
-		for (String word : words)
+		TreeSet<Integer>[] positions = new TreeSet[26];
+		for (int i = 0; i < 26; i++)
 		{
-			Boolean match = memo.get(word);
-			if (match == null)
-			{
-				int left = 0;
-				int right = 0;
-				while (left < S.length() && right < word.length())
-				{
-					if (S.charAt(left) == word.charAt(right))
-					{
-						right++;
-					}
-					left++;
-				}
-				match = right == word.length();
-				memo.put(word, match);
-			}
-			matches += match ? 1 : 0;
+			positions[i] = new TreeSet<>();
 		}
 
-		return matches;
+		for (int i = 0; i < s.length(); i++)
+		{
+			positions[s.charAt(i) - 'a'].add(i);
+		}
+
+		int numberOfMatchingSeq = 0;
+		outer:
+		for (String word : words)
+		{
+			int prev = -1;
+			for (char ch : word.toCharArray())
+			{
+				Integer next = positions[ch - 'a'].ceiling(prev + 1);
+				if (next == null)
+				{
+					continue outer;
+				}
+				prev = next;
+			}
+			numberOfMatchingSeq++;
+		}
+
+		return numberOfMatchingSeq;
 	}
 
 	public static void main(String[] args)
