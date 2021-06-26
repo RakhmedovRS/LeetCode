896bb10bb5243ef8541ff1342e4d65c92e89ba93
rakhmedovrs@gmail.com <rakhmedovrs@gmail.com>
2021-06-26 09:32:34 -0700

    Remove All Occurrences of a Substring

---
+++ src/main/java/tasks/RemoveAllOccurrencesOfSubstring.java
@@ -0,0 +1,29 @@
+package tasks;
+
+import common.Difficulty;
+import common.LeetCode;
+
+/**
+ * @author RakhmedovRS
+ * @created 6/26/2021
+ */
+@LeetCode(
+	id = 1910,
+	name = "Remove All Occurrences of a Substring",
+	url = "https://leetcode.com/problems/remove-all-occurrences-of-a-substring/",
+	difficulty = Difficulty.MEDIUM
+)
+public class RemoveAllOccurrencesOfSubstring
+{
+	public String removeOccurrences(String s, String part)
+	{
+		int index = s.indexOf(part);
+		while (index != -1)
+		{
+			s = s.substring(0, index) + s.substring(index + part.length());
+			index = s.indexOf(part);
+		}
+
+		return s;
+	}
+}
