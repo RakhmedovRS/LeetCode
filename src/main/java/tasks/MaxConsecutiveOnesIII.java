22991b5ad8d51a2b020c30497da9042bbeaded65
rakhmedovrs@gmail.com <rakhmedovrs@gmail.com>
2021-06-29 14:16:38 -0700

    Max Consecutive Ones III updated

--- src/main/java/tasks/MaxConsecutiveOnesIII.java
+++ src/main/java/tasks/MaxConsecutiveOnesIII.java
@@ -1,36 +1,42 @@
 package tasks;
 
+import common.Difficulty;
 import common.LeetCode;
 
 /**
  * @author RakhmedovRS
  * @created 15-Sep-20
  */
-@LeetCode(id = 1004, name = "Max Consecutive Ones III", url = "https://leetcode.com/problems/max-consecutive-ones-iii/")
+@LeetCode(
+	id = 1004,
+	name = "Max Consecutive Ones III",
+	url = "https://leetcode.com/problems/max-consecutive-ones-iii/",
+	difficulty = Difficulty.MEDIUM
+)
 public class MaxConsecutiveOnesIII
 {
-	public int longestOnes(int[] A, int K)
+	public int longestOnes(int[] nums, int k)
 	{
-		int max = 0;
 		int left = 0;
-		for (int right = 0; right < A.length; right++)
+		int right = 0;
+		int maxCountOfOnes = 0;
+		while (right < nums.length)
 		{
-			if (A[right] == 0)
+			if (nums[right++] == 0)
 			{
-				K--;
-				while (K < 0)
+				k--;
+				while (left < right && k < 0)
 				{
-					if (A[left] == 0)
+					if (nums[left++] == 0)
 					{
-						K++;
+						k++;
 					}
-					left++;
 				}
 			}
 
-			max = Math.max(max, 1 + right - left);
+			maxCountOfOnes = Math.max(maxCountOfOnes, right - left);
 		}
 
-		return max;
+		return maxCountOfOnes;
 	}
 }
