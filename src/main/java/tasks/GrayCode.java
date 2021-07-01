1bed515b7cf2862ad9d022a11748751bb14b94c6
rakhmedovrs@gmail.com <rakhmedovrs@gmail.com>
2021-07-01 16:00:26 -0700

    Gray Code

---
+++ src/main/java/tasks/GrayCode.java
@@ -0,0 +1,30 @@
+package tasks;
+
+import common.Difficulty;
+import common.LeetCode;
+
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * @author RakhmedovRS
+ * @created 7/1/2021
+ */
+@LeetCode(
+	id = 89,
+	name = "Gray Code",
+	url = "https://leetcode.com/problems/gray-code/",
+	difficulty = Difficulty.MEDIUM
+)
+public class GrayCode
+{
+	public List<Integer> grayCode(int n)
+	{
+		List<Integer> result = new LinkedList<>();
+		for (int i = 0; i < 1 << n; i++)
+		{
+			result.add(i ^ (i >> 1));
+		}
+		return result;
+	}
+}
