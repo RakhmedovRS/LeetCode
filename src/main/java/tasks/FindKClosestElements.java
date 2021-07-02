ac370d1c2afb099bb158db4b340f8c5e849f073c
rakhmedovrs@gmail.com <rakhmedovrs@gmail.com>
2021-07-02 11:05:45 -0700

    Find K Closest Elements updated

--- src/main/java/tasks/FindKClosestElements.java
+++ src/main/java/tasks/FindKClosestElements.java
@@ -20,76 +20,27 @@ public class FindKClosestElements
 {
 	public List<Integer> findClosestElements(int[] arr, int k, int x)
 	{
-		List<Integer> answer = new ArrayList<>();
-		if (x <= arr[0])
-		{
-			for (int i = 0; i < k; i++)
-			{
-				answer.add(arr[i]);
-			}
-		}
-		else if (x >= arr[arr.length - 1])
-		{
-			for (int i = arr.length - k; i < arr.length; i++)
-			{
-				answer.add(arr[i]);
-			}
-		}
-		else
-		{
-			int pos = findPos(arr, x);
-			int left = pos;
-			int right = pos + 1;
-			for (int i = 0; i < k; i++)
-			{
-				if (left >= 0 && right < arr.length)
-				{
-					if (x - arr[left] <= arr[right] - x)
-					{
-						answer.add(arr[left--]);
-					}
-					else
-					{
-						answer.add(arr[right++]);
-					}
-				}
-				else if (left >= 0)
-				{
-					answer.add(arr[left--]);
-				}
-				else
-				{
-					answer.add(arr[right++]);
-				}
-			}
-
-			answer.sort(null);
-		}
-
-		return answer;
-	}
-
-	private int findPos(int[] arr, int val)
-	{
-		int pos = -1;
+		List<Integer> list = new ArrayList<>();
 		int left = 0;
-		int middle;
 		int right = arr.length - 1;
-		while (left <= right)
+		while (1 + right - left > k)
 		{
-			middle = (left + right) / 2;
-			if (arr[middle] <= val)
+			if (Math.abs(arr[left] - x) >= Math.abs(arr[right] - x))
 			{
-				left = middle + 1;
-				pos = middle;
+				left++;
 			}
 			else
 			{
-				right = middle - 1;
+				right--;
 			}
 		}
 
-		return pos;
+		while (left <= right)
+		{
+			list.add(arr[left++]);
+		}
+
+		return list;
 	}
 
 	public static void main(String[] args)
