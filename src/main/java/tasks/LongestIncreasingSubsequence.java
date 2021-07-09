41d07747422096d1c329b5d6070ad58c36ff3aee
rakhmedovrs@gmail.com <rakhmedovrs@gmail.com>
2021-07-09 08:17:16 -0700

    Longest Increasing Subsequence updated

--- src/main/java/tasks/LongestIncreasingSubsequence.java
+++ src/main/java/tasks/LongestIncreasingSubsequence.java
@@ -1,5 +1,6 @@
 package tasks;
 
+import common.Difficulty;
 import common.LeetCode;
 
 import java.util.Arrays;
@@ -8,7 +9,12 @@ import java.util.Arrays;
  * @author RakhmedovRS
  * @created 14-Feb-20
  */
-@LeetCode(id = 300, name = "Longest Increasing Subsequence", url = "https://leetcode.com/problems/longest-increasing-subsequence/")
+@LeetCode(
+	id = 300,
+	name = "Longest Increasing Subsequence",
+	url = "https://leetcode.com/problems/longest-increasing-subsequence/",
+	difficulty = Difficulty.MEDIUM
+)
 public class LongestIncreasingSubsequence
 {
 	public int lengthOfLIS(int[] nums)
