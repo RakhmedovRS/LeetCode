f87ae0270402137db955fc03d5b7c1c31cd73208
rakhmedovrs@gmail.com <rakhmedovrs@gmail.com>
2021-07-12 09:57:42 -0700

    Concatenation of Array

---
+++ src/main/java/tasks/ConcatenationOfArray.java
@@ -0,0 +1,29 @@
+package tasks;
+
+import common.Difficulty;
+import common.LeetCode;
+
+/**
+ * @author RakhmedovRS
+ * @created 7/12/2021
+ */
+@LeetCode(
+	id = 1929,
+	name = "Concatenation of Array",
+	url = "https://leetcode.com/problems/concatenation-of-array/",
+	difficulty = Difficulty.EASY
+)
+public class ConcatenationOfArray
+{
+	public int[] getConcatenation(int[] nums)
+	{
+		int[] answer = new int[nums.length * 2];
+		for (int i = 0; i < nums.length; i++)
+		{
+			answer[i] = nums[i];
+			answer[i + nums.length] = nums[i];
+		}
+
+		return answer;
+	}
+}
