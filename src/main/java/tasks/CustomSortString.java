bffea21e96bfc7f5ce2758fbbe956c067049d935
rakhmedovrs@gmail.com <rakhmedovrs@gmail.com>
2021-07-14 11:47:45 -0700

    Custom Sort String updated

--- src/main/java/tasks/CustomSortString.java
+++ src/main/java/tasks/CustomSortString.java
@@ -1,45 +1,50 @@
 package tasks;
 
+import common.Difficulty;
 import common.LeetCode;
+
+import java.util.HashSet;
+import java.util.Set;
 
 /**
  * @author RakhmedovRS
  * @created 24-Jun-20
  */
-@LeetCode(id = 791, name = "Custom Sort String", url = "https://leetcode.com/problems/custom-sort-string/")
+@LeetCode(
+	id = 791,
+	name = "Custom Sort String",
+	url = "https://leetcode.com/problems/custom-sort-string/",
+	difficulty = Difficulty.MEDIUM
+)
 public class CustomSortString
 {
-	public String customSortString(String S, String T)
+	public String customSortString(String order, String str)
 	{
-		int[] table = createTable(T);
-		StringBuilder sorted = new StringBuilder();
-		StringBuilder unused = new StringBuilder();
-		for (char ch : S.toCharArray())
+		int[] memo = new int[26];
+		for (char ch : str.toCharArray())
 		{
-			while (table[ch - 'a']-- > 0)
+			memo[ch - 'a']++;
+		}
+
+		Set<Character> seen = new HashSet<>();
+		StringBuilder sb = new StringBuilder();
+		for (char ch : order.toCharArray())
+		{
+			seen.add(ch);
+			while (memo[ch - 'a']-- > 0)
 			{
-				sorted.append(ch);
+				sb.append(ch);
 			}
 		}
 
-		for (char ch : T.toCharArray())
+		for (char ch : str.toCharArray())
 		{
-			while (table[ch - 'a']-- > 0)
+			if (!seen.contains(ch))
 			{
-				unused.append(ch);
+				sb.append(ch);
 			}
 		}
 
-		return sorted.append(unused).toString();
-	}
-
-	private int[] createTable(String string)
-	{
-		int[] table = new int[26];
-		for (char ch : string.toCharArray())
-		{
-			table[ch - 'a']++;
-		}
-		return table;
+		return sb.toString();
 	}
 }
