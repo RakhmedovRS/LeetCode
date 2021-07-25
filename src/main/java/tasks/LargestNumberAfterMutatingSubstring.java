87838a4a450f2e8bf637b40e07f20d2d91999da6
rakhmedovrs@gmail.com <rakhmedovrs@gmail.com>
2021-07-25 16:15:40 -0700

    Largest Number After Mutating Substring

---
+++ src/main/java/tasks/LargestNumberAfterMutatingSubstring.java
@@ -0,0 +1,40 @@
+package tasks;
+
+import common.Difficulty;
+import common.LeetCode;
+
+/**
+ * @author RakhmedovRS
+ * @created 7/25/2021
+ */
+@LeetCode(
+	id = 1946,
+	name = "Largest Number After Mutating Substring",
+	url = "https://leetcode.com/problems/largest-number-after-mutating-substring/",
+	difficulty = Difficulty.MEDIUM
+)
+public class LargestNumberAfterMutatingSubstring
+{
+	public String maximumNumber(String num, int[] change)
+	{
+		char[] chars = num.toCharArray();
+		boolean seenGreater = false;
+		for (int i = 0; i < chars.length; i++)
+		{
+			if (chars[i] - '0' <= change[chars[i] - '0'])
+			{
+				if (chars[i] - '0' < change[chars[i] - '0'])
+				{
+					seenGreater = true;
+				}
+				chars[i] = (char) (change[chars[i] - '0'] + '0');
+			}
+			else if (seenGreater)
+			{
+				break;
+			}
+		}
+
+		return String.valueOf(chars);
+	}
+}
