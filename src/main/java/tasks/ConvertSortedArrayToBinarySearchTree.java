39e757ad26dcc4e3cf966d27a381324b4a6b183a
rakhmedovrs@gmail.com <rakhmedovrs@gmail.com>
2021-07-26 08:08:09 -0700

    Convert Sorted Array to Binary Search Tree updated

--- src/main/java/tasks/ConvertSortedArrayToBinarySearchTree.java
+++ src/main/java/tasks/ConvertSortedArrayToBinarySearchTree.java
@@ -1,5 +1,6 @@
 package tasks;
 
+import common.Difficulty;
 import common.LeetCode;
 import common.TreeNode;
 
@@ -7,29 +8,35 @@ import common.TreeNode;
  * @author RakhmedovRS
  * @created 26-Mar-20
  */
-@LeetCode(id = 108, name = "Convert Sorted Array to Binary Search Tree", url = "https://leetcode.com/problems/convert-sorted-array-to-binary-search-tree/")
+@LeetCode(
+	id = 108,
+	name = "Convert Sorted Array to Binary Search Tree",
+	url = "https://leetcode.com/problems/convert-sorted-array-to-binary-search-tree/",
+	difficulty = Difficulty.EASY
+)
 public class ConvertSortedArrayToBinarySearchTree
 {
-	private TreeNode getChild(int[] nums, int left, int right)
+	public TreeNode sortedArrayToBST(int[] nums)
+	{
+		return sortedArrayToBST(nums, 0, nums.length - 1);
+	}
+
+	private TreeNode sortedArrayToBST(int[] nums, int left, int right)
 	{
 		if (left > right)
 		{
 			return null;
 		}
-		int rootIndex = left + (right - left) / 2;
-		TreeNode node = new TreeNode(nums[rootIndex]);
-		node.left = getChild(nums, left, rootIndex - 1);
-		node.right = getChild(nums, rootIndex + 1, right);
 
-		return node;
-	}
-
-	public TreeNode sortedArrayToBST(int[] nums)
-	{
-		if (nums.length == 0)
+		if (left == right)
 		{
-			return null;
+			return new TreeNode(nums[left]);
 		}
-		return getChild(nums, 0, nums.length - 1);
+
+		int pos = (right + left) / 2;
+		TreeNode node = new TreeNode(nums[pos]);
+		node.left = sortedArrayToBST(nums, left, pos - 1);
+		node.right = sortedArrayToBST(nums, pos + 1, right);
+		return node;
 	}
 }
