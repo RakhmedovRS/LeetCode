1c4e521c244d0232b5f97dcc119ff769600c445c
rakhmedovrs@gmail.com <rakhmedovrs@gmail.com>
2021-07-27 09:25:31 -0700

    3Sum Closest updated

--- src/main/java/tasks/ThreeSumClosest.java
+++ src/main/java/tasks/ThreeSumClosest.java
@@ -1,46 +1,71 @@
 package tasks;
 
+import common.Difficulty;
 import common.LeetCode;
 
 import java.util.Arrays;
+import java.util.TreeMap;
 
 /**
  * @author RakhmedovRS
  * @created 25-Mar-20
  */
-@LeetCode(id = 16, name = "3Sum Closest", url = "https://leetcode.com/problems/3sum-closest/")
+@LeetCode(
+	id = 16,
+	name = "3Sum Closest",
+	url = "https://leetcode.com/problems/3sum-closest/",
+	difficulty = Difficulty.MEDIUM
+)
 public class ThreeSumClosest
 {
 	public int threeSumClosest(int[] nums, int target)
 	{
-		Arrays.sort(nums);
-		int diff = Integer.MAX_VALUE;
-		int closest = -1;
-		int sum;
-		int currentDiff;
-		for (int left = 0; left < nums.length - 2; left++)
+		TreeMap<Integer, Integer> map = new TreeMap<>();
+		for (int num : nums)
 		{
-			int middle = left + 1;
-			int right = nums.length - 1;
-			while (middle != right)
+			map.put(num, map.getOrDefault(num, 0) + 1);
+		}
+
+		Arrays.sort(nums);
+
+		int closest = 1_000_000_000;
+		for (int i = 0; i < nums.length; i++)
+		{
+			int countI = map.remove(nums[i]);
+			if (countI - 1 > 0)
 			{
-				sum = nums[left] + nums[middle] + nums[right];
-				currentDiff = Math.abs(target - sum);
-				if (currentDiff < diff)
+				map.put(nums[i], countI - 1);
+			}
+			for (int j = i + 1; j < nums.length; j++)
+			{
+				int countJ = map.remove(nums[j]);
+				if (countJ - 1 > 0)
 				{
-					closest = sum;
-					diff = currentDiff;
+					map.put(nums[j], countJ - 1);
 				}
 
-				if (sum > target)
+				Integer left = map.ceilingKey(target - (nums[i] + nums[j]));
+				Integer right = map.floorKey(target - (nums[i] + nums[j]));
+				if (left != null)
 				{
-					right--;
+					if (Math.abs(target - closest) > Math.abs(target - (left + nums[i] + nums[j])))
+					{
+						closest = left + nums[i] + nums[j];
+					}
 				}
-				else
+
+				if (right != null)
 				{
-					middle++;
+					if (Math.abs(target - closest) > Math.abs(target - (right + nums[i] + nums[j])))
+					{
+						closest = right + nums[i] + nums[j];
+					}
 				}
+
+				map.put(nums[j], countJ);
 			}
+
+			map.put(nums[i], countI);
 		}
 
 		return closest;
@@ -48,7 +73,7 @@ public class ThreeSumClosest
 
 	public static void main(String[] args)
 	{
-		System.out.println(new ThreeSumClosest().threeSumClosest(new int[]{1,2,4,8,16,32,64,128}, 82)); //82
+		System.out.println(new ThreeSumClosest().threeSumClosest(new int[]{1, 2, 4, 8, 16, 32, 64, 128}, 82)); //82
 		System.out.println(new ThreeSumClosest().threeSumClosest(new int[]{-1, 2, 1, -4}, 1)); //2
 	}
 }
