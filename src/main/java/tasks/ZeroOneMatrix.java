17af6c9bc3f98c3b375ea91e8e8c7c34462bf939
rakhmedovrs@gmail.com <rakhmedovrs@gmail.com>
2021-07-29 08:06:37 -0700

    01 Matrix updated

--- src/main/java/tasks/ZeroOneMatrix.java
+++ src/main/java/tasks/ZeroOneMatrix.java
@@ -1,58 +1,73 @@
 package tasks;
 
+import common.Difficulty;
 import common.LeetCode;
 
 import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.Queue;
 
 /**
  * @author RakhmedovRS
  * @created 21-May-20
  */
-@LeetCode(id = 542, name = "01 Matrix", url = "https://leetcode.com/problems/01-matrix/")
+@LeetCode(
+	id = 542,
+	name = "01 Matrix",
+	url = "https://leetcode.com/problems/01-matrix/",
+	difficulty = Difficulty.MEDIUM
+)
 public class ZeroOneMatrix
 {
-	public int[][] updateMatrix(int[][] matrix)
+	public int[][] updateMatrix(int[][] mat)
 	{
-		int[][] answer = new int[matrix.length][matrix[0].length];
+		int rows = mat.length;
+		int columns = mat[0].length;
+
+		int[][] answer = new int[rows][columns];
 		for (int[] row : answer)
 		{
 			Arrays.fill(row, Integer.MAX_VALUE);
 		}
 
-		for (int row = 0; row < matrix.length; row++)
+		Queue<int[]> queue = new LinkedList<>();
+		for (int row = 0; row < rows; row++)
 		{
-			for (int column = 0; column < matrix[row].length; column++)
+			for (int column = 0; column < columns; column++)
 			{
-				if (matrix[row][column] == 0)
+				if (mat[row][column] == 0)
 				{
-					answer[row][column] = 0;
-					updateMatrix(answer, row - 1, column, 1, matrix);
-					updateMatrix(answer, row + 1, column, 1, matrix);
-					updateMatrix(answer, row, column - 1, 1, matrix);
-					updateMatrix(answer, row, column + 1, 1, matrix);
+					queue.add(new int[]{row, column, 0});
+				}
+			}
+		}
+
+		int[] current;
+		int[][] steps = new int[][]{{-1, 0}, {0, -1}, {1, 0}, {0, 1}};
+		while (!queue.isEmpty())
+		{
+			current = queue.remove();
+			int row = current[0];
+			int column = current[1];
+			int distance = current[2];
+
+			if (answer[row][column] <= distance)
+			{
+				continue;
+			}
+
+			answer[row][column] = distance;
+			for (int[] step : steps)
+			{
+				int nextRow = step[0] + row;
+				int nextColumn = step[1] + column;
+				if (nextRow >= 0 && nextRow < rows && nextColumn >= 0 && nextColumn < columns)
+				{
+					queue.add(new int[]{nextRow, nextColumn, distance + 1});
 				}
 			}
 		}
 
 		return answer;
-	}
-
-	private void updateMatrix(int[][] answer, int row, int column, int steps, int[][] matrix)
-	{
-		if (row < 0
-			|| row == answer.length
-			|| column < 0
-			|| column == answer[row].length
-			|| answer[row][column] <= steps
-			|| matrix[row][column] == 0)
-		{
-			return;
-		}
-
-		answer[row][column] = steps++;
-		updateMatrix(answer, row - 1, column, steps, matrix);
-		updateMatrix(answer, row + 1, column, steps, matrix);
-		updateMatrix(answer, row, column - 1, steps, matrix);
-		updateMatrix(answer, row, column + 1, steps, matrix);
 	}
 }
