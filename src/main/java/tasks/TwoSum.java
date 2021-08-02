304bdece1fc750d8a8b2ee860c50443f7e6057dc
rakhmedovrs@gmail.com <rakhmedovrs@gmail.com>
2021-08-02 07:50:16 -0700

    Two Sum updated

--- src/main/java/tasks/TwoSum.java
+++ src/main/java/tasks/TwoSum.java
@@ -1,5 +1,6 @@
 package tasks;
 
+import common.Difficulty;
 import common.LeetCode;
 
 import java.util.HashMap;
@@ -9,27 +10,24 @@ import java.util.Map;
  * @author RakhmedovRS
  * @created 15-Feb-20
  */
-@LeetCode(id = 1, name = "Two Sum", url = "https://leetcode.com/problems/two-sum/")
+@LeetCode(
+	id = 1,
+	name = "Two Sum",
+	url = "https://leetcode.com/problems/two-sum/",
+	difficulty = Difficulty.EASY
+)
 public class TwoSum
 {
 	public int[] twoSum(int[] nums, int target)
 	{
-		Map<Integer, Integer> seen = new HashMap<>();
+		Map<Integer, Integer> map = new HashMap<>();
 		for (int i = 0; i < nums.length; i++)
 		{
-			if (seen.containsKey(target - nums[i]))
+			if (map.containsKey(target - nums[i]))
 			{
-				if (seen.get(target - nums[i]) != i)
-				{
-					return new int[]{seen.get(target - nums[i]), i};
-				}
-				else
-				{
-					continue;
-				}
+				return new int[]{i, map.get(target - nums[i])};
 			}
-
-			seen.put(nums[i], i);
+			map.put(nums[i], i);
 		}
 
 		return new int[0];
