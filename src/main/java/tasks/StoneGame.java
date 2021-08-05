2f439365834662b618fc26479e37bb1ba6969f66
rakhmedovrs@gmail.com <rakhmedovrs@gmail.com>
2021-08-05 08:38:39 -0700

    Stone Game updated

--- src/main/java/tasks/StoneGame.java
+++ src/main/java/tasks/StoneGame.java
@@ -1,5 +1,6 @@
 package tasks;
 
+import common.Difficulty;
 import common.LeetCode;
 
 import java.util.Arrays;
@@ -8,7 +9,12 @@ import java.util.Arrays;
  * @author RakhmedovRS
  * @created 23-Jul-20
  */
-@LeetCode(id = 877, name = "Stone Game", url = "https://leetcode.com/problems/stone-game/")
+@LeetCode(
+	id = 877,
+	name = "Stone Game",
+	url = "https://leetcode.com/problems/stone-game/",
+	difficulty = Difficulty.MEDIUM
+)
 public class StoneGame
 {
 	public boolean stoneGame(int[] piles)
