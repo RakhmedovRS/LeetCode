96264286845786962387ee8950c947d1ef64e733
rakhmedovrs@gmail.com <rakhmedovrs@gmail.com>
2021-08-08 14:05:00 -0700

    Check If String Is a Prefix of Array

---
+++ src/main/java/tasks/CheckIfStringIsPrefixOfArray.java
@@ -0,0 +1,40 @@
+package tasks;
+
+import common.Difficulty;
+import common.LeetCode;
+
+/**
+ * @author RakhmedovRS
+ * @created 8/8/2021
+ */
+@LeetCode(
+	id = 1961,
+	name = "Check If String Is a Prefix of Array",
+	url = "https://leetcode.com/problems/check-if-string-is-a-prefix-of-array/",
+	difficulty = Difficulty.EASY
+)
+public class CheckIfStringIsPrefixOfArray
+{
+	public boolean isPrefixString(String s, String[] words)
+	{
+		char[] chars = s.toCharArray();
+		int pos = 0;
+		for (String word : words)
+		{
+			for (char ch : word.toCharArray())
+			{
+				if (pos == chars.length || chars[pos++] != ch)
+				{
+					return false;
+				}
+			}
+
+			if (pos == chars.length)
+			{
+				return true;
+			}
+		}
+
+		return pos == chars.length;
+	}
+}
