61112034db784f1939a8e203a6e69da4ae6a8d5d
rakhmedovrs@gmail.com <rakhmedovrs@gmail.com>
2021-08-08 14:06:35 -0700

    Remove Stones to Minimize the Total

---
+++ src/main/java/tasks/RemoveStonesToMinimizeTheTotal.java
@@ -0,0 +1,41 @@
+package tasks;
+
+import common.Difficulty;
+import common.LeetCode;
+
+import java.util.Comparator;
+import java.util.PriorityQueue;
+
+/**
+ * @author RakhmedovRS
+ * @created 8/8/2021
+ */
+@LeetCode(
+	id = 1962,
+	name = "Remove Stones to Minimize the Total",
+	url = "https://leetcode.com/problems/remove-stones-to-minimize-the-total/",
+	difficulty = Difficulty.MEDIUM
+)
+public class RemoveStonesToMinimizeTheTotal
+{
+	public int minStoneSum(int[] piles, int k)
+	{
+		PriorityQueue<Integer> pq = new PriorityQueue<>(Comparator.reverseOrder());
+		for (int pile : piles)
+		{
+			pq.add(pile);
+		}
+
+		for (int i = 0; i < k; i++)
+		{
+			pq.add(pq.peek() % 2 == 0 ? pq.remove() / 2 : (pq.remove() / 2) + 1);
+		}
+
+		int sum = 0;
+		while (!pq.isEmpty())
+		{
+			sum += pq.remove();
+		}
+		return sum;
+	}
+}
