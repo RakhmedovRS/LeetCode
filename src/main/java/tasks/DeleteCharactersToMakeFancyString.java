c4d7288f1cb4dd827d187c67a9387c4a57a3b70b
rakhmedovrs@gmail.com <rakhmedovrs@gmail.com>
2021-08-08 14:01:59 -0700

    Delete Characters to Make Fancy String

---
+++ src/main/java/tasks/DeleteCharactersToMakeFancyString.java
@@ -0,0 +1,32 @@
+package tasks;
+
+import common.Difficulty;
+import common.LeetCode;
+
+/**
+ * @author RakhmedovRS
+ * @created 8/8/2021
+ */
+@LeetCode(
+	id = 1957,
+	name = "Delete Characters to Make Fancy String",
+	url = "https://leetcode.com/problems/delete-characters-to-make-fancy-string/",
+	difficulty = Difficulty.EASY
+)
+public class DeleteCharactersToMakeFancyString
+{
+	public String makeFancyString(String s)
+	{
+		StringBuilder sb = new StringBuilder();
+		for (char ch : s.toCharArray())
+		{
+			if (sb.length() > 1 && sb.charAt(sb.length() - 2) == sb.charAt(sb.length() - 1) && sb.charAt(sb.length() - 1) == ch)
+			{
+				continue;
+			}
+			sb.append(ch);
+		}
+
+		return sb.toString();
+	}
+}
