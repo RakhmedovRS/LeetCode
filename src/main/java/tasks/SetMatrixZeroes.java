c837dba8e27d7fdfd483ce1898c96d2d474df5ce
rakhmedovrs@gmail.com <rakhmedovrs@gmail.com>
2021-08-13 07:40:36 -0700

    Set Matrix Zeroes updated

--- src/main/java/tasks/SetMatrixZeroes.java
+++ src/main/java/tasks/SetMatrixZeroes.java
@@ -1,5 +1,6 @@
 package tasks;
 
+import common.Difficulty;
 import common.LeetCode;
 
 import java.util.Arrays;
@@ -8,82 +9,42 @@ import java.util.Arrays;
  * @author RakhmedovRS
  * @created 01-Apr-20
  */
-@LeetCode(id = 73, name = "Set Matrix Zeroes", url = "https://leetcode.com/problems/set-matrix-zeroes/")
+@LeetCode(
+	id = 73,
+	name = "Set Matrix Zeroes",
+	url = "https://leetcode.com/problems/set-matrix-zeroes/",
+	difficulty = Difficulty.MEDIUM
+)
 public class SetMatrixZeroes
 {
 	public void setZeroes(int[][] matrix)
 	{
-		boolean firstRowZero = false;
-		boolean firstColZero = false;
-
 		int rows = matrix.length;
 		int columns = matrix[0].length;
 
+		boolean[] rowState = new boolean[rows];
+		boolean[] columnState = new boolean[columns];
+
 		for (int row = 0; row < rows; row++)
-		{
-			if (matrix[row][0] == 0)
-			{
-				firstColZero = true;
-				break;
-			}
-		}
-
-		for (int column = 0; column < columns; column++)
-		{
-			if (matrix[0][column] == 0)
-			{
-				firstRowZero = true;
-				break;
-			}
-		}
-
-		for (int row = 1; row < rows; row++)
-		{
-			for (int column = 1; column < columns; column++)
-			{
-				if (matrix[row][column] == 0)
-				{
-					matrix[row][0] = 0;
-					matrix[0][column] = 0;
-				}
-			}
-		}
-
-		for (int row = 1; row < rows; row++)
-		{
-			if (matrix[row][0] == 0)
-			{
-				for (int column = 1; column < columns; column++)
-				{
-					matrix[row][column] = 0;
-				}
-			}
-		}
-
-		for (int column = 1; column < columns; column++)
-		{
-			if (matrix[0][column] == 0)
-			{
-				for (int j = 1; j < rows; j++)
-				{
-					matrix[j][column] = 0;
-				}
-			}
-		}
-
-		if (firstRowZero)
 		{
 			for (int column = 0; column < columns; column++)
 			{
-				matrix[0][column] = 0;
+				if (matrix[row][column] == 0)
+				{
+					rowState[row] = true;
+					columnState[column] = true;
+				}
 			}
 		}
 
-		if (firstColZero)
+		for (int row = 0; row < rows; row++)
 		{
-			for (int row = 0; row < rows; row++)
+			for (int column = 0; column < columns; column++)
 			{
-				matrix[row][0] = 0;
+				if (rowState[row] || columnState[column])
+				{
+					matrix[row][column] = 0;
+				}
 			}
 		}
 	}
