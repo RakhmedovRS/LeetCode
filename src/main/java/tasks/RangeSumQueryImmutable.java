b2ac54c6f462c6561c03cf004ef72c639d145826
rakhmedovrs@gmail.com <rakhmedovrs@gmail.com>
2021-08-16 07:38:40 -0700

    Range Sum Query - Immutable updated

--- src/main/java/tasks/RangeSumQueryImmutable.java
+++ src/main/java/tasks/RangeSumQueryImmutable.java
@@ -1,30 +1,36 @@
 package tasks;
 
+import common.Difficulty;
 import common.LeetCode;
 
 /**
  * @author RakhmedovRS
  * @created 15-May-20
  */
-@LeetCode(id = 303, name = "Range Sum Query - Immutable", url = "https://leetcode.com/problems/range-sum-query-immutable/")
+@LeetCode(
+	id = 303,
+	name = "Range Sum Query - Immutable",
+	url = "https://leetcode.com/problems/range-sum-query-immutable/",
+	difficulty = Difficulty.EASY
+)
 public class RangeSumQueryImmutable
 {
 	class NumArray
 	{
-		int[] sum;
+		int[] nums;
 
 		public NumArray(int[] nums)
 		{
-			sum = new int[nums.length + 1];
-			for (int i = 0; i < nums.length; i++)
+			this.nums = nums;
+			for (int i = 1; i < nums.length; i++)
 			{
-				sum[i + 1] = sum[i] + nums[i];
+				this.nums[i] += this.nums[i - 1];
 			}
 		}
 
-		public int sumRange(int i, int j)
+		public int sumRange(int left, int right)
 		{
-			return sum[j + 1] - sum[i];
+			return nums[right] - (left == 0 ? 0 : nums[left - 1]);
 		}
 	}
 }
