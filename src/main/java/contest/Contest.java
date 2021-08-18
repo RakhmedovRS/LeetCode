d92a16c6165c20de51b78efc8aefe8b8662c7d20
rakhmedovrs@gmail.com <rakhmedovrs@gmail.com>
2021-08-18 14:36:30 -0700

    Rollback changes in Contest

--- src/main/java/contest/Contest.java
+++ src/main/java/contest/Contest.java
@@ -1,7 +1,5 @@
 package contest;
 
-import common.TreeNode;
-
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
@@ -14,25 +12,7 @@ import java.util.stream.Collectors;
 public class Contest
 {
 
-	public int goodNodes(TreeNode root)
-	{
-		return dfs(root.val, root);
-	}
 
-	private int dfs(int val, TreeNode treeNode)
-	{
-		if (treeNode == null)
-		{
-			return 0;
-		}
-
-		if (treeNode.left == null && treeNode.right == null)
-		{
-			return 1;
-		}
-
-		return dfs(val, treeNode.left) + dfs(val, treeNode.right) + dfs(treeNode.val, treeNode.left) + + dfs(treeNode.val, treeNode.right);
-	}
 
 	public static void main(String[] args) throws Exception
 	{
