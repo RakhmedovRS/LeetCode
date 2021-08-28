c9401fff8c413dc656a6e584852561688a3d5ece
rakhmedovrs@gmail.com <rakhmedovrs@gmail.com>
2021-08-28 09:33:17 -0700

    Add Digits updated

--- src/main/java/tasks/AddDigits.java
+++ src/main/java/tasks/AddDigits.java
@@ -1,12 +1,18 @@
 package tasks;
 
+import common.Difficulty;
 import common.LeetCode;
 
 /**
  * @author RakhmedovRS
  * @created 10-Jun-20
  */
-@LeetCode(id = 258, name = "Add Digits", url = "https://leetcode.com/problems/add-digits/")
+@LeetCode(
+	id = 258,
+	name = "Add Digits",
+	url = "https://leetcode.com/problems/add-digits/",
+	difficulty = Difficulty.EASY
+)
 public class AddDigits
 {
 	public int addDigits(int num)
