09ed71201416789af8140d12b0c873cb85547c21
rakhmedovrs@gmail.com <rakhmedovrs@gmail.com>
2021-09-12 18:30:42 -0700

    Number of Pairs of Interchangeable Rectangles

---
+++ src/main/java/tasks/NumberOfPairsOfInterchangeableRectangles.java
@@ -0,0 +1,50 @@
+package tasks;
+
+import common.Difficulty;
+import common.LeetCode;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author RakhmedovRS
+ * @created 9/12/2021
+ */
+@LeetCode(
+	id = 2001,
+	name = "Number of Pairs of Interchangeable Rectangles",
+	url = "https://leetcode.com/problems/number-of-pairs-of-interchangeable-rectangles/",
+	difficulty = Difficulty.MEDIUM
+)
+public class NumberOfPairsOfInterchangeableRectangles
+{
+	public long interchangeableRectangles(int[][] rectangles)
+	{
+		Map<String, Long> map = new HashMap<>();
+		long answer = 0;
+		for (int[] rectangle : rectangles)
+		{
+			int gcd = gcd(rectangle[0], rectangle[1]);
+
+			String ratio = (rectangle[0] / gcd) + "/" + (rectangle[1] / gcd);
+			map.put(ratio, map.getOrDefault(ratio, 0L) + 1);
+
+			long count = map.get(ratio);
+			if (count > 1)
+			{
+				answer += count - 1;
+			}
+		}
+
+		return answer;
+	}
+
+	private int gcd(int x, int y)
+	{
+		if (y == 0)
+		{
+			return x;
+		}
+		return gcd(y, x % y);
+	}
+}
