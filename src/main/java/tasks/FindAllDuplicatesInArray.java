f7a5ef443e919b28deee783f31fca55ac491c14e
rakhmedovrs@gmail.com <rakhmedovrs@gmail.com>
2021-10-06 07:41:27 -0700

    Find All Duplicates in an Array updated

--- src/main/java/tasks/FindAllDuplicatesInArray.java
+++ src/main/java/tasks/FindAllDuplicatesInArray.java
@@ -1,5 +1,6 @@
 package tasks;
 
+import common.Difficulty;
 import common.LeetCode;
 
 import java.util.ArrayList;
@@ -9,21 +10,31 @@ import java.util.List;
  * @author RakhmedovRS
  * @created 13-Jun-20
  */
-@LeetCode(id = 442, name = "Find All Duplicates in an Array", url = "https://leetcode.com/problems/find-all-duplicates-in-an-array/")
+@LeetCode(
+	id = 442,
+	name = "Find All Duplicates in an Array",
+	url = "https://leetcode.com/problems/find-all-duplicates-in-an-array/",
+	difficulty = Difficulty.MEDIUM
+)
 public class FindAllDuplicatesInArray
 {
 	public List<Integer> findDuplicates(int[] nums)
 	{
-		List<Integer> res = new ArrayList<>();
-		for (int i = 0; i < nums.length; i++)
+		int[] memo = new int[100_001];
+		for (int num : nums)
 		{
-			int index = Math.abs(nums[i]) - 1;
-			if (nums[index] < 0)
-			{
-				res.add(Math.abs(index + 1));
-			}
-			nums[index] = -nums[index];
+			memo[num]++;
 		}
-		return res;
+
+		List<Integer> answer = new ArrayList<>();
+		for (int i = 0; i < memo.length; i++)
+		{
+			if (memo[i] == 2)
+			{
+				answer.add(i);
+			}
+		}
+
+		return answer;
 	}
 }
