63009f2055a0e2ccba12c0c7cdd7bd2624a673c9
rakhmedovrs@gmail.com <rakhmedovrs@gmail.com>
2021-10-16 10:15:13 -0700

    Minimum Number of Moves to Seat Everyone

---
+++ src/main/java/tasks/MinimumNumberOfMovesToSeatEveryone.java
@@ -0,0 +1,33 @@
+package tasks;
+
+import common.Difficulty;
+import common.LeetCode;
+
+import java.util.Arrays;
+
+/**
+ * @author RakhmedovRS
+ * @created 10/16/2021
+ */
+@LeetCode(
+	id = 2037,
+	name = "Minimum Number of Moves to Seat Everyone",
+	url = "https://leetcode.com/problems/minimum-number-of-moves-to-seat-everyone/",
+	difficulty = Difficulty.EASY
+)
+public class MinimumNumberOfMovesToSeatEveryone
+{
+	public int minMovesToSeat(int[] seats, int[] students)
+	{
+		int moves = 0;
+		Arrays.sort(seats);
+		Arrays.sort(students);
+
+		for (int i = 0; i < seats.length; i++)
+		{
+			moves += Math.abs(seats[i] - students[i]);
+		}
+
+		return moves;
+	}
+}
