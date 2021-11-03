7b55633152ad1707e49ff19a088278d1539eee61
rakhmedovrs@gmail.com <rakhmedovrs@gmail.com>
2021-11-03 07:40:42 -0700

    Sum Root to Leaf Numbers updated

--- src/main/java/tasks/SumRootToLeafNumbers.java
+++ src/main/java/tasks/SumRootToLeafNumbers.java
@@ -1,5 +1,6 @@
 package tasks;
 
+import common.Difficulty;
 import common.LeetCode;
 import common.TreeNode;
 
@@ -7,37 +8,34 @@ import common.TreeNode;
  * @author RakhmedovRS
  * @created 26-Jun-20
  */
-@LeetCode(id = 129, name = "Sum Root to Leaf Numbers", url = "https://leetcode.com/problems/sum-root-to-leaf-numbers/")
+@LeetCode(
+	id = 129,
+	name = "Sum Root to Leaf Numbers",
+	url = "https://leetcode.com/problems/sum-root-to-leaf-numbers/",
+	difficulty = Difficulty.MEDIUM
+)
 public class SumRootToLeafNumbers
 {
 	public int sumNumbers(TreeNode root)
+	{
+		return sumNumbers(root, 0);
+	}
+
+	public int sumNumbers(TreeNode root, int current)
 	{
 		if (root == null)
 		{
 			return 0;
 		}
 
-		int[] answer = new int[]{0};
-		traverse(root, 0, answer);
-		return answer[0];
-	}
-
-	private void traverse(TreeNode root, int numbers, int[] answer)
-	{
-		if (root == null)
-		{
-			return;
-		}
-
-		numbers *= 10;
-		numbers += root.val;
+		current *= 10;
+		current += root.val;
 
 		if (root.left == null && root.right == null)
 		{
-			answer[0] += numbers;
+			return current;
 		}
 
-		traverse(root.left, numbers, answer);
-		traverse(root.right, numbers, answer);
+		return sumNumbers(root.left, current) + sumNumbers(root.right, current);
 	}
 }
