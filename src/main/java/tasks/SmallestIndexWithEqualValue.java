946f489be2346f24532d71122ad5461f064eaff8
rakhmedovrs@gmail.com <rakhmedovrs@gmail.com>
2021-11-07 09:07:01 -0800

    Smallest Index With Equal Value

---
+++ src/main/java/tasks/SmallestIndexWithEqualValue.java
@@ -0,0 +1,30 @@
+package tasks;
+
+import common.Difficulty;
+import common.LeetCode;
+
+/**
+ * @author RakhmedovRS
+ * @created 11/7/2021
+ */
+@LeetCode(
+	id = 2057,
+	name = "Smallest Index With Equal Value",
+	url = "https://leetcode.com/problems/smallest-index-with-equal-value/",
+	difficulty = Difficulty.EASY
+)
+public class SmallestIndexWithEqualValue
+{
+	public int smallestEqual(int[] nums)
+	{
+		for (int i = 0; i < nums.length; i++)
+		{
+			if ((i % 10) == nums[i])
+			{
+				return i;
+			}
+		}
+
+		return -1;
+	}
+}
