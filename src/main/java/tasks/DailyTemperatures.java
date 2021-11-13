4a0ead713e997c605b966606be70af5f2381d8c2
rakhmedovrs@gmail.com <rakhmedovrs@gmail.com>
2021-11-13 10:30:04 -0800

    Daily Temperatures updated

--- src/main/java/tasks/DailyTemperatures.java
+++ src/main/java/tasks/DailyTemperatures.java
@@ -1,5 +1,6 @@
 package tasks;
 
+import common.Difficulty;
 import common.LeetCode;
 
 import java.util.Deque;
@@ -9,23 +10,27 @@ import java.util.LinkedList;
  * @author RakhmedovRS
  * @created 02-Jul-20
  */
-@LeetCode(id = 739, name = "Daily Temperatures", url = "https://leetcode.com/problems/daily-temperatures/")
+@LeetCode(
+	id = 739,
+	name = "Daily Temperatures",
+	url = "https://leetcode.com/problems/daily-temperatures/",
+	difficulty = Difficulty.MEDIUM
+)
 public class DailyTemperatures
 {
-	public int[] dailyTemperatures(int[] T)
+	public int[] dailyTemperatures(int[] temperatures)
 	{
-		int[] answer = new int[T.length];
-
-		Deque<Integer> stack = new LinkedList<>();
-		for (int i = 0; i < T.length; i++)
+		int[] answer = new int[temperatures.length];
+		LinkedList<Integer> list = new LinkedList<>();
+		for (int i = 0; i < temperatures.length; i++)
 		{
-			while (!stack.isEmpty() && T[stack.peek()] < T[i])
+			while (!list.isEmpty() && temperatures[i] > temperatures[list.getLast()])
 			{
-				int index = stack.poll();
-				answer[index] = i - index;
+				int prev =  list.removeLast();
+				answer[prev] = i - prev;
 			}
 
-			stack.push(i);
+			list.addLast(i);
 		}
 
 		return answer;
