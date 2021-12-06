2acb1a6bf898aabb22706e77e2f829fd1467f3c8
rakhmedovrs@gmail.com <rakhmedovrs@gmail.com>
2021-12-06 08:09:57 -0800

    Minimum Cost to Move Chips to The Same Position updated

--- src/main/java/tasks/MinimumCostToMoveChipsToTheSamePosition.java
+++ src/main/java/tasks/MinimumCostToMoveChipsToTheSamePosition.java
@@ -1,12 +1,18 @@
 package tasks;
 
+import common.Difficulty;
 import common.LeetCode;
 
 /**
  * @author RakhmedovRS
  * @created 10/19/2020
  */
-@LeetCode(id = 1217, name = "Minimum Cost to Move Chips to The Same Position", url = "https://leetcode.com/problems/minimum-cost-to-move-chips-to-the-same-position/")
+@LeetCode(
+	id = 1217,
+	name = "Minimum Cost to Move Chips to The Same Position",
+	url = "https://leetcode.com/problems/minimum-cost-to-move-chips-to-the-same-position/",
+	difficulty = Difficulty.EASY
+)
 public class MinimumCostToMoveChipsToTheSamePosition
 {
 	public int minCostToMoveChips(int[] position)
