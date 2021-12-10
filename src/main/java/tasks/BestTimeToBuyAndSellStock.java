51ccfac399e6636c7c1af38cb305de3dab8f3a16
rakhmedovrs@gmail.com <rakhmedovrs@gmail.com>
2021-12-10 08:06:13 -0800

    Best Time to Buy and Sell Stock updated

--- src/main/java/tasks/BestTimeToBuyAndSellStock.java
+++ src/main/java/tasks/BestTimeToBuyAndSellStock.java
@@ -1,12 +1,18 @@
 package tasks;
 
+import common.Difficulty;
 import common.LeetCode;
 
 /**
  * @author RakhmedovRS
  * @created 20-Feb-20
  */
-@LeetCode(id = 121, name = "Best Time to Buy and Sell Stock", url = "https://leetcode.com/problems/best-time-to-buy-and-sell-stock/")
+@LeetCode(
+	id = 121,
+	name = "Best Time to Buy and Sell Stock",
+	url = "https://leetcode.com/problems/best-time-to-buy-and-sell-stock/",
+	difficulty = Difficulty.EASY
+)
 public class BestTimeToBuyAndSellStock
 {
 	public int maxProfit(int[] prices)
