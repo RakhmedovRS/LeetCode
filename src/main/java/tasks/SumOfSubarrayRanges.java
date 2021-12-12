0c3b84ec3dcd58665440160a191f2f1a42dbdd2d
rakhmedovrs@gmail.com <rakhmedovrs@gmail.com>
2021-12-12 12:28:05 -0800

    Sum of Subarray Ranges

---
+++ src/main/java/tasks/SumOfSubarrayRanges.java
@@ -0,0 +1,36 @@
+package tasks;
+
+import common.Difficulty;
+import common.LeetCode;
+
+/**
+ * @author RakhmedovRS
+ * @created 12/12/2021
+ */
+@LeetCode(
+	id = 2104,
+	name = "Sum of Subarray Ranges",
+	url = "https://leetcode.com/problems/sum-of-subarray-ranges/",
+	difficulty = Difficulty.MEDIUM
+)
+public class SumOfSubarrayRanges
+{
+	public long subArrayRanges(int[] nums)
+	{
+		long sum = 0;
+		for (int i = 0; i < nums.length; i++)
+		{
+			int min = nums[i];
+			int max = nums[i];
+			for (int j = i; j < nums.length; j++)
+			{
+				min = Math.min(min, nums[j]);
+				max = Math.max(max, nums[j]);
+
+				sum += (max - min);
+			}
+		}
+
+		return sum;
+	}
+}
