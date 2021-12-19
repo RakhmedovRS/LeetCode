f73a6ed576c486ae38cae1ba6c6a507ab7ceaf98
rakhmedovrs@gmail.com <rakhmedovrs@gmail.com>
2021-12-19 15:19:26 -0800

    Find First Palindromic String in the Array

---
+++ src/main/java/tasks/FindFirstPalindromicStringInTheArray.java
@@ -0,0 +1,48 @@
+package tasks;
+
+import common.Difficulty;
+import common.LeetCode;
+
+/**
+ * @author RakhmedovRS
+ * @created 12/19/2021
+ */
+@LeetCode(
+	id = 2108,
+	name = "Find First Palindromic String in the Array",
+	url = "https://leetcode.com/problems/find-first-palindromic-string-in-the-array/",
+	difficulty = Difficulty.EASY
+)
+public class FindFirstPalindromicStringInTheArray
+{
+	public String firstPalindrome(String[] words)
+	{
+		for (String s : words)
+		{
+			if (isPalindrome(s))
+			{
+				return s;
+			}
+		}
+
+		return "";
+	}
+
+	private boolean isPalindrome(String string)
+	{
+		int start = 0;
+		int end = string.length() - 1;
+
+		while (start < end)
+		{
+			if (string.charAt(start) != string.charAt(end))
+			{
+				return false;
+			}
+			start++;
+			end--;
+		}
+
+		return true;
+	}
+}
