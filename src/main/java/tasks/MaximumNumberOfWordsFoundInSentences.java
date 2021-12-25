9b45a19bb368da1317404552cdd4be82ca27e954
rakhmedovrs@gmail.com <rakhmedovrs@gmail.com>
2021-12-25 08:17:58 -0800

    Maximum Number of Words Found in Sentences

---
+++ src/main/java/tasks/MaximumNumberOfWordsFoundInSentences.java
@@ -0,0 +1,28 @@
+package tasks;
+
+import common.Difficulty;
+import common.LeetCode;
+
+/**
+ * @author RakhmedovRS
+ * @created 12/25/2021
+ */
+@LeetCode(
+	id = 2114,
+	name = "Maximum Number of Words Found in Sentences",
+	url = "https://leetcode.com/problems/maximum-number-of-words-found-in-sentences/",
+	difficulty = Difficulty.EASY
+)
+public class MaximumNumberOfWordsFoundInSentences
+{
+	public int mostWordsFound(String[] sentences)
+	{
+		int max = 0;
+		for (String s : sentences)
+		{
+			max = Math.max(max, s.split(" ").length);
+		}
+
+		return max;
+	}
+}
