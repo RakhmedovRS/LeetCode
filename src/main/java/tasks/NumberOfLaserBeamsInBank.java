096be8bab1026dd53ab5fe5ed0c2d55e1fef93ae
rakhmedovrs@gmail.com <rakhmedovrs@gmail.com>
2022-01-02 14:27:28 -0800

    Number of Laser Beams in a Bank

---
+++ src/main/java/tasks/NumberOfLaserBeamsInBank.java
@@ -0,0 +1,43 @@
+package tasks;
+
+import common.Difficulty;
+import common.LeetCode;
+
+/**
+ * @author RakhmedovRS
+ * @created 1/2/2022
+ */
+@LeetCode(
+	id = 2125,
+	name = "Number of Laser Beams in a Bank",
+	url = "https://leetcode.com/problems/number-of-laser-beams-in-a-bank/",
+	difficulty = Difficulty.MEDIUM
+)
+public class NumberOfLaserBeamsInBank
+{
+	public int numberOfBeams(String[] bank)
+	{
+		int total = 0;
+		int prev = 0;
+		int current = 0;
+		for (String row : bank)
+		{
+			current = 0;
+			for (char ch : row.toCharArray())
+			{
+				if (ch == '1')
+				{
+					current++;
+				}
+			}
+
+			if (current != 0)
+			{
+				total += prev * current;
+				prev = current;
+			}
+		}
+
+		return total;
+	}
+}
