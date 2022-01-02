4705e21eba9c52d287a4dbeee86ef042cb641462
rakhmedovrs@gmail.com <rakhmedovrs@gmail.com>
2022-01-02 14:29:04 -0800

    Destroying Asteroids

---
+++ src/main/java/tasks/DestroyingAsteroids.java
@@ -0,0 +1,38 @@
+package tasks;
+
+import common.Difficulty;
+import common.LeetCode;
+
+import java.util.Arrays;
+
+/**
+ * @author RakhmedovRS
+ * @created 1/2/2022
+ */
+@LeetCode(
+	id = 2126,
+	name = "Destroying Asteroids",
+	url = "https://leetcode.com/problems/destroying-asteroids/",
+	difficulty = Difficulty.MEDIUM
+)
+public class DestroyingAsteroids
+{
+	public boolean asteroidsDestroyed(int mass, int[] asteroids)
+	{
+		Arrays.sort(asteroids);
+		long current = mass;
+		for (int asteroid : asteroids)
+		{
+			if (current >= asteroid)
+			{
+				current += asteroid;
+			}
+			else
+			{
+				return false;
+			}
+		}
+
+		return true;
+	}
+}
