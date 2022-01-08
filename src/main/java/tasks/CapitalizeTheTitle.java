9df2b1128eadabbe59f94bad40554509bbb9c4c9
rakhmedovrs@gmail.com <rakhmedovrs@gmail.com>
2022-01-08 08:08:22 -0800

    Capitalize the Title

---
+++ src/main/java/tasks/CapitalizeTheTitle.java
@@ -0,0 +1,40 @@
+package tasks;
+
+import common.Difficulty;
+import common.LeetCode;
+
+/**
+ * @author RakhmedovRS
+ * @created 1/8/2022
+ */
+@LeetCode(
+	id = 2129,
+	name = "Capitalize the Title",
+	url = "https://leetcode.com/problems/capitalize-the-title/",
+	difficulty = Difficulty.EASY
+)
+public class CapitalizeTheTitle
+{
+	public String capitalizeTitle(String title)
+	{
+		StringBuilder sb = new StringBuilder();
+		for (String word : title.split(" "))
+		{
+			if (sb.length() > 0)
+			{
+				sb.append(" ");
+			}
+
+			char[] chars = word.toLowerCase().toCharArray();
+
+			if (word.length() > 2)
+			{
+				chars[0] = Character.toUpperCase(chars[0]);
+			}
+
+			sb.append(chars);
+		}
+
+		return sb.toString();
+	}
+}
