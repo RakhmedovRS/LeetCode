354091b38981e0a5cadd574c626dd759c27ccbbe
rakhmedovrs@gmail.com <rakhmedovrs@gmail.com>
2022-01-22 11:29:30 -0800

    Minimum Cost of Buying Candies With Discount

---
+++ src/main/java/tasks/MinimumCostOfBuyingCandiesWithDiscount.java
@@ -0,0 +1,48 @@
+package tasks;
+
+import common.Difficulty;
+import common.LeetCode;
+
+import java.util.Comparator;
+import java.util.PriorityQueue;
+
+/**
+ * @author RakhmedovRS
+ * @created 1/22/2022
+ */
+@LeetCode(
+	id = 2144,
+	name = "Minimum Cost of Buying Candies With Discount",
+	url = "https://leetcode.com/problems/minimum-cost-of-buying-candies-with-discount/",
+	difficulty = Difficulty.EASY
+)
+public class MinimumCostOfBuyingCandiesWithDiscount
+{
+	public int minimumCost(int[] cost)
+	{
+		PriorityQueue<Integer> pq = new PriorityQueue<>(Comparator.reverseOrder());
+		for (int c : cost)
+		{
+			pq.add(c);
+		}
+
+		int result = 0;
+		int num = 0;
+		while (!pq.isEmpty())
+		{
+			num++;
+			if (num % 3 == 0)
+			{
+				pq.remove();
+			}
+			else
+			{
+				result += pq.remove();
+			}
+
+			num %= 3;
+		}
+
+		return result;
+	}
+}
