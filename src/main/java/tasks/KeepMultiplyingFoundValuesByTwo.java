550ce41e9fdf57723427cf293318256767fad2d2
rakhmedovrs@gmail.com <rakhmedovrs@gmail.com>
2022-01-30 10:31:27 -0800

    Keep Multiplying Found Values by Two

---
+++ src/main/java/tasks/KeepMultiplyingFoundValuesByTwo.java
@@ -0,0 +1,36 @@
+package tasks;
+
+import common.Difficulty;
+import common.LeetCode;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * @author RakhmedovRS
+ * @created 1/30/2022
+ */
+@LeetCode(
+	id = 2154,
+	name = "Keep Multiplying Found Values by Two",
+	url = "https://leetcode.com/problems/keep-multiplying-found-values-by-two/",
+	difficulty = Difficulty.EASY
+)
+public class KeepMultiplyingFoundValuesByTwo
+{
+	public int findFinalValue(int[] nums, int original)
+	{
+		Set<Integer> vals = new HashSet<>();
+		for (int num : nums)
+		{
+			vals.add(num);
+		}
+
+		while (vals.contains(original))
+		{
+			original *= 2;
+		}
+
+		return original;
+	}
+}
