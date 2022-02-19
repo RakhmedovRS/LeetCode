adfa8f276ca6afbdc97f83d41e677a43ed814510
rakhmedovrs@gmail.com <rakhmedovrs@gmail.com>
2022-02-19 08:17:47 -0800

    Maximum Split of Positive Even Integers

---
+++ src/main/java/tasks/MaximumSplitOfPositiveEvenIntegers.java
@@ -0,0 +1,56 @@
+package tasks;
+
+import common.Difficulty;
+import common.LeetCode;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * @author RakhmedovRS
+ * @created 2/19/2022
+ */
+@LeetCode(
+	id = 2178,
+	name = "Maximum Split of Positive Even Integers",
+	url = "https://leetcode.com/problems/maximum-split-of-positive-even-integers/",
+	difficulty = Difficulty.MEDIUM
+)
+public class MaximumSplitOfPositiveEvenIntegers
+{
+	public List<Long> maximumEvenSplit(long finalSum)
+	{
+		if (finalSum % 2 != 0)
+		{
+			return new ArrayList<>();
+		}
+
+		LinkedList<Long> answer = new LinkedList<>();
+		if (finalSum == 2 || finalSum == 4)
+		{
+			answer.addLast(finalSum);
+		}
+		else
+		{
+			answer.addLast(2L);
+			long sum = 2;
+			while (finalSum != sum)
+			{
+				if (sum > finalSum)
+				{
+					long diff = sum - finalSum;
+					answer.remove(diff);
+					sum -= diff;
+				}
+				else
+				{
+					sum += answer.getLast() + 2;
+					answer.addLast(answer.getLast() + 2);
+				}
+			}
+		}
+
+		return answer;
+	}
+}
