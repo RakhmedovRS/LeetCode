a2307847d0e87dd0149988506cc84d0c7ff53943
rakhmedovrs@gmail.com <rakhmedovrs@gmail.com>
2022-02-19 08:14:35 -0800

    Count Equal and Divisible Pairs in an Array

---
+++ src/main/java/tasks/CountEqualAndDivisiblePairsInArray.java
@@ -0,0 +1,34 @@
+package tasks;
+
+import common.Difficulty;
+import common.LeetCode;
+
+/**
+ * @author RakhmedovRS
+ * @created 2/19/2022
+ */
+@LeetCode(
+	id = 2176,
+	name = "Count Equal and Divisible Pairs in an Array",
+	url = "https://leetcode.com/problems/count-equal-and-divisible-pairs-in-an-array/",
+	difficulty = Difficulty.EASY
+)
+public class CountEqualAndDivisiblePairsInArray
+{
+	public int countPairs(int[] nums, int k)
+	{
+		int count = 0;
+		for (int i = 0; i < nums.length; i++)
+		{
+			for (int j = i + 1; j < nums.length; j++)
+			{
+				if (nums[i] == nums[j] && i * j % k == 0)
+				{
+					count++;
+				}
+			}
+		}
+
+		return count;
+	}
+}
