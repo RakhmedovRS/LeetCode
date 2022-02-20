e394ef5f5e382cd20755de6464669ff9f5b25078
rakhmedovrs@gmail.com <rakhmedovrs@gmail.com>
2022-02-20 09:19:04 -0800

    Merge Nodes in Between Zeros

---
+++ src/main/java/tasks/MergeNodesInBetweenZeros.java
@@ -0,0 +1,51 @@
+package tasks;
+
+import common.Difficulty;
+import common.LeetCode;
+import common.ListNode;
+
+/**
+ * @author RakhmedovRS
+ * @created 2/20/2022
+ */
+@LeetCode(
+	id = 2181,
+	name = "Merge Nodes in Between Zeros",
+	url = "https://leetcode.com/problems/merge-nodes-in-between-zeros/",
+	difficulty = Difficulty.MEDIUM
+)
+public class MergeNodesInBetweenZeros
+{
+	public ListNode mergeNodes(ListNode head)
+	{
+		ListNode dummy = new ListNode();
+		int sum = 0;
+		ListNode current = dummy;
+		boolean seenZero = false;
+		while (head != null)
+		{
+			if (head.val == 0)
+			{
+				if (seenZero)
+				{
+					current.next = new ListNode(sum);
+					current = current.next;
+					sum = 0;
+				}
+				else
+				{
+					seenZero = true;
+				}
+			}
+
+			if (seenZero)
+			{
+				sum += head.val;
+			}
+
+			head = head.next;
+		}
+
+		return dummy.next;
+	}
+}
