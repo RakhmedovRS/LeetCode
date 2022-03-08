f6e142736c4f196ca602cd7b707d975608735aae
rakhmedovrs@gmail.com <rakhmedovrs@gmail.com>
2022-03-08 10:31:51 -0800

    Kids With the Greatest Number of Candies updated

--- src/main/java/tasks/KidsWithTheGreatestNumberOfCandies.java
+++ src/main/java/tasks/KidsWithTheGreatestNumberOfCandies.java
@@ -1,5 +1,6 @@
 package tasks;
 
+import common.Difficulty;
 import common.LeetCode;
 
 import java.util.ArrayList;
@@ -9,7 +10,12 @@ import java.util.List;
  * @author RakhmedovRS
  * @created 02-Jun-20
  */
-@LeetCode(id = 1431, name = "Kids With the Greatest Number of Candies", url = "https://leetcode.com/problems/kids-with-the-greatest-number-of-candies/")
+@LeetCode(
+	id = 1431,
+	name = "Kids With the Greatest Number of Candies",
+	url = "https://leetcode.com/problems/kids-with-the-greatest-number-of-candies/",
+	difficulty = Difficulty.EASY
+)
 public class KidsWithTheGreatestNumberOfCandies
 {
 	public List<Boolean> kidsWithCandies(int[] candies, int extraCandies)
