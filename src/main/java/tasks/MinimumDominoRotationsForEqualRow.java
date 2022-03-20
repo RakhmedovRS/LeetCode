bc001903076b4791d4d21f0c6ce55d21d00c064d
rakhmedovrs@gmail.com <rakhmedovrs@gmail.com>
2022-03-20 15:28:04 -0700

    Minimum Domino Rotations For Equal Row updated

--- src/main/java/tasks/MinimumDominoRotationsForEqualRow.java
+++ src/main/java/tasks/MinimumDominoRotationsForEqualRow.java
@@ -1,44 +1,65 @@
 package tasks;
 
+import common.Difficulty;
 import common.LeetCode;
 
 /**
  * @author RakhmedovRS
  * @created 10/19/2020
  */
-@LeetCode(id = 1007, name = "Minimum Domino Rotations For Equal Row", url = "https://leetcode.com/problems/minimum-domino-rotations-for-equal-row/")
+@LeetCode(
+	id = 1007,
+	name = "Minimum Domino Rotations For Equal Row",
+	url = "https://leetcode.com/problems/minimum-domino-rotations-for-equal-row/",
+	difficulty = Difficulty.MEDIUM
+)
 public class MinimumDominoRotationsForEqualRow
 {
 	public int minDominoRotations(int[] A, int[] B)
 	{
-		int a = countOfSwaps(A[0], A, B);
-		int b = countOfSwaps(A[0], B, A);
-		int c = countOfSwaps(B[0], A, B);
-		int d = countOfSwaps(B[0], B, A);
-		int min = Math.min(a, Math.min(b, Math.min(c, d)));
+		int min = Integer.MAX_VALUE;
 
-		return min == Integer.MAX_VALUE ? -1 : min;
-	}
-
-	private int countOfSwaps(int val, int[] A, int[] B)
-	{
-		int count = 0;
-		for (int i = 0; i < A.length; i++)
+		for (int i = 1; i <= 6; i++)
 		{
-			if (A[i] == val)
+			int down = 0;
+			int up = 0;
+			for (int pos = 0; pos < A.length; pos++)
 			{
-				continue;
+				if (A[pos] == i)
+				{
+					up++;
+				}
+				else if (B[pos] == i)
+				{
+					down++;
+				}
 			}
-			else if (B[i] == val)
+
+			if (up + down == A.length)
 			{
-				count++;
+				min = Math.min(min, down);
 			}
-			else
+
+			down = 0;
+			up = 0;
+			for (int pos = 0; pos < A.length; pos++)
 			{
-				return Integer.MAX_VALUE;
+				if (B[pos] == i)
+				{
+					down++;
+				}
+				else if (A[pos] == i)
+				{
+					up++;
+				}
+			}
+
+			if (up + down == A.length)
+			{
+				min = Math.min(min, up);
 			}
 		}
 
-		return count;
+		return min == Integer.MAX_VALUE ? -1 : min;
 	}
 }
