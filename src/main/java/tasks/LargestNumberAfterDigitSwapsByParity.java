01d92a7aa21e05e2410541e261d99f6e216838bc
rakhmedovrs@gmail.com <rakhmedovrs@gmail.com>
2022-04-10 12:48:13 -0700

    Largest Number After Digit Swaps by Parity

---
+++ src/main/java/tasks/LargestNumberAfterDigitSwapsByParity.java
@@ -0,0 +1,54 @@
+package tasks;
+
+import common.Difficulty;
+import common.LeetCode;
+
+import java.util.Comparator;
+import java.util.PriorityQueue;
+
+/**
+ * @author RakhmedovRS
+ * @created 4/10/2022
+ */
+@LeetCode(
+	id = 2231,
+	name = "Largest Number After Digit Swaps by Parity",
+	url = "https://leetcode.com/problems/largest-number-after-digit-swaps-by-parity/",
+	difficulty = Difficulty.EASY
+)
+public class LargestNumberAfterDigitSwapsByParity
+{
+	public int largestInteger(int num)
+	{
+		PriorityQueue<Integer> even = new PriorityQueue<>(Comparator.reverseOrder());
+		PriorityQueue<Integer> odd = new PriorityQueue<>(Comparator.reverseOrder());
+		char[] chars = String.valueOf(num).toCharArray();
+		for (int i = 0; i < chars.length; i++)
+		{
+			if ((chars[i] - '0') % 2 == 0)
+			{
+				even.add(chars[i] - '0');
+			}
+			else
+			{
+				odd.add(chars[i] - '0');
+			}
+		}
+
+		int res = 0;
+		for (int i = 0; i < chars.length; i++)
+		{
+			res *= 10;
+			if ((chars[i] - '0') % 2 == 0)
+			{
+				res += even.remove();
+			}
+			else
+			{
+				res += odd.remove();
+			}
+		}
+
+		return res;
+	}
+}
