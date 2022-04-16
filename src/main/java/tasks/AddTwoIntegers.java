f2e1f4a13c70587950f8b1cde51ec00861519564
rakhmedovrs@gmail.com <rakhmedovrs@gmail.com>
2022-04-16 09:19:55 -0700

    Add Two Integers

---
+++ src/main/java/tasks/AddTwoIntegers.java
@@ -0,0 +1,22 @@
+package tasks;
+
+import common.Difficulty;
+import common.LeetCode;
+
+/**
+ * @author RakhmedovRS
+ * @created 4/16/2022
+ */
+@LeetCode(
+	id = 2235,
+	name = "Add Two Integers",
+	url = "https://leetcode.com/problems/add-two-integers/",
+	difficulty = Difficulty.EASY
+)
+public class AddTwoIntegers
+{
+	public int sum(int num1, int num2)
+	{
+		return num1 + num2;
+	}
+}
