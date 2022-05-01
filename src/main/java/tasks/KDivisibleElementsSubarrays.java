ab645d145f5d28b9a2860d1f67bc3a4babc19940
rakhmedovrs@gmail.com <rakhmedovrs@gmail.com>
2022-05-01 10:11:27 -0700

    K Divisible Elements Subarrays

---
+++ src/main/java/tasks/KDivisibleElementsSubarrays.java
@@ -0,0 +1,46 @@
+package tasks;
+
+import common.Difficulty;
+import common.LeetCode;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * @author RakhmedovRS
+ * @created 5/1/2022
+ */
+@LeetCode(
+	id = 2261,
+	name = "K Divisible Elements Subarrays",
+	url = "https://leetcode.com/problems/k-divisible-elements-subarrays/",
+	difficulty = Difficulty.MEDIUM
+)
+public class KDivisibleElementsSubarrays
+{
+	public int countDistinct(int[] nums, int k, int p)
+	{
+		Set<String> set = new HashSet<>();
+		for (int i = 0; i < nums.length; i++)
+		{
+			int count = 0;
+			for (int j = i; j < nums.length; j++)
+			{
+				if (nums[j] % p == 0)
+				{
+					count++;
+				}
+
+				if (count > k)
+				{
+					break;
+				}
+
+				set.add(Arrays.toString(Arrays.copyOfRange(nums, i, j + 1)));
+			}
+		}
+
+		return set.size();
+	}
+}
