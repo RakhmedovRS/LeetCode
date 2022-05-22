08d060b78a20aba91b57e84c7d8ca2f955d19ccf
rakhmedovrs@gmail.com <rakhmedovrs@gmail.com>
2022-05-22 09:25:53 -0700

    Percentage of Letter in String

---
+++ src/main/java/tasks/PercentageOfLetterInString.java
@@ -0,0 +1,31 @@
+package tasks;
+
+import common.Difficulty;
+import common.LeetCode;
+
+/**
+ * @author RakhmedovRS
+ * @created 5/22/2022
+ */
+@LeetCode(
+	id = 2278,
+	name = "Percentage of Letter in String",
+	url = "https://leetcode.com/problems/percentage-of-letter-in-string/",
+	difficulty = Difficulty.EASY
+)
+public class PercentageOfLetterInString
+{
+	public int percentageLetter(String s, char letter)
+	{
+		double count = 0;
+		for (char ch : s.toCharArray())
+		{
+			if (ch == letter)
+			{
+				count++;
+			}
+		}
+
+		return (int) ((count / s.length()) * 100);
+	}
+}
