5e8692c46c7decc5f2e62ebfc78996114964f287
rakhmedovrs@gmail.com <rakhmedovrs@gmail.com>
2022-05-31 08:39:53 -0700

    Smallest Range I updated

--- src/main/java/tasks/SmallestRangeI.java
+++ src/main/java/tasks/SmallestRangeI.java
@@ -1,12 +1,18 @@
 package tasks;
 
+import common.Difficulty;
 import common.LeetCode;
 
 /**
  * @author RakhmedovRS
  * @created 22-Jun-20
  */
-@LeetCode(id = 908, name = "Smallest Range I", url = "https://leetcode.com/problems/smallest-range-i/")
+@LeetCode(
+	id = 908,
+	name = "Smallest Range I",
+	url = "https://leetcode.com/problems/smallest-range-i/",
+	difficulty = Difficulty.EASY
+)
 public class SmallestRangeI
 {
 	public int smallestRangeI(int[] A, int K)
