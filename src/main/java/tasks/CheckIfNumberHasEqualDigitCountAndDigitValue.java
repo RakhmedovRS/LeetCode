a99356fcb29c91b767ea3d54df54da0e15dff9ff
rakhmedovrs@gmail.com <rakhmedovrs@gmail.com>
2022-06-12 18:22:42 -0700

    Check if Number Has Equal Digit Count and Digit Value

---
+++ src/main/java/tasks/CheckIfNumberHasEqualDigitCountAndDigitValue.java
@@ -0,0 +1,37 @@
+package tasks;
+
+import common.Difficulty;
+import common.LeetCode;
+
+/**
+ * @author RakhmedovRS
+ * @created 6/12/2022
+ */
+@LeetCode(
+	id = 2283,
+	name = "Check if Number Has Equal Digit Count and Digit Value",
+	url = "https://leetcode.com/problems/check-if-number-has-equal-digit-count-and-digit-value/",
+	difficulty = Difficulty.EASY
+)
+public class CheckIfNumberHasEqualDigitCountAndDigitValue
+{
+	public boolean digitCount(String num)
+	{
+		int[] memo = new int[10];
+		char[] chars = num.toCharArray();
+		for (char ch : chars)
+		{
+			memo[ch - '0']++;
+		}
+
+		for (int i = 0; i < chars.length; i++)
+		{
+			if (memo[i] != chars[i] - '0')
+			{
+				return false;
+			}
+		}
+
+		return true;
+	}
+}
