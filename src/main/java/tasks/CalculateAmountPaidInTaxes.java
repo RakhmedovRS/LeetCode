409d31cefc3ac0f5f2531bb727c9999715f0bf81
rakhmedovrs@gmail.com <rakhmedovrs@gmail.com>
2022-06-16 19:49:18 -0700

    Calculate Amount Paid in Taxes

---
+++ src/main/java/tasks/CalculateAmountPaidInTaxes.java
@@ -0,0 +1,33 @@
+package tasks;
+
+import common.Difficulty;
+import common.LeetCode;
+
+/**
+ * @author RakhmedovRS
+ * @created 6/16/2022
+ */
+@LeetCode(
+	id = 2303,
+	name = "Calculate Amount Paid in Taxes",
+	url = "https://leetcode.com/problems/calculate-amount-paid-in-taxes/",
+	difficulty = Difficulty.EASY
+)
+public class CalculateAmountPaidInTaxes
+{
+	public double calculateTax(int[][] brackets, int income)
+	{
+		double tax = 0;
+		double rest = income;
+		int prev = 0;
+		for (int[] bracket : brackets)
+		{
+			double t = (Math.min(rest, bracket[0] - prev) * bracket[1]) / 100;
+			rest -= Math.min(rest, bracket[0] - prev);
+			tax += t;
+			prev = bracket[0];
+		}
+
+		return tax;
+	}
+}
