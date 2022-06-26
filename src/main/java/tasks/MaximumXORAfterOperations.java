f79f319237d9e1b7680d86c9c6a3c32f29abf631
rakhmedovrs@gmail.com <rakhmedovrs@gmail.com>
2022-06-26 10:30:17 -0700

    Maximum XOR After Operations

---
+++ src/main/java/tasks/MaximumXORAfterOperations.java
@@ -0,0 +1,25 @@
+package tasks;
+
+import common.Difficulty;
+import common.LeetCode;
+
+/**
+ * @author RakhmedovRS
+ * @created 6/26/2022
+ */
+@LeetCode(
+	id = 2317,
+	name = "Maximum XOR After Operations",
+	url = "https://leetcode.com/problems/maximum-xor-after-operations/",
+	difficulty = Difficulty.MEDIUM
+)
+public class MaximumXORAfterOperations
+{
+	public int maximumXOR(int[] nums)
+	{
+		int res = 0;
+		for (int a : nums)
+			res |= a;
+		return res;
+	}
+}
