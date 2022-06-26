3e7997dab5f3af45712b97b061dbfdca218265c1
rakhmedovrs@gmail.com <rakhmedovrs@gmail.com>
2022-06-25 23:41:07 -0700

    Count Asterisks

---
+++ src/main/java/tasks/CountAsterisks.java
@@ -0,0 +1,38 @@
+package tasks;
+
+import common.Difficulty;
+import common.LeetCode;
+
+/**
+ * @author RakhmedovRS
+ * @created 6/25/2022
+ */
+@LeetCode(
+	id = 2315,
+	name = "Count Asterisks",
+	url = "https://leetcode.com/problems/count-asterisks/",
+	difficulty = Difficulty.EASY
+)
+public class CountAsterisks
+{
+	public int countAsterisks(String s)
+	{
+		int answer = 0;
+		int count = 0;
+		for (char ch : s.toCharArray())
+		{
+			if (ch == '|')
+			{
+				count++;
+				count %= 2;
+			}
+			else if (ch == '*' && count == 0)
+			{
+				answer++;
+			}
+		}
+
+
+		return answer;
+	}
+}
