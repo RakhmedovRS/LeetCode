aab2df2c50d37c2664996d3ded429bb3282f5021
rakhmedovrs@gmail.com <rakhmedovrs@gmail.com>
2022-06-29 10:26:12 -0700

    Predict the Winner updated

--- src/main/java/tasks/PredictTheWinner.java
+++ src/main/java/tasks/PredictTheWinner.java
@@ -1,5 +1,6 @@
 package tasks;
 
+import common.Difficulty;
 import common.LeetCode;
 
 import java.util.Arrays;
@@ -8,7 +9,12 @@ import java.util.Arrays;
  * @author RakhmedovRS
  * @created 18-Aug-20
  */
-@LeetCode(id = 486, name = "Predict the Winner", url = "https://leetcode.com/problems/predict-the-winner/")
+@LeetCode(
+	id = 486,
+	name = "Predict the Winner",
+	url = "https://leetcode.com/problems/predict-the-winner/",
+	difficulty = Difficulty.MEDIUM
+)
 public class PredictTheWinner
 {
 	public boolean PredictTheWinner(int[] nums)
