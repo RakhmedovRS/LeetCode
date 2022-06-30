0ca06382152bc8fd4832f44c72b083ca61cf95b5
rakhmedovrs@gmail.com <rakhmedovrs@gmail.com>
2022-06-30 08:59:25 -0700

    Flood Fill updated

--- src/main/java/tasks/FloodFill.java
+++ src/main/java/tasks/FloodFill.java
@@ -1,5 +1,6 @@
 package tasks;
 
+import common.Difficulty;
 import common.LeetCode;
 
 import java.util.Arrays;
@@ -8,7 +9,12 @@ import java.util.Arrays;
  * @author RakhmedovRS
  * @created 11-May-20
  */
-@LeetCode(id = 733, name = "Flood Fill", url = "https://leetcode.com/problems/flood-fill/")
+@LeetCode(
+	id = 733,
+	name = "Flood Fill",
+	url = "https://leetcode.com/problems/flood-fill/",
+	difficulty = Difficulty.EASY
+)
 public class FloodFill
 {
 	public int[][] floodFill(int[][] image, int sr, int sc, int newColor)
