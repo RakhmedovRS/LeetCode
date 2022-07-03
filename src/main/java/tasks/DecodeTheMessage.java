aec8b5aa114eaa03908df21a6f9734ba098adccb
rakhmedovrs@gmail.com <rakhmedovrs@gmail.com>
2022-07-03 12:11:55 -0700

    Decode the Message

---
+++ src/main/java/tasks/DecodeTheMessage.java
@@ -0,0 +1,46 @@
+package tasks;
+
+import common.Difficulty;
+import common.LeetCode;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author RakhmedovRS
+ * @created 7/3/2022
+ */
+@LeetCode(
+	id = 2325,
+	name = "Decode the Message",
+	url = "https://leetcode.com/problems/decode-the-message/",
+	difficulty = Difficulty.EASY
+)
+public class DecodeTheMessage
+{
+	public String decodeMessage(String key, String message)
+	{
+		Map<Character, Character> map = new HashMap<>();
+		char ch = 'a';
+		for (char c : key.toCharArray())
+		{
+			if (map.containsKey(c) || c == ' ')
+			{
+				continue;
+			}
+
+			map.put(c, ch++);
+		}
+
+		map.put(' ', ' ');
+
+		StringBuilder sb = new StringBuilder();
+
+		for (char c : message.toCharArray())
+		{
+			sb.append(map.get(c));
+		}
+
+		return sb.toString();
+	}
+}
