6d92e44002177ba719870dd2f1c5c377ebb60e5d
rakhmedovrs@gmail.com <rakhmedovrs@gmail.com>
2022-07-17 09:14:03 -0700

    Maximum Number of Pairs in Array

---
+++ src/main/java/tasks/MaximumNumberOfPairsInArray.java
@@ -0,0 +1,36 @@
+package tasks;
+
+import common.Difficulty;
+import common.LeetCode;
+
+/**
+ * @author RakhmedovRS
+ * @created 7/17/2022
+ */
+@LeetCode(
+	id = 2341,
+	name = "Maximum Number of Pairs in Array",
+	url = "https://leetcode.com/problems/maximum-number-of-pairs-in-array/",
+	difficulty = Difficulty.EASY
+)
+public class MaximumNumberOfPairsInArray
+{
+	public int[] numberOfPairs(int[] nums)
+	{
+		int[] memo = new int[101];
+		for (int num : nums)
+		{
+			memo[num]++;
+		}
+
+		int[] answer = new int[2];
+
+		for (int count : memo)
+		{
+			answer[0] += count / 2;
+			answer[1] += count % 2;
+		}
+
+		return answer;
+	}
+}
