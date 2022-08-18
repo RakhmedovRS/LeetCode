8b7b069f9dff4ac0ee5a5908ee561c9a48ef662c
Ruslan Rakhmedov <rakhmedovrs@gmail.com>
2022-08-18 08:36:37 -0700

    Fix

--- src/main/java/tasks/CountSpecialIntegers.java
+++ src/main/java/tasks/CountSpecialIntegers.java
@@ -19,7 +19,7 @@ public class CountSpecialIntegers
 {
     public int countSpecialNumbers(int n)
     {
-        String val  = "" + n;
+        String val = "" + n;
         int len = val.length() - 1;
         int count;
         if (len == 0)
@@ -41,32 +41,73 @@ public class CountSpecialIntegers
             }
         }
 
-        int number = 0;
-        for (int i = 0; i < len; i++)
+        boolean[] used = new boolean[10];
+        char[] chars = val.toCharArray();
+        count += (chars[0] - '0' - 1) * fact(9) / fact(9 - chars.length + 1);
+        used[chars[0] - '0'] = true;
+        for (int i = 1; i < chars.length; i++)
         {
-            number *= 10;
-            number += 9;
+            int c = 0;
+            for (int j = 0; j < (chars[i] - '0'); j++)
+            {
+                if (!used[j])
+                {
+                    c++;
+                }
+            }
+            count += c * fact(9 - i) / fact(9 - chars.length + 1);
+            if (used[chars[i] - '0'])
+            {
+                break;
+            }
+            used[chars[i] - '0'] = true;
         }
 
-        outer: while (number++ < n)
+        if (unique(n))
         {
-            int used = 1 << 11;
-            int temp = number;
-            while (temp > 0)
-            {
-                int digit = temp % 10;
-                temp /= 10;
-                if ((used & (1 << digit)) > 0)
-                {
-                    continue outer;
-                }
+            count += 1;
+        }
+        return count;
+    }
 
-                used ^= (1 << digit);
+    int fact(int n)
+    {
+        int result = 1;
+        if (n > 9)
+        {
+            result = -1;
+        }
+        else if (n < 2)
+        {
+            result = 1;
+        }
+        else
+        {
+            for (int i = 2; i <= n; i++)
+            {
+                result *= i;
+            }
+        }
+
+        return result;
+    }
+
+    boolean unique(int number)
+    {
+        boolean[] used = new boolean[10];
+        int temp = number;
+        while (temp > 0)
+        {
+            int digit = temp % 10;
+            temp /= 10;
+            if (used[digit])
+            {
+                return false;
             }
 
-            count++;
+            used[digit] = true;
         }
 
-        return count;
+        return true;
     }
 }
