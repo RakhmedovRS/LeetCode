ac7383cd236516aaf7276609b8e658a0677b666f
Ruslan Rakhmedov <rakhmedovrs@gmail.com>
2022-08-22 08:50:17 -0700

    Time Needed to Rearrange a Binary String

---
+++ src/main/java/tasks/TimeNeededToRearrangeBinaryString.java
@@ -0,0 +1,47 @@
+package tasks;
+
+import common.Difficulty;
+import common.LeetCode;
+
+/**
+ * @author Ruslan Rakhmedov
+ * @created 8/22/2022
+ * @Ruslan Rakhmedov$
+ * @1.0$
+ */
+@LeetCode(
+        id = 2380,
+        name = "Time Needed to Rearrange a Binary String",
+        url = "https://leetcode.com/problems/time-needed-to-rearrange-a-binary-string/",
+        difficulty = Difficulty.MEDIUM
+)
+public class TimeNeededToRearrangeBinaryString
+{
+    public int secondsToRemoveOccurrences(String s)
+    {
+        return secondsToRemoveOccurrences(s.toCharArray());
+    }
+
+    public int secondsToRemoveOccurrences(char[] chars)
+    {
+        boolean seen = false;
+
+        for (int i = chars.length - 1; i > 0; i--)
+        {
+            if (chars[i - 1] == '0' && chars[i] == '1')
+            {
+                seen = true;
+                chars[i - 1] = '1';
+                chars[i] = '0';
+                i--;
+            }
+        }
+
+        if (seen)
+        {
+            return 1 + secondsToRemoveOccurrences(chars);
+        }
+
+        return 0;
+    }
+}
