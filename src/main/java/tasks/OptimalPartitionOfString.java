574e54018f1816fdd0a1def7e24d1ac0cf33a182
Ruslan Rakhmedov <rakhmedovrs@gmail.com>
2022-09-12 14:16:41 -0700

    Optimal Partition of String

---
+++ src/main/java/tasks/OptimalPartitionOfString.java
@@ -0,0 +1,38 @@
+package tasks;
+
+import common.Difficulty;
+import common.LeetCode;
+
+/**
+ * @author Ruslan Rakhmedov
+ * @created 9/12/2022
+ * @$
+ * @$
+ */
+@LeetCode(
+        id = 2405,
+        name = "Optimal Partition of String",
+        url = "https://leetcode.com/problems/optimal-partition-of-string/",
+        difficulty = Difficulty.MEDIUM
+)
+public class OptimalPartitionOfString
+{
+    public int partitionString(String s)
+    {
+        int count = 1;
+        boolean[] seen = new boolean[26];
+        for (int i = 0; i < s.length(); i++)
+        {
+            char ch = s.charAt(i);
+            if (seen[ch - 'a'])
+            {
+                count++;
+                seen = new boolean[26];
+            }
+
+            seen[ch - 'a'] = true;
+        }
+
+        return count;
+    }
+}
