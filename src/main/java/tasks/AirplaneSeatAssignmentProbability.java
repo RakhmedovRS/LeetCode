9cf6ab655981600f2522f19bf15d700231f8d39c
Ruslan Rakhmedov <rakhmedovrs@gmail.com>
2022-10-08 14:39:53 -0700

    Airplane Seat Assignment Probabilit updated

--- src/main/java/tasks/AirplaneSeatAssignmentProbability.java
+++ src/main/java/tasks/AirplaneSeatAssignmentProbability.java
@@ -1,12 +1,18 @@
 package tasks;
 
+import common.Difficulty;
 import common.LeetCode;
 
 /**
  * @author RakhmedovRS
  * @created 17-Sep-20
  */
-@LeetCode(id = 1227, name = "Airplane Seat Assignment Probability", url = "https://leetcode.com/problems/airplane-seat-assignment-probability/")
+@LeetCode(
+		id = 1227,
+		name = "Airplane Seat Assignment Probability",
+		url = "https://leetcode.com/problems/airplane-seat-assignment-probability/",
+		difficulty = Difficulty.MEDIUM
+)
 public class AirplaneSeatAssignmentProbability
 {
 	public double nthPersonGetsNthSeat(int n)
