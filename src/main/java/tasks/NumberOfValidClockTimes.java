b50d2c69e5a2f6ddd5a0f45bb53fba44671f6bde
Ruslan Rakhmedov <rakhmedovrs@gmail.com>
2022-10-15 09:16:14 -0700

    Number of Valid Clock Times

---
+++ src/main/java/tasks/NumberOfValidClockTimes.java
@@ -0,0 +1,43 @@
+package tasks;
+
+import common.Difficulty;
+import common.LeetCode;
+
+/**
+ * @author Ruslan Rakhmedov
+ * @created 10/15/2022
+ * @$
+ * @$
+ */
+@LeetCode(
+        id = 2437,
+        name = "Number of Valid Clock Times",
+        url = "https://leetcode.com/problems/number-of-valid-clock-times/",
+        difficulty = Difficulty.EASY
+)
+public class NumberOfValidClockTimes
+{
+    public int countTime(String time)
+    {
+        int count = 0;
+        outer:
+        for (int i = 0; i < 1440; i++)
+        {
+            int hh = i / 60;
+            int mm = i % 60;
+            String t = "" + (hh < 10 ? "0" + hh : hh) + ":" + (mm < 10 ? "0" + mm : mm);
+
+            for (int j = 0; j < time.length(); j++)
+            {
+                if (time.charAt(j) != '?' && time.charAt(j) != t.charAt(j))
+                {
+                    continue outer;
+                }
+            }
+
+            count++;
+        }
+
+        return count;
+    }
+}
