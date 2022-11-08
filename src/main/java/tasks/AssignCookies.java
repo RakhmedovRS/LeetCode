373f297fea8a02ccc00f28843355572bc42cb066
Ruslan Rakhmedov <rakhmedovrs@gmail.com>
2022-11-08 08:21:00 -0800

    Assign Cookies updated

--- src/main/java/tasks/AssignCookies.java
+++ src/main/java/tasks/AssignCookies.java
@@ -1,5 +1,6 @@
 package tasks;
 
+import common.Difficulty;
 import common.LeetCode;
 
 import java.util.Arrays;
@@ -8,7 +9,12 @@ import java.util.Arrays;
  * @author RakhmedovRS
  * @created 10/1/2020
  */
-@LeetCode(id = 455, name = "Assign Cookies", url = "https://leetcode.com/problems/assign-cookies/")
+@LeetCode(
+		id = 455,
+		name = "Assign Cookies",
+		url = "https://leetcode.com/problems/assign-cookies/",
+		difficulty = Difficulty.EASY
+)
 public class AssignCookies
 {
 	public int findContentChildren(int[] greed, int[] size)
