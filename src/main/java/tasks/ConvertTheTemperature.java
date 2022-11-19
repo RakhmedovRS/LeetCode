e71e0e8d5467b4e8368764e75855bc33343047bf
Ruslan Rakhmedov <rakhmedovrs@gmail.com>
2022-11-19 08:34:48 -0800

    Convert the Temperature

---
+++ src/main/java/tasks/ConvertTheTemperature.java
@@ -0,0 +1,23 @@
+package tasks;
+
+import common.Difficulty;
+import common.LeetCode;
+
+/**
+ * @author Ruslan Rakhmedov
+ * @created 11/19/2022
+ * @$
+ * @$
+ */
+@LeetCode(
+        id = 2469,
+        name = "Convert the Temperature",
+        url = "https://leetcode.com/problems/convert-the-temperature/",
+        difficulty = Difficulty.EASY
+)
+public class ConvertTheTemperature
+{
+    public double[] convertTemperature(double celsius) {
+        return new double[]{celsius + 273.15D, celsius * 1.8D + 32};
+    }
+}
