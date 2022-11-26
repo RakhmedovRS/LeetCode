798730c33f3c58e3732986ecdcd0bf4b2ab3eda0
Ruslan Rakhmedov <rakhmedovrs@gmail.com>
2022-11-26 08:04:43 -0800

    Minimum Cuts to Divide a Circle

---
+++ src/main/java/tasks/MinimumCutsЕoDivideCircle.java
@@ -0,0 +1,34 @@
+package tasks;
+
+import common.Difficulty;
+import common.LeetCode;
+
+/**
+ * @author Ruslan Rakhmedov
+ * @created 11/26/2022
+ * @$
+ * @$
+ */
+@LeetCode(
+        id = 2481,
+        name = "Minimum Cuts to Divide a Circle",
+        url = "https://leetcode.com/problems/minimum-cuts-to-divide-a-circle/",
+        difficulty = Difficulty.EASY
+)
+public class MinimumCutsЕoDivideCircle
+{
+    public int numberOfCuts(int n)
+    {
+        if (n <= 1)
+        {
+            return 0;
+        }
+
+        if (n % 2 == 0)
+        {
+            return n / 2;
+        }
+
+        return n;
+    }
+}
