61a98ac87cd1b60cede731674b8b60dc2298a1a4
Ruslan Rakhmedov <rakhmedovrs@gmail.com>
2022-12-26 09:08:46 -0800

    Shortest Distance to Target String in a Circular Array

---
+++ src/main/java/tasks/ShortestDistanceToTargetStringInCircularArray.java
@@ -0,0 +1,46 @@
+package tasks;
+
+import common.Difficulty;
+import common.LeetCode;
+
+/**
+ * @author Ruslan Rakhmedov
+ * @created 12/26/2022
+ * @$
+ * @$
+ */
+@LeetCode(
+        id = 2515,
+        name = "Shortest Distance to Target String in a Circular Array",
+        url = "https://leetcode.com/problems/shortest-distance-to-target-string-in-a-circular-array/",
+        difficulty = Difficulty.EASY
+)
+public class ShortestDistanceToTargetStringInCircularArray
+{
+    public int closetTarget(String[] words, String target, int startIndex)
+    {
+        int pos = Integer.MAX_VALUE;
+        for (int i = startIndex, step = 0; i < words.length * 2; i++, step++)
+        {
+            if (words[i % words.length].equals(target))
+            {
+                pos = Math.min(pos, step);
+            }
+        }
+
+        for (int i = startIndex, step = 0; step < words.length; step++, i--)
+        {
+            if (i < 0)
+            {
+                i = words.length - 1;
+            }
+
+            if (words[i].equals(target))
+            {
+                pos = Math.min(pos, step);
+            }
+        }
+
+        return pos == Integer.MAX_VALUE ? -1 : pos;
+    }
+}
