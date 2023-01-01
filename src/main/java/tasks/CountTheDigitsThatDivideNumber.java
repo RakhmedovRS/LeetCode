bb455ea2c4e3bc4323e9cc8a771cfcf3a368e369
Ruslan Rakhmedov <rakhmedovrs@gmail.com>
2023-01-01 09:26:15 -0800

    Count the Digits That Divide a Number

---
+++ src/main/java/tasks/CountTheDigitsThatDivideNumber.java
@@ -0,0 +1,32 @@
+package tasks;
+
+import common.Difficulty;
+import common.LeetCode;
+
+/**
+ * @author Ruslan Rakhmedov
+ * @created 1/1/2023
+ * @$
+ * @$
+ */
+@LeetCode(
+        id = 2520,
+        name = "Count the Digits That Divide a Number",
+        url = "https://leetcode.com/problems/count-the-digits-that-divide-a-number/",
+        difficulty = Difficulty.EASY
+)
+public class CountTheDigitsThatDivideNumber
+{
+    public int countDigits(int num)
+    {
+        int cnt = 0;
+        for (char ch: (""+num).toCharArray())
+        {
+            if (num % (ch - '0') == 0)
+            {
+                cnt++;
+            }
+        }
+        return cnt;
+    }
+}
