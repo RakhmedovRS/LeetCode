35e8764d005c831e8f7884ebaeb762edc543bdf3
Ruslan Rakhmedov <rakhmedovrs@gmail.com>
2023-01-14 11:23:38 -0800

    Contiguous Array. closes #75

--- src/main/java/tasks/ContiguousArray.java
+++ src/main/java/tasks/ContiguousArray.java
@@ -1,5 +1,6 @@
 package tasks;
 
+import common.Difficulty;
 import common.LeetCode;
 
 import java.util.HashMap;
@@ -9,7 +10,12 @@ import java.util.Map;
  * @author RakhmedovRS
  * @created 13-Apr-20
  */
-@LeetCode(id = 525, name = "Contiguous Array", url = "https://leetcode.com/problems/contiguous-array/")
+@LeetCode(
+		id = 525,
+		name = "Contiguous Array",
+		url = "https://leetcode.com/problems/contiguous-array/",
+		difficulty = Difficulty.MEDIUM
+)
 public class ContiguousArray
 {
 	public int findMaxLength(int[] nums)
