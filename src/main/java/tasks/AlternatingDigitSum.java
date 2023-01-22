d5ea5f38f170e0ab4e5552c405362f95e4ffe23c
Ruslan Rakhmedov <rakhmedovrs@gmail.com>
2023-01-22 09:38:06 -0800

    Alternating Digit Sum

---
+++ src/main/java/tasks/AlternatingDigitSum.java
@@ -0,0 +1,40 @@
+package tasks;
+
+import common.Difficulty;
+import common.LeetCode;
+
+/**
+ * @author Ruslan Rakhmedov
+ * @created 1/22/2023
+ * @$
+ * @$
+ */
+@LeetCode(
+        id = 2544,
+        name = "Alternating Digit Sum",
+        url = "https://leetcode.com/problems/alternating-digit-sum/",
+        difficulty = Difficulty.EASY
+)
+public class AlternatingDigitSum
+{
+    public int alternateDigitSum(int n) {
+        int sum = 0;
+        boolean pos = true;
+        String s = "" + n;
+        for (char ch: s.toCharArray())
+        {
+            if (pos)
+            {
+                sum += ch - '0';
+            }
+            else
+            {
+                sum -= ch - '0';
+            }
+
+            pos = !pos;
+        }
+
+        return sum;
+    }
+}
