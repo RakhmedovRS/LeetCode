fcb7fd1c55e1b3db5fe1320386b218baae3395b3
Ruslan Rakhmedov <rakhmedovrs@gmail.com>
2023-02-12 08:33:12 -0800

    Find the Array Concatenation Value

---
+++ src/main/java/tasks/FindTheArrayConcatenationValue.java
@@ -0,0 +1,37 @@
+package tasks;
+
+import common.Difficulty;
+import common.LeetCode;
+
+/**
+ * @author Ruslan Rakhmedov
+ * @created 2/12/2023
+ * @$
+ * @$
+ */
+@LeetCode(
+        id = 2562,
+        name = "Find the Array Concatenation Value",
+        url = "https://leetcode.com/problems/find-the-array-concatenation-value/description/",
+        difficulty = Difficulty.EASY
+)
+public class FindTheArrayConcatenationValue
+{
+    public long findTheArrayConcVal(int[] nums)
+    {
+        int left = 0;
+        int right = nums.length - 1;
+        long res = 0;
+        while (left <= right)
+        {
+            if (left == right)
+            {
+                res += nums[left++];
+                continue;
+            }
+            res += (Long.parseLong("" + nums[left++] + nums[right--]));
+        }
+
+        return res;
+    }
+}
