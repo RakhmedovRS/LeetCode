ba518e32cd84918f4112d21c3baa656ba7490e6a
Ruslan Rakhmedov <rakhmedovrs@gmail.com>
2023-03-04 08:33:35 -0800

    Split With Minimum Sum

---
+++ src/main/java/tasks/SplitWithMinimumSum.java
@@ -0,0 +1,47 @@
+package tasks;
+
+import common.Difficulty;
+import common.LeetCode;
+
+import java.util.LinkedList;
+
+/**
+ * @author Ruslan Rakhmedov
+ * @created 3/4/2023
+ * @$
+ * @$
+ */
+@LeetCode(
+        id = 2578,
+        name = "Split With Minimum Sum",
+        url = "https://leetcode.com/problems/split-with-minimum-sum/",
+        difficulty = Difficulty.EASY
+)
+public class SplitWithMinimumSum
+{
+    public int splitNum(int num) {
+        LinkedList<Integer> list = new LinkedList<>();
+        while (num > 0)
+        {
+            list.add(num % 10);
+            num /= 10;
+        }
+
+        list.sort(null);
+
+        int a = 0;
+        int b = 0;
+        while (!list.isEmpty())
+        {
+            a *= 10;
+            a += list.removeFirst();
+            if (!list.isEmpty())
+            {
+                b *= 10;
+                b += list.removeFirst();
+            }
+        }
+
+        return a + b;
+    }
+}
