f9a4b23d5eae4d9f9351f1a020a8e86067791e24
Ruslan Rakhmedov <rakhmedovrs@gmail.com>
2023-03-30 18:17:07 -0700

    Added helper for Graph problems

---
+++ src/main/java/contest/GraphHelper.java
@@ -0,0 +1,28 @@
+package contest;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * @author Ruslan Rakhmedov
+ * @created 3/30/2023
+ */
+public final class GraphHelper
+{
+    public Map<Integer, List<Integer>> buildUndirectedGraph(int[][] edges)
+    {
+        Map<Integer, List<Integer>> graph = new HashMap<>();
+        for (int[] edge : edges)
+        {
+            graph.putIfAbsent(edge[0], new ArrayList<>());
+            graph.putIfAbsent(edge[1], new ArrayList<>());
+
+            graph.get(edge[0]).add(edge[1]);
+            graph.get(edge[1]).add(edge[0]);
+        }
+
+        return graph;
+    }
+}
