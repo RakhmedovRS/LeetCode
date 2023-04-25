66655de083fd2e65ee679b2f7778b23ec2a88800
Ruslan Rakhmedov <rakhmedovrs@gmail.com>
2023-04-24 19:31:35 -0700

    Calculate Delayed Arrival Time

---
+++ src/main/java/tasks/CalculateDelayedArrivalTime.java
@@ -0,0 +1,18 @@
+package tasks;
+
+import common.Difficulty;
+import common.LeetCode;
+
+@LeetCode(
+        id = 2651,
+        name = "Calculate Delayed Arrival Time",
+        url = "https://leetcode.com/problems/calculate-delayed-arrival-time/",
+        difficulty = Difficulty.EASY
+)
+public class CalculateDelayedArrivalTime
+{
+    public int findDelayedArrivalTime(int arrivalTime, int delayedTime)
+    {
+        return (arrivalTime + delayedTime) % 24;
+    }
+}
