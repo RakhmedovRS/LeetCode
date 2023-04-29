8d50a6f9b244db25de3e95bf621f4f34b83cd55e
Ruslan Rakhmedov <rakhmedovrs@gmail.com>
2023-04-29 09:45:23 -0700

    Maximum Sum With Exactly K Elements

---
+++ src/main/java/tasks/MaximumSumWithExactlyKElements.java
@@ -0,0 +1,35 @@
+package tasks;
+
+import common.Difficulty;
+import common.LeetCode;
+
+/**
+ * @author Ruslan Rakhmedov
+ * @created 4/29/2023
+ */
+@LeetCode(
+        id = 2656,
+        name = "Maximum Sum With Exactly K Elements",
+        url = "https://leetcode.com/problems/maximum-sum-with-exactly-k-elements/description/",
+        difficulty = Difficulty.EASY
+)
+public class MaximumSumWithExactlyKElements
+{
+    public int maximizeSum(int[] nums, int k)
+    {
+        int max = 0;
+        for (int n: nums)
+        {
+            max = Math.max(max, n);
+        }
+
+        int sum = 0;
+        while (k-- > 0)
+        {
+            sum += max;
+            max++;
+        }
+
+        return sum;
+    }
+}
