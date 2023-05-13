ba2631f98ce9154ff3ec5562676b3ba5ef5ee070
Ruslan Rakhmedov <rakhmedovrs@gmail.com>
2023-05-13 10:35:59 -0700

    Sum in a Matrix

---
+++ src/main/java/tasks/SumInMatrix.java
@@ -0,0 +1,51 @@
+package tasks;
+
+import common.Difficulty;
+import common.LeetCode;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+
+/**
+ * @author Ruslan Rakhmedov
+ * @created 5/13/2023
+ */
+@LeetCode(
+        id = 2679,
+        name = "Sum in a Matrix",
+        url = "https://leetcode.com/problems/sum-in-a-matrix/",
+        difficulty = Difficulty.MEDIUM
+)
+public class SumInMatrix
+{
+    public int matrixSum(int[][] nums)
+    {
+        List<List<Integer>> lists = new ArrayList<>();
+        int rows = nums.length;
+        int columns = nums[0].length;
+        for (int row = 0; row < rows; row++)
+        {
+            List<Integer> list = new ArrayList<>();
+            for (int val : nums[row])
+            {
+                list.add(val);
+            }
+            list.sort(Comparator.reverseOrder());
+            lists.add(list);
+        }
+
+        int s = 0;
+        for (int column = 0; column < columns; column++)
+        {
+            int max = 0;
+            for (int row = 0; row < rows; row++)
+            {
+                max = Math.max(max, lists.get(row).get(column));
+            }
+            s += max;
+        }
+
+        return s;
+    }
+}
