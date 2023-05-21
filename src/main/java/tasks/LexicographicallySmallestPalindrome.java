6bd1acfc26189ec7516a8d9a060fc8b95711dbb7
Ruslan Rakhmedov <rakhmedovrs@gmail.com>
2023-05-20 21:50:33 -0700

    Lexicographically Smallest Palindrome

---
+++ src/main/java/tasks/LexicographicallySmallestPalindrome.java
@@ -0,0 +1,38 @@
+package tasks;
+
+import common.Difficulty;
+import common.LeetCode;
+
+/**
+ * @author Ruslan Rakhmedov
+ * @created 5/20/2023
+ */
+@LeetCode(
+        id = 2697,
+        name = "Lexicographically Smallest Palindrome",
+        url = "https://leetcode.com/problems/lexicographically-smallest-palindrome/",
+        difficulty = Difficulty.EASY
+)
+public class LexicographicallySmallestPalindrome
+{
+    public String makeSmallestPalindrome(String s)
+    {
+        char[] chars = s.toCharArray();
+        for (int left = 0, right = chars.length - 1; left < right; left++, right--)
+        {
+            if (chars[left] != chars[right])
+            {
+                if (chars[left] <= chars[right])
+                {
+                    chars[right] = chars[left];
+                }
+                else
+                {
+                    chars[left] = chars[right];
+                }
+            }
+        }
+
+        return new String(chars);
+    }
+}
