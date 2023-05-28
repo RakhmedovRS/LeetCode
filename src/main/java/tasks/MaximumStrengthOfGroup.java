854db0ffbdd1165c7bb06ee77798e7db25b67099
Ruslan Rakhmedov <rakhmedovrs@gmail.com>
2023-05-27 21:29:10 -0600

    Maximum Strength of a Group

---
+++ src/main/java/tasks/MaximumStrengthOfGroup.java
@@ -0,0 +1,62 @@
+package tasks;
+
+import common.Difficulty;
+import common.LeetCode;
+
+import java.util.Arrays;
+
+@LeetCode(
+        id = 2708,
+        name = "Maximum Strength of a Group",
+        url = "https://leetcode.com/problems/maximum-strength-of-a-group/description/",
+        difficulty = Difficulty.MEDIUM
+)
+public class MaximumStrengthOfGroup
+{
+    public long maxStrength(int[] nums)
+    {
+        if (nums.length == 1)
+        {
+            return nums[0];
+        }
+        Arrays.sort(nums);
+        int neg = 0;
+        Long res = null;
+        while (neg < nums.length && nums[neg] < 0)
+        {
+            if (neg + 1 < nums.length && nums[neg + 1] < 0)
+            {
+                if (res == null)
+                {
+                    res = 1L;
+                }
+                res *= nums[neg];
+                res *= nums[neg + 1];
+                neg += 2;
+            }
+            else
+            {
+                break;
+            }
+        }
+
+        for (int num : nums)
+        {
+            if (num > 0)
+            {
+                if (res == null)
+                {
+                    res = 1L;
+                }
+                res *= num;
+            }
+        }
+
+        if (res == null)
+        {
+            return 0;
+        }
+
+        return res;
+    }
+}
