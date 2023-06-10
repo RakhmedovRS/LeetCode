766e2826aa7e385c6cc55258ddffa185fadb4c5b
Ruslan Rakhmedov <rakhmedovrs@gmail.com>
2023-06-10 09:11:25 -0700

    Check if The Number is Fascinating

---
+++ src/main/java/tasks/CheckIfTheNumberIsFascinating.java
@@ -0,0 +1,65 @@
+package tasks;
+
+import common.Difficulty;
+import common.LeetCode;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * @author Ruslan Rakhmedov
+ * @created 6/10/2023
+ */
+@LeetCode(
+        id = 2729,
+        name = "Check if The Number is Fascinating",
+        url = "https://leetcode.com/problems/check-if-the-number-is-fascinating/",
+        difficulty = Difficulty.EASY
+)
+public class CheckIfTheNumberIsFascinating
+{
+    public boolean isFascinating(int n)
+    {
+        Set<Character> set = new HashSet<>();
+        for (char ch : (n + "").toCharArray())
+        {
+            if (ch == '0')
+            {
+                return false;
+            }
+
+            if (!set.add(ch))
+            {
+                return false;
+            }
+        }
+
+        for (char ch : ((n * 2) + "").toCharArray())
+        {
+            if (ch == '0')
+            {
+                return false;
+            }
+
+            if (!set.add(ch))
+            {
+                return false;
+            }
+        }
+
+        for (char ch : ((n * 3) + "").toCharArray())
+        {
+            if (ch == '0')
+            {
+                return false;
+            }
+
+            if (!set.add(ch))
+            {
+                return false;
+            }
+        }
+
+        return set.size() == 9;
+    }
+}
