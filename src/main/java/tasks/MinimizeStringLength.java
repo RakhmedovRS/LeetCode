948126f8be25e680f43669f82bb82ac47ef92da3
Ruslan Rakhmedov <rakhmedovrs@gmail.com>
2023-06-10 09:19:09 -0700

    Minimize String Length

---
+++ src/main/java/tasks/MinimizeStringLength.java
@@ -0,0 +1,31 @@
+package tasks;
+
+import common.Difficulty;
+import common.LeetCode;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * @author Ruslan Rakhmedov
+ * @created 6/10/2023
+ */
+@LeetCode(
+        id = 2716,
+        name = "Minimize String Length",
+        url = "https://leetcode.com/problems/minimize-string-length/description/",
+        difficulty = Difficulty.EASY
+)
+public class MinimizeStringLength
+{
+    public int minimizedStringLength(String s)
+    {
+        Set<Character> characters = new HashSet<>();
+        for (char ch: s.toCharArray())
+        {
+            characters.add(ch);
+        }
+
+        return characters.size();
+    }
+}
