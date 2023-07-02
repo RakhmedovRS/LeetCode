6e41b5c49236dea074ea0166a57ed40af33c8888
Ruslan Rakhmedov <rakhmedovrs@gmail.com>
2023-07-02 11:40:18 -0700

    Longest Even Odd Subarray With Threshold

---
+++ src/main/java/tasks/LongestEvenOddSubarrayWithThreshold.java
@@ -0,0 +1,52 @@
+package tasks;
+
+import common.Difficulty;
+import common.LeetCode;
+
+/**
+ * @author Ruslan Rakhmedov
+ * @created 7/2/2023
+ */
+@LeetCode(
+        id = 2760,
+        name = "Longest Even Odd Subarray With Threshold",
+        url = "https://leetcode.com/problems/longest-even-odd-subarray-with-threshold/",
+        difficulty = Difficulty.EASY
+)
+public class LongestEvenOddSubarrayWithThreshold
+{
+    public int longestAlternatingSubarray(int[] nums, int threshold)
+    {
+        int max = 0;
+        for (int num: nums)
+        {
+            if (num <= threshold && num % 2 == 0)
+            {
+                max = 1;
+                break;
+            }
+        }
+
+
+        outer:
+        for (int l = 0; l < nums.length; l++)
+        {
+            if (nums[l] % 2 != 0 || nums[l] > threshold)
+            {
+                continue;
+            }
+
+            for (int r = l + 1; r < nums.length; r++)
+            {
+                if (nums[r] > threshold || nums[r - 1] > threshold || nums[r - 1] % 2 == nums[r] % 2)
+                {
+                    continue outer;
+                }
+
+                max = Math.max(max, 1 + r - l);
+            }
+        }
+
+        return max;
+    }
+}
