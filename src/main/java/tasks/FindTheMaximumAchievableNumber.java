a1bc7316a8ce5acc9c643b5e00a30c6a5bbdeb70
Ruslan Rakhmedov <rakhmedovrs@gmail.com>
2023-07-09 13:45:43 -0700

    Find the Maximum Achievable Number

---
+++ src/main/java/tasks/FindTheMaximumAchievableNumber.java
@@ -0,0 +1,22 @@
+package tasks;
+
+import common.Difficulty;
+import common.LeetCode;
+
+/**
+ * @author Ruslan Rakhmedov
+ * @created 7/9/2023
+ */
+@LeetCode(
+        id = 2769,
+        name = "Find the Maximum Achievable Number",
+        url = "https://leetcode.com/problems/find-the-maximum-achievable-number/",
+        difficulty = Difficulty.EASY
+)
+public class FindTheMaximumAchievableNumber
+{
+    public int theMaximumAchievableX(int num, int t)
+    {
+        return num + t * 2;
+    }
+}
