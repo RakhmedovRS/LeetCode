83b78546a52afa39d126d3e0c19dfde91cd55cd8
Ruslan Rakhmedov <rakhmedovrs@gmail.com>
2023-07-22 21:03:49 -0700

    Largest Element in an Array after Merge Operations

---
+++ src/main/java/tasks/LargestElementInArrayAfterMergeOperations.java
@@ -0,0 +1,35 @@
+package tasks;
+
+import common.Difficulty;
+import common.LeetCode;
+
+/**
+ * @author Ruslan Rakhmedov
+ * @created 7/22/2023
+ */
+@LeetCode(
+		id = 2789,
+		name = "Largest Element in an Array after Merge Operations",
+		url = "https://leetcode.com/problems/largest-element-in-an-array-after-merge-operations/",
+		difficulty = Difficulty.MEDIUM
+)
+public class LargestElementInArrayAfterMergeOperations
+{
+	public long maxArrayValue(int[] nums)
+	{
+		long max = 0;
+		for (int i = nums.length - 1; i >= 0; i--)
+		{
+			if (nums[i] <= max)
+			{
+				max += nums[i];
+			}
+			else
+			{
+				max = nums[i];
+			}
+		}
+
+		return max;
+	}
+}
