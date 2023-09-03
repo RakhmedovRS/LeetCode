158f98e0c120622291c825d565a2926200df7b23
Ruslan Rakhmedov <rakhmedovrs@gmail.com>
2023-09-02 18:04:45 -0700

    Check if Strings Can be Made Equal With Operations I

---
+++ src/main/java/tasks/CheckIfStringsCanBeMadeEqualWithOperationsI.java
@@ -0,0 +1,44 @@
+package tasks;
+
+import common.Difficulty;
+import common.LeetCode;
+
+/**
+ * @author Ruslan Rakhmedov
+ * @created 9/2/2023
+ */
+@LeetCode(
+		id = 2839,
+		name = "Check if Strings Can be Made Equal With Operations I",
+		url = "https://leetcode.com/problems/check-if-strings-can-be-made-equal-with-operations-i/",
+		difficulty = Difficulty.EASY
+)
+public class CheckIfStringsCanBeMadeEqualWithOperationsI
+{
+	public boolean canBeEqual(String s1, String s2) {
+		char[] a1 = s1.toCharArray();
+		char[] a2 = s2.toCharArray();
+
+		for (int i = 0; i < 2; i++)
+		{
+			if (a1[i] > a1[i + 2])
+			{
+				char temp = a1[i];
+				a1[i] = a1[i + 2];
+				a1[i + 2] = temp;
+			}
+		}
+
+		for (int i = 0; i < 2; i++)
+		{
+			if (a2[i] > a2[i + 2])
+			{
+				char temp = a2[i];
+				a2[i] = a2[i + 2];
+				a2[i + 2] = temp;
+			}
+		}
+
+		return String.valueOf(a1).equals(String.valueOf(a2));
+	}
+}
