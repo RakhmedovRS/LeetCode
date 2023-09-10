b05c1f934ac48995cfd521c1abff901fa545084f
Ruslan Rakhmedov <rakhmedovrs@gmail.com>
2023-09-09 22:05:24 -0700

    Determine if a Cell Is Reachable at a Given Time

---
+++ src/main/java/tasks/DetermineIfCellIsReachableAtGivenTime.java
@@ -0,0 +1,55 @@
+package tasks;
+
+import common.Difficulty;
+import common.LeetCode;
+
+/**
+ * @author Ruslan Rakhmedov
+ * @created 9/9/2023
+ */
+@LeetCode(
+		id = 2849,
+		name = "Determine if a Cell Is Reachable at a Given Time",
+		url = "https://leetcode.com/problems/determine-if-a-cell-is-reachable-at-a-given-time/",
+		difficulty = Difficulty.MEDIUM
+)
+public class DetermineIfCellIsReachableAtGivenTime
+{
+	public boolean isReachableAtTime(int sx, int sy, int fx, int fy, int t)
+	{
+		int dist = eucDistance(new int[]{sx, sy}, new int[]{fx, fy});
+		if (dist == 0)
+		{
+			return t == 0 || t > 1;
+		}
+
+		int xDiff = Math.abs(sx - fx);
+		int yDiff = Math.abs(sy - fy);
+
+		if (xDiff >= yDiff)
+		{
+			xDiff -= yDiff;
+			if (xDiff + yDiff > t)
+			{
+				return false;
+			}
+		}
+		else
+		{
+			yDiff -= xDiff;
+			if (xDiff + yDiff > t)
+			{
+				return false;
+			}
+		}
+
+		return true;
+	}
+
+	private int eucDistance(int[] a, int[] b)
+	{
+		double xX = a[0] - b[0];
+		double yY = a[1] - b[1];
+		return (int)Math.sqrt(xX * xX + yY * yY);
+	}
+}
