8e448e683e5b01d76a807cfb0dda35366deffe75
Ruslan Rakhmedov <rakhmedovrs@gmail.com>
2023-09-16 21:10:25 -0700

    Sum of Values at Indices With K Set Bits

---
+++ src/main/java/tasks/SumOfValuesAtIndicesWithKSetBits.java
@@ -0,0 +1,32 @@
+package tasks;
+
+import common.Difficulty;
+import common.LeetCode;
+
+import java.util.List;
+
+/**
+ * @author Ruslan Rakhmedov
+ * @created 9/16/2023
+ */
+@LeetCode(
+		id = 2859,
+		name = "Sum of Values at Indices With K Set Bits",
+		url = "https://leetcode.com/problems/sum-of-values-at-indices-with-k-set-bits/",
+		difficulty = Difficulty.EASY
+)
+public class SumOfValuesAtIndicesWithKSetBits
+{
+	public int sumIndicesWithKSetBits(List<Integer> nums, int k)
+	{
+		int sum = 0;
+		for (int i = 0; i < nums.size(); i++)
+		{
+			if (Integer.bitCount(i) == k)
+			{
+				sum += nums.get(i);
+			}
+		}
+		return sum;
+	}
+}
