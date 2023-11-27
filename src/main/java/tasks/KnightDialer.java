6819ef9017b1db464cb612d5dc7fde19468924e5
Ruslan Rakhmedov <rakhmedovrs@gmail.com>
2023-11-26 18:57:19 -0800

    Knight Dialer updated

--- src/main/java/tasks/KnightDialer.java
+++ src/main/java/tasks/KnightDialer.java
@@ -3,7 +3,7 @@ package tasks;
 import common.Difficulty;
 import common.LeetCode;
 
-import java.util.Arrays;
+import java.util.*;
 
 /**
  * @author RakhmedovRS
@@ -17,81 +17,80 @@ import java.util.Arrays;
 )
 public class KnightDialer
 {
-	public int knightDialer(int N)
+	int MOD = 1_000_000_007;
+
+	public int knightDialer(int n)
 	{
-		if (N == 0)
-		{
-			return 0;
-		}
+		int[][] grid = new int[][]{
+				{1, 2, 3},
+				{4, 5, 6},
+				{7, 8, 9},
+				{5, 0, 5},
+		};
 
-		int MOD = 1_000_000_007;
-		int[][] numPad =
-				{
-						{1, 2, 3},
-						{4, 5, 6},
-						{7, 8, 9},
-						{10, 0, 11}
-				};
-		Integer[][] memo = new Integer[N + 1][12];
-		for (int i = 0; i < 10; i++)
+		int[][] memo = new int[][]{
+				{1, 1, 1},
+				{1, 1, 1},
+				{1, 1, 1},
+				{0, 1, 0},
+		};
+
+		Map<Integer, List<int[]>> moves = new HashMap<>();
+		// 1 -> 6, 1 -> 8
+		moves.put(1, Arrays.asList(new int[]{1, 2}, new int[]{2, 1}));
+		// 2 -> 7, 2 -> 9
+		moves.put(2, Arrays.asList(new int[]{2, 0}, new int[]{2, 2}));
+		// 3 -> 4, 3 -> 8
+		moves.put(3, Arrays.asList(new int[]{1, 0}, new int[]{2, 1}));
+		// 4 -> 3, 4 -> 9, 4 -> 0
+		moves.put(4, Arrays.asList(new int[]{0, 2}, new int[]{2, 2}, new int[]{3, 1}));
+		// 5..
+		moves.put(5, new ArrayList<>());
+		// 6 -> 1, 6 -> 7, 6 -> 0
+		moves.put(6, Arrays.asList(new int[]{0, 0}, new int[]{2, 0}, new int[]{3, 1}));
+		// 7 -> 2, 7 -> 6
+		moves.put(7, Arrays.asList(new int[]{0, 1}, new int[]{1, 2}));
+		// 8 -> 1, 8 -> 3
+		moves.put(8, Arrays.asList(new int[]{0, 0}, new int[]{0, 2}));
+		// 9 -> 4, 9 -> 2
+		moves.put(9, Arrays.asList(new int[]{1, 0}, new int[]{0, 1}));
+		// 0 -> 4, 0 -> 6
+		moves.put(0, Arrays.asList(new int[]{1, 0}, new int[]{1, 2}));
+
+		int res = 0;
+		for (int i = 2; i <= n; i++)
 		{
-			memo[0][i] = 1;
-		}
-		int[][] steps = new int[][]{{-2, -1}, {-2, 1}, {-1, -2}, {-1, 2}, {1, -2}, {1, 2}, {2, -1}, {2, 1}};
-		int prevRow;
-		int prevColumn;
-		for (int n = 1; n <= N; n++)
-		{
-			Arrays.fill(memo[n], 0);
-			for (int row = 0; row < numPad.length; row++)
+			int[][] nextMemo = new int[4][3];
+			for (int row = 0; row < 4; row++)
 			{
-				for (int column = 0; column < numPad[0].length; column++)
+				for (int column = 0; column < 3; column++)
 				{
-					if (numPad[row][column] == 10 || numPad[row][column] == 11)
-					{
-						continue;
-					}
+					int currNum = grid[row][column];
 
-					for (int[] step : steps)
+					List<int[]> movesToNextNum = moves.get(currNum);
+
+					for (int[] moveToNextNum : movesToNextNum)
 					{
-						prevRow = row + step[0];
-						prevColumn = column + step[1];
-						if (isValidMove(prevRow, prevColumn, numPad))
-						{
-							memo[n][numPad[row][column]] += memo[n - 1][numPad[prevRow][prevColumn]] % MOD;
-							memo[n][numPad[row][column]] %= MOD;
-						}
+						int nextRow = moveToNextNum[0];
+						int nextColumn = moveToNextNum[1];
+
+						nextMemo[nextRow][nextColumn] = (nextMemo[nextRow][nextColumn] + memo[row][column]) % MOD;
 					}
 				}
 			}
+
+			memo = nextMemo;
 		}
 
-		long result = 0;
-		for (int row = 0; row < numPad.length; row++)
+		for (int row = 0; row < 4; row++)
 		{
-			for (int column = 0; column < numPad[0].length; column++)
+			for (int column = 0; column < 3; column++)
 			{
-				if (numPad[row][column] == 10 || numPad[row][column] == 11)
-				{
-					continue;
-				}
-
-				result += memo[N - 1][numPad[row][column]] % MOD;
-				result %= MOD;
+				res = (res + memo[row][column]) % MOD;
 			}
 		}
 
-		return (int) (result % MOD);
-	}
-
-	private boolean isValidMove(int row, int column, int[][] numPad)
-	{
-		return row >= 0
-				&& row < numPad.length
-				&& column >= 0
-				&& column < numPad[row].length
-				&& numPad[row][column] != 10
-				&& numPad[row][column] != 11;
+		return res;
 	}
 
 	public static void main(String[] args)
