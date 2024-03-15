e29e1ccfd7c697b02d1fd759525b470b69772e58
Ruslan Rakhmedov <rakhmedovrs@gmail.com>
2024-03-15 14:33:14 -0700

    LRUCache updated

--- src/main/java/tasks/LRUCache.java
+++ src/main/java/tasks/LRUCache.java
@@ -16,78 +16,95 @@ import java.util.Map;
 		url = "https://leetcode.com/problems/lru-cache/",
 		difficulty = Difficulty.MEDIUM
 )
-public class LRUCache {
-	private class Entry {
-		private Entry prev;
-		private Entry next;
-		private final int key;
-		private int value;
+class LRUCache {
 
-		public Entry(int key, int value) {
+	private static class CacheEntry {
+		private final int key;
+		private final int val;
+		private CacheEntry prev;
+		private CacheEntry next;
+
+		public CacheEntry() {
+			key = -1;
+			val = -1;
+		}
+
+		public CacheEntry(int key, int val) {
 			this.key = key;
-			this.value = value;
+			this.val = val;
 		}
 	}
 
-	private final Entry head;
-	private final Entry tail;
-	private final Map<Integer, Entry> map;
-	int size;
+	private final int capacity;
+	private final Map<Integer, CacheEntry> cacheEntryMap;
+	private final CacheEntry head;
+	private final CacheEntry tail;
 
 	public LRUCache(int capacity) {
-		head = new Entry(0, 0);
-		tail = new Entry(0, 0);
-		map = new HashMap<>();
-		size = capacity;
+		cacheEntryMap = new HashMap<>();
+		this.capacity = capacity;
+		head = new CacheEntry();
+		tail = new CacheEntry();
 		head.next = tail;
 		tail.prev = head;
 	}
 
 	public int get(int key) {
-		Entry entry = map.get(key);
-		if (entry == null) {
+		CacheEntry cacheEntry = cacheEntryMap.get(key);
+		if (cacheEntry == null) {
 			return -1;
 		}
 
-		entry.prev.next = entry.next;
-		entry.next.prev = entry.prev;
+		//remove item from the current position
+		CacheEntry cacheEntryPrev = cacheEntry.prev;
+		CacheEntry cacheEntryNext = cacheEntry.next;
+		cacheEntryPrev.next = cacheEntryNext;
+		cacheEntryNext.prev = cacheEntryPrev;
 
-		entry.next = head.next;
-		entry.prev = head;
-		head.next.prev = entry;
-		head.next = entry;
-		return entry.value;
+		//place it next to head
+		CacheEntry headNext = head.next;
+		head.next = cacheEntry;
+		cacheEntry.prev = head;
+		cacheEntry.next = headNext;
+		headNext.prev = cacheEntry;
+
+		return cacheEntry.val;
 	}
 
 	public void put(int key, int value) {
-		Entry entry = map.get(key);
-		if (entry == null) {
-			entry = new Entry(key, value);
-			head.next.prev = entry;
-			entry.next = head.next;
-			head.next = entry;
-			entry.prev = head;
-			if (size == 0) {
-				Entry removedLink = tail.prev;
-				removedLink.prev.next = tail;
-				tail.prev = removedLink.prev;
-				map.remove(removedLink.key);
-			}
-			else {
-				size--;
-			}
-			map.put(key, entry);
+		CacheEntry cacheEntry = cacheEntryMap.get(key);
+		if (cacheEntry != null) {
+			//remove item from the current position
+			CacheEntry cacheEntryPrev = cacheEntry.prev;
+			CacheEntry cacheEntryNext = cacheEntry.next;
+			cacheEntryPrev.next = cacheEntryNext;
+			cacheEntryNext.prev = cacheEntryPrev;
 		}
-		else {
-			entry.value = value;
 
-			entry.prev.next = entry.next;
-			entry.next.prev = entry.prev;
+		cacheEntry = new CacheEntry(key, value);
 
-			entry.next = head.next;
-			entry.prev = head;
-			head.next = entry;
-			entry.next.prev = entry;
+		//place it next to head
+		CacheEntry headNext = head.next;
+		head.next = cacheEntry;
+		cacheEntry.prev = head;
+		cacheEntry.next = headNext;
+		headNext.prev = cacheEntry;
+
+		cacheEntryMap.put(key, cacheEntry);
+
+		evictKeyIfNeeded();
+	}
+
+	private void evictKeyIfNeeded() {
+		if (cacheEntryMap.size() > capacity) {
+			//remove item from the tail
+			CacheEntry evictedEntry = tail.prev;
+			CacheEntry prev = evictedEntry.prev;
+			CacheEntry next = evictedEntry.next;
+			prev.next = next;
+			next.prev = prev;
+
+			cacheEntryMap.remove(evictedEntry.key);
 		}
 	}
 
