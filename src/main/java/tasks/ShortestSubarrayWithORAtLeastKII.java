955516e521c0dc6492741b3d6d07a1809fabcb2d
Ruslan Rakhmedov <rakhmedovrs@gmail.com>
2024-03-30 09:24:39 -0700

    Shortest Subarray With OR at Least K II

---
+++ src/main/java/tasks/ShortestSubarrayWithORAtLeastKII.java
@@ -0,0 +1,58 @@
+package tasks;
+
+import common.Difficulty;
+import common.LeetCode;
+
+/**
+ * @author Ruslan Rakhmedov
+ * @created 2024-03-30
+ */
+@LeetCode(
+		id = 3097,
+		name = "Shortest Subarray With OR at Least K II",
+		url = "https://leetcode.com/problems/shortest-subarray-with-or-at-least-k-ii/description/",
+		difficulty = Difficulty.MEDIUM
+)
+public class ShortestSubarrayWithORAtLeastKII {
+	public int minimumSubarrayLength(int[] nums, int k) {
+		int or = 0;
+		int left = 0;
+		int right = 0;
+		int min = Integer.MAX_VALUE;
+		int[] bits = new int[32];
+		while (right < nums.length) {
+			or(bits, nums[right++]);
+			or = constructNum(bits);
+			while (or >= k && left < right) {
+				min = Math.min(min, right - left);
+				removeBitsForNumber(bits, nums[left++]);
+				or = constructNum(bits);
+			}
+		}
+		return min == Integer.MAX_VALUE ? -1 : min;
+	}
+
+	private void or(int[] bits, int num) {
+		char[] chars = Integer.toBinaryString(num).toCharArray();
+		for (int i = chars.length - 1, j = 0; i >= 0; i--, j++) {
+			bits[j] += chars[i] - '0';
+		}
+	}
+
+	private void removeBitsForNumber(int[] bits, int num) {
+		char[] chars = Integer.toBinaryString(num).toCharArray();
+		for (int i = chars.length - 1, j = 0; i >= 0; i--, j++) {
+			bits[j] -= chars[i] - '0';
+		}
+	}
+
+	private int constructNum(int[] bits) {
+		int num = 0;
+		for (int pow = 0, i = 0; i < bits.length; i++, pow++) {
+			if (bits[i] > 0) {
+				num += Math.pow(2, pow);
+			}
+		}
+		return num;
+	}
+}
