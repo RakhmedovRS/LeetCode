80d2b98d44a73f2461819d6cb566bf44ae0e6267
Ruslan Rakhmedov <rakhmedovrs@gmail.com>
2024-04-20 22:30:26 -0700

    Minimum Number of Operations to Satisfy Conditions

---
+++ src/main/java/tasks/MinimumNumberOfOperationsToSatisfyConditions.java
@@ -0,0 +1,16 @@
+package tasks;
+import common.Difficulty;
+import common.LeetCode;
+import java.util.*;
+
+/**
+ * @author Ruslan Rakhmedov
+ * @created 2024-04-20
+ */
+@LeetCode(
+id = 3122,
+ name = "Minimum Number of Operations to Satisfy Conditions",
+ url = "https://leetcode.com/problems/minimum-number-of-operations-to-satisfy-conditions/description/",
+ difficulty = Difficulty.MEDIUM
+)
+public class MinimumNumberOfOperationsToSatisfyConditions {}
