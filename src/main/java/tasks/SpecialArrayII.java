0e8eb75f390fc5fc30aa904529f3aba0ea9d9bbe
Ruslan Rakhmedov <rakhmedovrs@gmail.com>
2024-05-26 23:19:28 -0700

    Special Array II

---
+++ src/main/java/tasks/SpecialArrayII.java
@@ -0,0 +1,16 @@
+package tasks;
+import common.Difficulty;
+import common.LeetCode;
+import java.util.*;
+
+/**
+ * @author Ruslan Rakhmedov
+ * @created 2024-05-26
+ */
+@LeetCode(
+id = 3152,
+ name = "Special Array II",
+ url = "https://leetcode.com/problems/special-array-ii/description/",
+ difficulty = Difficulty.MEDIUM
+)
+public class SpecialArrayII {}
