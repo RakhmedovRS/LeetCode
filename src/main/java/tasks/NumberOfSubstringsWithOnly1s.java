d21193fc980e466aef075e63219b9c2a9d577fd1
rakhmedovrs <rakhmedovrs@gmail.com>
2025-11-15 20:05:13 -0800

    Number of Substrings With Only 1s

--- src/main/java/tasks/NumberOfSubstringsWithOnly1s.java
+++ src/main/java/tasks/NumberOfSubstringsWithOnly1s.java
@@ -1,26 +1,36 @@
 package tasks;
 
+import common.Difficulty;
 import common.LeetCode;
 
 /**
  * @author RakhmedovRS
  * @created 12-Jul-20
  */
-@LeetCode(id = 1513, name = "Number of Substrings With Only 1s", url = "https://leetcode.com/problems/number-of-substrings-with-only-1s/")
+@LeetCode(id = 1513,
+        name = "Number of Substrings With Only 1s",
+        url = "https://leetcode.com/problems/number-of-substrings-with-only-1s/",
+        difficulty = Difficulty.MEDIUM
+)
 public class NumberOfSubstringsWithOnly1s {
-	public int numSub(String s) {
-		long ones = 0;
-		int mod = (int) Math.pow(10, 9) + 7;
-		String[] parts = s.split("0");
-		for (String part : parts) {
-			int i = 1;
-			while (i <= part.length()) {
-				ones += i;
-				i++;
-			}
-			ones %= mod;
-		}
+    int MOD = 1_000_000_007;
 
-		return (int) ones;
-	}
+    public int numSub(String s) {
+        long ans = 0;
+        int ones = 0;
+        for (char ch : s.toCharArray()) {
+            if (ch == '0') {
+                ans = (ans + ((long) ones * (ones + 1)) / 2) % MOD;
+                ones = 0;
+            } else {
+                ones++;
+            }
+        }
+
+        if (ones != 0) {
+            ans = (ans + ((long) ones * (ones + 1)) / 2) % MOD;
+        }
+
+        return (int) ans;
+    }
 }
