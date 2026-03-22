e960e1356a24de7ec76aa29197f03c508384aeaa
rakhmedovrs <rakhmedovrs@gmail.com>
2026-03-22 10:03:26 -0700

    Construct Uniform Parity Array I

---
+++ src/main/java/tasks/ConstructUniformParityArrayI.java
@@ -0,0 +1,20 @@
+package tasks;
+import common.Difficulty;
+import common.LeetCode;
+import java.util.*;
+
+/**
+ * @author Ruslan Rakhmedov
+ * @created 2026-03-22
+ */
+@LeetCode(
+id = 3875,
+ name = "Construct Uniform Parity Array I",
+ url = "https://leetcode.com/problems/construct-uniform-parity-array-i/description/",
+ difficulty = Difficulty.EASY
+)
+public class ConstructUniformParityArrayI {
+    public boolean uniformArray(int[] nums1) {
+        return true;
+    }
+}
