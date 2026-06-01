c3fae4a9cf86422c10ed1f4bb60a99b61fdb5454
rakhmedovrs <rakhmedovrs@gmail.com>
2026-05-31 21:08:38 -0700

    Maximum Number of Items From Sale I

---
+++ src/main/java/tasks/MaximumNumberOfItemsFromSaleI.java
@@ -0,0 +1,16 @@
+package tasks;
+import common.Difficulty;
+import common.LeetCode;
+import java.util.*;
+
+/**
+ * @author Ruslan Rakhmedov
+ * @created 2026-05-31
+ */
+@LeetCode(
+id = 3946,
+ name = "Maximum Number of Items From Sale I",
+ url = "https://leetcode.com/problems/maximum-number-of-items-from-sale-i/description/",
+ difficulty = Difficulty.MEDIUM
+)
+public class MaximumNumberOfItemsFromSaleI {}
